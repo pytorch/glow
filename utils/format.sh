023cc83caf0957261922a7e9abcec88bf8fd9ee1
Nadav Rotem <nrotem@fb.com>
2017-07-14 15:41:08 -0700

    Add a script to run clang-format on all files.

---
+++ utils/format.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [ $(which clang-format) ]; then
+  FILES=`find lib include tools unittests -name \*.h -print -o -name \*.cpp -print`
+
+  FARRAY=( $FILES ) # count the number of files to process
+  echo  Formatting ${#FARRAY[@]} files
+
+  for F in $FILES; do
+    clang-format -i $F
+    echo -n .
+  done
+  echo
+  echo "Done"
+  exit
+fi
+
+echo "ERROR: can't find clang-format in your path."
+
