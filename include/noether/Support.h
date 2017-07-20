c338418196fe86d8c08f707939f6f1514d91d388
Nadav Rotem <nrotem@fb.com>
2017-07-20 10:41:16 -0700

    Add an RAII utility for measuring the training rate.

---
+++ include/noether/Support.h
@@ -0,0 +1,30 @@
+#ifndef NOETHER_SUPPORT_H
+#define NOETHER_SUPPORT_H
+
+#include <chrono>
+#include <iostream>
+
+namespace noether {
+
+/// A class for measuring the lifetime of some event
+/// and for rate calculation.
+class TimerGuard {
+  int iterations_;
+  std::chrono::time_point<std::chrono::system_clock> start;
+public:
+
+  TimerGuard(int iterations) : iterations_(iterations) {
+    start = std::chrono::system_clock::now();
+  }
+
+  ~TimerGuard() {
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::cout <<"Rate: " << (iterations_/elapsed_seconds.count()) <<
+    "/sec\n";
+  }
+};
+
+} // namespace
+
+#endif // NOETHER_SUPPORT_H
