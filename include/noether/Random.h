032e34bb8939e37bfe041d95d85e06eab1266318
Nadav Rotem <nrotem@fb.com>
2017-07-26 11:21:24 -0700

    Add a new utility for computing random numbers. This code is extracted from Tensor, but is not yet hooked up because the new random number generator exposed some bugs or numeric instability in MNIST and some of the other small tests.

---
+++ include/noether/Random.h
@@ -0,0 +1,12 @@
+#ifndef NOETHER_RANDOM_H
+#define NOETHER_RANDOM_H
+
+
+namespace noether {
+
+/// \returns the next uniform random number in the range -1..1.
+double nextRand();
+
+} // namespace
+
+#endif // NOETHER_RANDOM_H
