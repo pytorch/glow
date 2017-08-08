73b580f1b863710adb25fd99d93cacb9a5ff53ee
Nadav Rotem <nrotem@fb.com>
2017-08-08 11:06:41 -0700

    Add a unit test that performs numeric gradient checks on the FC, Concat, Regression and the RELU nodes.

--- tests/Network/network.cpp
+++ tests/Network/network.cpp
@@ -291,3 +291,79 @@ TEST(Network, learnSingleValueConcat) {
   EXPECT_NEAR(RNWH.at({0}), 0.9, 0.1);
 }
 
+/// Compute the regression loss for the tensor \p X with regard to Y.
+FloatTy computeL2Loss(Tensor *X, Tensor *Y) {
+  assert(X->dims().size() == 1 && "Invalid input dims");
+  assert(X->dims() == Y->dims() && "Invalid input dims");
+  auto xH = X->getHandle<FloatTy>();
+  auto yH = Y->getHandle<FloatTy>();
+  FloatTy loss = 0;
+
+  for (size_t i = 0, e = X->size(); i < e; i++) {
+    FloatTy dy = (xH.at({i}) - yH.at({i}));
+    loss += 0.5 * dy * dy;
+  }
+
+  return loss;
+}
+
+TEST(Network, gradientCheck_FC_Concat_RELU) {
+  // Learning a single input vector.
+  Network N;
+  N.getConfig().batchSize = 10;
+
+  size_t numInputElem = 20;
+  size_t numOutputElem = 10;
+
+  auto *A = N.createArrayNode(numInputElem);
+  auto *B = N.createArrayNode(numInputElem);
+  NodeBase *FA = N.createFullyConnectedNode(A, numOutputElem / 2);
+  FA = N.createRELUNode(FA);
+
+  NodeBase *FB = N.createFullyConnectedNode(B, numOutputElem / 2);
+  FB = N.createRELUNode(FB);
+
+  NodeBase *O = N.createConcatNode({FA, FB}, 0);
+  auto *RN = N.createRegressionNode(O);
+
+  Tensor inputs(ElemKind::FloatTy, {numInputElem});
+  Tensor outputs(ElemKind::FloatTy, {numOutputElem});
+
+  auto inputsH = inputs.getHandle<FloatTy>();
+  auto outputsH = inputs.getHandle<FloatTy>();
+
+  inputsH.randomize(100);
+  outputsH.randomize(100);
+
+  // Train the network just once to calculate the grads.
+  N.train(RN, {A, RN}, {&inputs, &outputs});
+
+  float delta = 0.001;
+
+  auto analyticalGrads = A->getGradHandle(N.getMainContext()).clone();
+  auto analyticalGradsH = analyticalGrads.getHandle<FloatTy>();
+
+  for (size_t i = 0; i < analyticalGrads.size(); i++) {
+    auto old = inputsH.at({i});
+
+    // Calculate f(x+e):
+    inputsH.at({i}) = old + delta;
+    Tensor *res = N.infer(RN, {A}, {&inputs});
+    auto plusLoss = computeL2Loss(&outputs, res);
+
+    // Calculate f(x-e):
+    inputsH.at({i}) = old - delta;
+    res = N.infer(RN, {A}, {&inputs});
+    auto minusLoss = computeL2Loss(&outputs, res);
+    inputsH.at({i}) = old;
+
+    auto numericGrad = (plusLoss - minusLoss)/(2 * delta);
+    auto analyticalGrad = analyticalGradsH.at({i});
+
+    auto err = std::abs(analyticalGrad - numericGrad) /
+               std::abs(analyticalGrad + numericGrad);
+
+    // Make sure that the analytical and numerical gradients agree.
+    EXPECT_LE(err, 0.01);
+  }
+}
