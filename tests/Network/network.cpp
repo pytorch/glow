38347d811158bb3f0d07afb0e1597ff462b42bb5
Nadav Rotem <nrotem@fb.com>
2017-08-08 13:39:39 -0700

    Add a gradient check for Convolution and MaxPool. This test exposed a bug in the MaxPool backprop!

--- tests/Network/network.cpp
+++ tests/Network/network.cpp
@@ -307,8 +307,12 @@ FloatTy computeL2Loss(Tensor *X, Tensor *Y) {
   return loss;
 }
 
+/// \returns the error rate when comparing two grads.
+FloatTy gradDiff(FloatTy G1, FloatTy G2) {
+  return std::abs(G1 - G2) / std::abs(G1 + G2 + 1);
+}
+
 TEST(Network, gradientCheck_FC_Concat_RELU) {
-  // Learning a single input vector.
   Network N;
   N.getConfig().batchSize = 10;
 
@@ -360,10 +364,67 @@ TEST(Network, gradientCheck_FC_Concat_RELU) {
     auto numericGrad = (plusLoss - minusLoss)/(2 * delta);
     auto analyticalGrad = analyticalGradsH.at({i});
 
-    auto err = std::abs(analyticalGrad - numericGrad) /
-               std::abs(analyticalGrad + numericGrad);
+    auto err =  gradDiff(analyticalGrad, numericGrad);
 
     // Make sure that the analytical and numerical gradients agree.
     EXPECT_LE(err, 0.01);
   }
 }
+
+TEST(Network, gradientCheck_Conv) {
+  Network N;
+  N.getConfig().batchSize = 10;
+
+  size_t numDim = 10;
+  size_t numOutputElem = 10;
+
+  auto *A = N.createArrayNode({numDim, numDim, 1});
+  NodeBase *O = N.createConvNode(A, 16, 5, 1, 2);
+  O = N.createMaxPoolNode(O, 3, 3, 0);
+  O = N.createFullyConnectedNode(O, numOutputElem);
+  O = N.createRELUNode(O);
+  auto *RN = N.createRegressionNode(O);
+
+  Tensor inputs(ElemKind::FloatTy, {numDim, numDim, 1});
+  Tensor outputs(ElemKind::FloatTy, {numOutputElem});
+
+  auto inputsH = inputs.getHandle<FloatTy>();
+  auto outputsH = inputs.getHandle<FloatTy>();
+
+  inputsH.randomize(1);
+  outputsH.randomize(1);
+
+  // Train the network just once to calculate the grads.
+  N.train(RN, {A, RN}, {&inputs, &outputs});
+
+  float delta = 0.001;
+
+  auto analyticalGrads = A->getGradHandle(N.getMainContext()).clone();
+  auto analyticalGradsH = analyticalGrads.getHandle<FloatTy>();
+
+  for (size_t i = 0; i < numDim; i++) {
+    for (size_t j = 0; j < numDim; j++) {
+      auto old = inputsH.at({i, j , 0});
+
+      // Calculate f(x+e):
+      inputsH.at({i, j, 0}) = old + delta;
+      Tensor *res = N.infer(RN, {A}, {&inputs});
+      auto plusLoss = computeL2Loss(&outputs, res);
+
+      // Calculate f(x-e):
+      inputsH.at({i, j, 0}) = old - delta;
+      res = N.infer(RN, {A}, {&inputs});
+      auto minusLoss = computeL2Loss(&outputs, res);
+      inputsH.at({i, j, 0}) = old;
+
+      auto numericGrad = (plusLoss - minusLoss)/(2 * delta);
+      auto analyticalGrad = analyticalGradsH.at({i, j, 0});
+
+      auto err =  gradDiff(analyticalGrad, numericGrad);
+
+      // Make sure that the analytical and numerical gradients agree.
+      EXPECT_LE(err, 0.04);
+    }
+  }
+}
+
