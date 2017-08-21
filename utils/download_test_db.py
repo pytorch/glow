b2a89b3db4d606ce3ba561f77b5f9ea6fdcc8e95
Nadav Rotem <nrotem@fb.com>
2017-08-20 23:25:21 -0700

    Add a script that downloads the mnist+cifar test databases.
    Summary:
    Add a script that downloads the mnist+cifar test databases. The script
    downloads the databases and extracts them to disk. These files are used in the
    project by the programs in the /examples directory and demonstrate the use of
    the library.
    
    Test Plan: None.
    
    Reviewers: #glow, msmelyan
    
    Reviewed By: msmelyan
    
    Subscribers: #glow
    
    Differential Revision: https://phabricator.intern.facebook.com/D5666634
    
    Signature: t1:5666634:1503296416:9aff943e70bc2553d645331abcc630dda46e75a7
    
    Blame Revision: None

---
+++ utils/download_test_db.py
@@ -0,0 +1,53 @@
+import pickle
+import gzip
+import array
+import urllib
+import tarfile
+import os.path
+
+# This script downloads and extracts the mnist and cifar-10 databases.
+
+print("""Downloading test files. If the download fails try setting up a proxy:
+    #export http_proxy="http://fwdproxy:8080
+
+""")
+
+mnist_filename = "mnist.pkl.gz"
+cifar10_filename = "cifar-10.binary.tar.gz"
+
+if os.path.exists(mnist_filename):
+    print("MNIST file found. Not downloading.")
+else:
+    print("Downloading MNIST ... ")
+    urllib.urlretrieve ("http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz", mnist_filename)
+
+if os.path.exists(cifar10_filename):
+    print("CIFAR file found. Not downloading.")
+else:
+    print("Downloading CIFAR ... ")
+    urllib.urlretrieve ("http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz", cifar10_filename)
+
+def dumpToFile(dataset):
+    data, labels = dataset
+
+    imagesFile = open('mnist_images.bin', 'wb')
+    data.tofile(imagesFile)
+    imagesFile.close()
+
+    labelsFile = open('mnist_labels.bin', 'wb')
+    L = array.array('B', labels)
+    L.tofile(labelsFile)
+    labelsFile.close()
+
+print("Extracting the mnist database.")
+
+with gzip.open(mnist_filename, 'rb') as f:
+    train_set, valid_set, test_set = pickle.load(f)
+    dumpToFile(train_set)
+
+
+print("Extracting the CIFAR-10 database.")
+tar = tarfile.open(cifar10_filename, "r:gz")
+tar.extractall()
+tar.close()
+
