91501af844c482a254a2ae10d78ba95973db9bdd
Nadav Rotem <nrotem@fb.com>
2017-09-08 10:15:06 -0700

    Teach the loader driver to load png images.
    Summary:
    Teach the loader program driver to load images in the NCHW format (BGR order)
    and pass them for inference in the models that are loaded from disk. This is
    just a test program and not a general inference driver.
    
    Test Plan: This is a test. We use it to run VGG19.
    
    Reviewers: #glow, jongsoo
    
    Reviewed By: jongsoo
    
    Subscribers: jongsoo, tulloch, #glow
    
    Differential Revision: https://phabricator.intern.facebook.com/D5788800
    
    Tags: none
    
    Signature: 5788800:1504821071:557f7a060163edce34e12961fb13e5035877a227

--- tools/loader/loader.cpp
+++ tools/loader/loader.cpp
@@ -6,27 +6,56 @@
 
 using namespace glow;
 
+/// Loads and normalizes a PNG into a tensor in the NCHW 3x224x224 format.
+void loadImageAndPreprocess(const std::string &filename, Tensor *result) {
+  Tensor localCopy;
+  readPngImage(&localCopy, filename.c_str());
+  auto catH = localCopy.getHandle<FloatTy>();
+
+  auto dims = localCopy.dims();
+
+  result->reset(ElemKind::FloatTy, {1, 3, dims[0], dims[1]});
+  auto RH = result->getHandle<FloatTy>();
+
+  for (unsigned z = 0; z < 3; z++) {
+    for (unsigned y = 0; y < dims[1]; y++) {
+      for (unsigned x = 0; x < dims[0]; x++) {
+        // Convert to BGR, and subtract the mean, which we hardcode to 128.
+        RH.at({0, 2 - z, x, y}) = (catH.at({x, y, z}) - 128);
+      }
+    }
+  }
+}
+
 int main(int argc, char **argv) {
-  if (argc != 3) {
-    std::cerr << "Usage:  " << argv[0] << " network_structure.pb weights.pb\n";
+  if (argc != 4) {
+    std::cerr << "Usage: " << argv[0]
+              << "image.png network_structure.pb weights.pb\n";
     return -1;
   }
 
-  auto *data = new Tensor(ElemKind::FloatTy, {8, 224, 224, 3});
-  auto *expected_softmax = new Tensor(ElemKind::IndexTy, {8, 1});
+  Tensor data;
+  Tensor expected_softmax(ElemKind::IndexTy, {1, 1});
 
-  auto H = data->getHandle<FloatTy>();
-  H.randomize(1);
+  loadImageAndPreprocess(argv[1], &data);
 
   glow::Network N;
-  caffe2ModelLoader LD(argv[1], argv[2], {"data", "softmax_expected"},
-                       {data, expected_softmax}, N);
+  caffe2ModelLoader LD(argv[2], argv[3], {"data", "softmax_expected"},
+                       {&data, &expected_softmax}, N);
 
   auto *SM = LD.getNodeByName("prob");
+  Variable *input = (Variable *)LD.getNodeByName("data");
 
   N.dumpGraph();
 
-  auto *res = N.infer(SM, {}, {});
-  res->getHandle<FloatTy>().dump();
+  auto *res = N.infer(SM, {input}, {&data});
+  auto H = res->getHandle<FloatTy>();
+  H.dump("res=", "\n");
+  Tensor slice = H.extractSlice(0);
+  auto SH = slice.getHandle<FloatTy>();
+
+  std::cout << "\n";
+  std::cout << "Result =" << SH.maxArg() << "\n";
+
   return 0;
 }
