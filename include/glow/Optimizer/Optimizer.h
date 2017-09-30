43f6edbf86ee4ed1307bc538362f776bd44963a2
Nadav Rotem <nrotem@fb.com>
2017-09-29 17:54:04 -0700

    Start to sketch out the optimizer.
    This commit adds a very simple optimizer library that contains a single method
    optimize(). This method drives the whole optimization pipeline, which is
    currently very basic. The next step would be to add this into the compilation
    pipeline and run it on all of our tests.

---
+++ include/glow/Optimizer/Optimizer.h
@@ -0,0 +1,12 @@
+#ifndef GLOW_OPTIMIZER_OPTIMIZER_H
+#define GLOW_OPTIMIZER_OPTIMIZER_H
+
+namespace glow {
+
+class Module;
+
+void optimize(Module &M);
+
+} // namespace glow
+
+#endif // GLOW_OPTIMIZER_OPTIMIZER_H
---
+++ src/glow/Optimizer/Optimizer.cpp
@@ -0,0 +1,54 @@
+#include "glow/Optimizer/Optimizer.h"
+#include "glow/IR/IR.h"
+#include "glow/IR/Instrs.h"
+#include "glow/Support/Casting.h"
+
+#include <unordered_map>
+
+using namespace glow;
+
+/// Hoists Dealloc instructions right after their last use.
+static void hoistDealloc(Module &M) {
+  using iterator = Module::InstListTy::iterator;
+  /// Maps activation instructions to their last non-dealloc user.
+  std::unordered_map<AllocActivationInst *, iterator> lastUser;
+  auto &bb = M.getInstrs();
+
+  // Record the last use of each dealloc.
+  for (auto it = bb.begin(), e = bb.end(); it != e; ++it) {
+    if (isa<DeallocActivationInst>(*it))
+      continue;
+
+    for (int i = 0, e = (*it)->getNumOperands(); i < e; i++) {
+      auto op = (*it)->getOperand(i).first;
+      if (auto alloc = dyn_cast<AllocActivationInst>(op)) {
+        lastUser[alloc] = it;
+      }
+    }
+  }
+
+  // Now that we've found the last user we can hoist the instruction.
+  for (auto it = bb.begin(), e = bb.end(); it != e; /* increment below */) {
+    iterator curr = it;
+    auto *da = dyn_cast<DeallocActivationInst>(*curr);
+    if (!da) {
+      ++it;
+      continue;
+    }
+
+    auto *alloc = cast<AllocActivationInst>(da->getOperand(0).first);
+
+    it = bb.erase(curr);
+    auto where = lastUser[alloc];
+    where++;
+    bb.insert(where, da);
+  }
+}
+
+void glow::optimize(Module &M) {
+  M.verify();
+
+  hoistDealloc(M);
+
+  M.verify();
+}
