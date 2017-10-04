eac226ce71f1ceb7c02abd925f5bc4f787a23006
Nadav Rotem <nrotem@fb.com>
2017-10-03 18:03:54 -0700

    GraphIR: sketch out the graph builder.

--- include/glow/Graph/Graph.h
+++ include/glow/Graph/Graph.h
@@ -1,18 +1,87 @@
 #ifndef GLOW_GRAPH_GRAPH_H
 #define GLOW_GRAPH_GRAPH_H
 
+#include "glow/IR/Instrs.h"
+
+#include "llvm/ADT/ArrayRef.h"
+
+#include <vector>
+
 namespace glow {
 
 class Node;
+class Variable;
+class ConvolutionNode;
+class PoolNode;
+class FullyConnectedNode;
+class ReluNode;
+class SigmoidNode;
+class TanhNode;
+class SoftMaxNode;
+class RegressionNode;
+class ReshapeNode;
+class TransposeNode;
+class ConcatNode;
+class BatchNormalizationNode;
+class LocalResponseNormalizationNode;
+class ArithmeticNode;
 
 /// Represents the compute graph.
 class Graph final {
   /// A list of nodes that the graph owns.
   std::vector<Node *> nodes_;
+  /// A list of variables.
+  std::vector<Variable *> vars_;
+
+  /// Inserts the node \p N to the list of nodes, and returns the inserted node.
+  Node *addNode(Node *N) {
+    nodes_.push_back(N);
+    return N;
+  }
 
 public:
   Graph() = default;
   ~Graph();
+
+  /// @name High-level, operation-level IRBuilder.
+  ///@{
+
+  ConvolutionNode *createConv(Node *input, size_t depth, size_t kernel,
+                              size_t stride, size_t pad);
+
+  PoolNode *createPool(Node *input, PoolInst::OpKind kind, size_t kernel,
+                       size_t stride, size_t pad);
+
+  FullyConnectedNode *createFullyConnected(Node *input, size_t outDepth);
+
+  ReluNode *createRELU(Node *input);
+
+  SigmoidNode *createSigmoid(Node *input);
+
+  TanhNode *createTanh(Node *input);
+
+  SoftMaxNode *createSoftMax(Node *input, Node *selected);
+
+  RegressionNode *createRegression(Node *input, Node *expected);
+
+  ReshapeNode *createReshape(Node *input, llvm::ArrayRef<size_t> shape);
+
+  TransposeNode *createTranspose(Node *input, llvm::ArrayRef<unsigned> shuffle);
+
+  ConcatNode *createConcat(llvm::ArrayRef<Node *> inputs, unsigned dimension);
+
+  BatchNormalizationNode *createBatchNormalization(Node *input,
+                                                   size_t channelIdx = 0,
+                                                   float epsilon = 1e-5,
+                                                   float momentum = 0.9);
+
+  LocalResponseNormalizationNode *
+  createLocalResponseNormalization(Node *input, size_t halfWindowSize = 2,
+                                   float alpha = 1e-4, float beta = 0.75,
+                                   float k = 2.0);
+
+  ArithmeticNode *createArithmetic(Node *LHS, Node *RHS,
+                                   ArithmeticInst::OpKind op);
 };
 
 } // namespace glow
