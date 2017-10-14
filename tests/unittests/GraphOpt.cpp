c371c79f04e4ca4ebae8e53a4ddd75588db10653
Nadav Rotem <nrotem@fb.com>
2017-10-14 10:12:08 -0700

    Graph Optimizer: Convert the DCE optimization to the new use-def api and add a unit test that checks that the DCE optimization works.

---
+++ tests/unittests/GraphOpt.cpp
@@ -0,0 +1,41 @@
+// Copyright 2017 Facebook Inc.  All Rights Reserved.
+
+#include "glow/Graph/Graph.h"
+#include "glow/Graph/Node.h"
+#include "glow/Graph/Nodes.h"
+#include "glow/IR/IR.h"
+#include "glow/Optimizer/Optimizer.h"
+
+#include "gtest/gtest.h"
+
+#include <algorithm>
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <iostream>
+#include <string>
+
+using namespace glow;
+
+TEST(GraphOptz, DCE) {
+
+  Graph G;
+  Module M(&G);
+  Node *K = G.createVariable(ElemKind::FloatTy, {4, 320, 200, 3}, "input");
+
+  for (int i = 0; i < 40; i++) {
+    K = G.createRELU("relu", K);
+    // Add a graph structure that diverges and converges, to catch algorithms
+    // that perform a dump recursive scan.
+    K = G.createArithmetic("arith", K, K, ArithmeticNode::OpKind::Add);
+  }
+
+  // Check that we know how many knows we've created.
+  EXPECT_EQ(G.getNodes().size(), 40);
+
+  // Optimize all of the dead code.
+  ::glow::optimize(G, OptimizationMode::Infer);
+
+  //  All of the nodes are gone.
+  EXPECT_EQ(G.getNodes().size(), 0);
+}
