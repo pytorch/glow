958e80e5d63724867795c672e51f14da190fb36b
Nadav Rotem <nrotem@fb.com>
2017-10-15 18:43:41 -0700

    Add a few images and some scripts that run the pretrained caffe models on the images. Run the tests with the script: ./tests/images/run.sh in the build directory.

--- utils/download_caffe2_models.sh
+++ utils/download_caffe2_models.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 for model in "resnet50" "vgg16" "vgg19" "inception_v1" "inception_v2" "shufflenet" "squeezenet" ; do
   for file in predict_net.pbtxt predict_net.pb init_net.pb; do
     wget https://s3.amazonaws.com/download.caffe2.ai/models/$model/$file -P $model
