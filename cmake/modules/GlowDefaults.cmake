b90f37ff7179ce04a46662009edf5072222535ce
Saleem Abdulrasool <compnerd@compnerd.org>
2017-10-16 08:41:47 -0700

    build: merge Flags and GlowDefaults
    The Flags cmake module was used to specify default compile flags.
    Merge the settings into `GlowDefaults.cmake`. NFC

--- cmake/modules/GlowDefaults.cmake
+++ cmake/modules/GlowDefaults.cmake
@@ -9,3 +9,13 @@ if(NOT ENABLE_TESTING)
   set(ENABLE_TESTING YES CACHE STRING "" FORCE)
 endif()
 
+if(MSVC OR CMAKE_CXX_SIMULATE_ID STREQUAL "MSVC")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Wall /EHsc- /GR-")
+  set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /fp:fast")
+  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /Oy /Od")
+else()
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -fno-exceptions -fno-rtti")
+  set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -march=native -ffast-math")
+  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -fno-omit-frame-pointer -O0")
+endif()
+
