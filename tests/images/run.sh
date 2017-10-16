d296f938f070d5d88bfd0477a66e01c5c35cfe2a
Nadav Rotem <nrotem@fb.com>
2017-10-15 18:43:41 -0700

    Add a few images and some scripts that run the pretrained caffe models on the images. Run the tests with the script: ./tests/images/run.sh in the build directory.

---
+++ tests/images/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+for png_filename in tests/images/*.png; do
+  ./bin/loader $png_filename 0to1 resnet50/predict_net.pb resnet50/init_net.pb
+  ./bin/loader $png_filename 128to127 vgg19/predict_net.pb vgg19/init_net.pb
+done
+
