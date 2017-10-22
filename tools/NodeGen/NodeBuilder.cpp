99e13db045e5681c2f7779fb125ec9b411424d76
Nadav Rotem <nrotem@fb.com>
2017-10-22 16:02:40 -0700

    Split NodeGen into a library and the piece of code that defines the graph nodes.

---
+++ tools/NodeGen/NodeBuilder.cpp
@@ -0,0 +1,208 @@
+#include "NodeBuilder.h"
+
+void NodeBuilder::emitEnumModePrinters(std::ostream &os) const {
+  os << "const char *" << name_ << "Node::getModeStr(" << name_
+     << "Node::Mode m) {\n";
+  os << "\tstatic const char *names[] = {";
+  for (const auto &e : enum_) {
+    os << "\"" << e << "\", ";
+  }
+  os << "nullptr};\n";
+  os << "\treturn names[static_cast<int>(m)];\n";
+  os << "}\n";
+}
+
+void NodeBuilder::emitCtor(std::ostream &os) const {
+  os << "\t" << name_ << "Node(llvm::StringRef name";
+
+  // Constructor non-standard parameter list:
+  for (const auto &op : extraParams_) {
+    os << ", " << op.first << " " << op.second << " ";
+  }
+
+  // The enum 'Mode' parameter:
+  if (enum_.size()) {
+    os << ", Mode mode";
+  }
+
+  // The operands of the graph node:
+  for (const auto &op : operands_) {
+    os << ", Node *" << op;
+  }
+
+  // Extra class members:
+  for (const auto &op : members_) {
+    os << ", " << op.first << " " << op.second;
+  }
+
+  // Initialize the base clases:
+  os << "):\n\t Node(Kinded::Kind::" << name_ << "NodeKind, " << ty_
+     << ", name)";
+
+  // Print the initialization list:
+  if (enum_.size()) {
+    os << ", mode_(mode)";
+  }
+
+  // Initialize the operands:
+  for (const auto &op : operands_) {
+    os << ", " << op << "_(" << op << ")";
+  }
+
+  // Initialize the members:
+  for (const auto &op : members_) {
+    os << ", " << op.second << "_(" << op.second << ") ";
+  }
+
+  // Empty constructor body.
+  os << " {}\n\n";
+}
+
+void NodeBuilder::emitClassMembers(std::ostream &os) const {
+  // Emit the type of the enum (which is public).
+  if (enum_.size()) {
+    os << "\tpublic:\n\tenum class Mode {\n";
+    for (const auto &E : enum_) {
+      os << "\t  " << E << ",\n";
+    }
+    os << "\t};\n";
+
+    os << "\tprivate:\n";
+  }
+
+  // Emit class members:
+  if (enum_.size()) {
+    os << "\tMode mode_;\n";
+  }
+  for (const auto &op : operands_) {
+    os << "\tNodeOperand " << op << "_;\n";
+  }
+  for (const auto &op : members_) {
+    os << "\t" << op.first << " " << op.second << "_;\n";
+  }
+  os << "\n";
+}
+
+void NodeBuilder::emitSettersGetters(std::ostream &os) const {
+  // Print the getters/setters.
+  for (const auto &op : operands_) {
+    // Synthesize a user-defined operand getter.
+    auto it = overrideGetter_.find(op);
+    if (it != overrideGetter_.end()) {
+      os << "\t" << it->second << "\n";
+      continue;
+    }
+
+    // Synthesize the general getter.
+    os << "\tNode *get" << op << "() const { return " << op << "_; }\n";
+  }
+
+  for (const auto &op : members_) {
+    // Synthesize a user-defined member getter.
+    auto it = overrideGetter_.find(op.second);
+    if (it != overrideGetter_.end()) {
+      os << "\t" << it->second << "\n";
+      continue;
+    }
+
+    // Synthesize the general getter.
+    os << "\t" << op.first + " get" << op.second << "() const { return "
+       << op.second << "_; }\n";
+  }
+  // Synthesize the 'classof' method that enables the non-rtti polymorphism.
+  os << "\nstatic bool classof(const Kinded *k) { return k->getKind() == "
+        "Kinded::Kind::"
+     << name_ << "NodeKind; }\n";
+
+  if (enum_.size()) {
+    os << "\tMode getMode() const { return mode_; }\n";
+  }
+}
+
+void NodeBuilder::emitPrettyPrinter(std::ostream &os) const {
+  os << "std::string " << name_
+     << "Node::getDebugDesc() const {\n\t\tDescriptionBuilder "
+        "db(getKindName());\n\t\tdb.addParam(\"name\", getName())\n";
+
+  if (enum_.size()) {
+    os << "\t\t.addParam(\"Mode\", getModeStr())\n";
+  }
+
+  for (const auto &op : operands_) {
+    os << "\t\t.addParam(\"" << op << "\", *get" << op << "()->getType())\n";
+  }
+
+  for (const auto &mem : members_) {
+    os << "\t\t.addParam(\"" << mem.second << "\", get" << mem.second
+       << "())\n";
+  }
+  os << "\t\t.addParam(\"users\", getNumUsers());\n\t\treturn db;\n}\n";
+}
+
+void NodeBuilder::emitEquator(std::ostream &os) const {
+  os << "bool " << name_ << "Node::isEqual(const " << name_
+     << "Node &other) {\n\treturn true";
+
+  if (enum_.size()) {
+    os << " &&\n\t getMode() == other.getMode()";
+  }
+
+  for (const auto &op : operands_) {
+    os << " &&\n\t " << op << "_ == other." << op << "_";
+  }
+
+  for (const auto &mem : members_) {
+    os << " &&\n\t " << mem.second << "_ == other." << mem.second << "_";
+  }
+
+  os << ";\n }\n";
+}
+
+void NodeBuilder::emitVisitor(std::ostream &os) const {
+  os << "void " << name_
+     << "Node::visit(Node *parent, NodeVisitor *visitor) {\n\tif "
+        "(!visitor->shouldVisit(parent, this)) { return; }\n";
+
+  os << "\tvisitor->pre(parent, this);\n";
+  for (const auto &op : operands_) {
+    os << "\tget" << op << "()->visit(this, visitor);\n";
+  }
+  os << "\tvisitor->post(parent, this);\n";
+  os << "}\n";
+}
+
+void NodeBuilder::emitNodeClass(std::ostream &os) const {
+  os << "namespace glow {\nclass " << name_ << "Node final : public Node {\n";
+
+  emitClassMembers(os);
+
+  os << "\tpublic:\n";
+
+  emitCtor(os);
+
+  emitSettersGetters(os);
+
+  os << "\tstd::string getDebugDesc() const override;\n";
+  os << "\tbool isEqual(const " << name_ << "Node &other);\n";
+  os << "\tvoid visit(Node *parent, NodeVisitor *visitor) override;\n";
+  if (enum_.size()) {
+    os << "\tconst char *getModeStr() const { return getModeStr(mode_); "
+          "}\n\tstatic const char *getModeStr(Mode m);\n";
+  }
+
+  os << "};\n\n} // namespace glow\n";
+}
+
+void NodeBuilder::emitCppMethods(std::ostream &os) const {
+  emitPrettyPrinter(os);
+  emitVisitor(os);
+  emitEquator(os);
+  if (enum_.size()) {
+    emitEnumModePrinters(os);
+  }
+}
+
+NodeBuilder::~NodeBuilder() {
+  emitNodeClass(hStream);
+  emitCppMethods(cStream);
+}
---
+++ tools/NodeGen/NodeBuilder.h
@@ -0,0 +1,137 @@
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+class Builder;
+
+class NodeBuilder {
+  /// The type-initialization expression.
+  std::string ty_;
+  /// The node name.
+  std::string name_;
+  /// The node operands.
+  std::vector<std::string> operands_;
+  /// A list of node members. Format: (type, name).
+  std::vector<std::pair<std::string, std::string>> members_;
+  /// The node enum cases.
+  std::vector<std::string> enum_;
+  /// A list of extra parameters that are passed to the constructor.
+  std::vector<std::pair<std::string, std::string>> extraParams_;
+  /// A list of getters to override. Format (variable name, alternative getter).
+  std::unordered_map<std::string, std::string> overrideGetter_;
+  /// Header file stream.
+  std::ofstream &hStream;
+  /// CPP file stream.
+  std::ofstream &cStream;
+  /// Def file stream.
+  std::ofstream &dStream;
+
+public:
+  NodeBuilder(std::ofstream &H, std::ofstream &C, std::ofstream &D,
+              const std::string &name)
+      : name_(name), hStream(H), cStream(C), dStream(D) {}
+
+  /// Add an operand to the node. The name should start with a capital letter.
+  /// For example: "Input".
+  NodeBuilder &addOperand(const std::string &op) {
+    operands_.push_back(op);
+    return *this;
+  }
+  /// Add a member to the node. Format: type, name.
+  /// The name should start with a capital letter.
+  /// For example: "Filter".
+  NodeBuilder &addMember(const std::string &type, const std::string &name) {
+    members_.push_back({type, name});
+    return *this;
+  }
+
+  /// Override the getter for variable \p var with the body \p body.
+  NodeBuilder &overrideGetter(const std::string &var, const std::string &body) {
+    assert(!overrideGetter_.count(var) && "Variable already overridden");
+    overrideGetter_[var] = body;
+    return *this;
+  }
+
+  /// Add an field to the enum. The enum name should start with a capital
+  /// letter. For example: "External".
+  NodeBuilder &addEnumCase(const std::string &op) {
+    enum_.push_back(op);
+    return *this;
+  }
+  /// Set the expression that initializes the type of the node.
+  /// Example: 'LHS->getType()'.
+  NodeBuilder &setType(const std::string &ty) {
+    ty_ = ty;
+    return *this;
+  }
+  /// Add a parameter to the constructor. For example "TypeRef" "outTy".
+  NodeBuilder &addExtraParam(const std::string &type, const std::string &name) {
+    extraParams_.push_back({type, name});
+    return *this;
+  }
+
+  /// Emits the methods that converts an enum case into a textual label.
+  void emitEnumModePrinters(std::ostream &os) const;
+
+  /// Emit the Node class constructor.
+  void emitCtor(std::ostream &os) const;
+
+  /// Emits the class members (the fields of the class).
+  void emitClassMembers(std::ostream &os) const;
+
+  /// Emit stters/getters for each accessible class member.
+  void emitSettersGetters(std::ostream &os) const;
+
+  /// Emit the methods that print a textual summary of the node.
+  void emitPrettyPrinter(std::ostream &os) const;
+
+  /// Emit the isEqual method that performs node comparisons.
+  void emitEquator(std::ostream &os) const;
+
+  /// Emit the 'visit' method that implements node visitors.
+  void emitVisitor(std::ostream &os) const;
+
+  /// Emit the class definition for the node.
+  void emitNodeClass(std::ostream &os) const;
+
+  /// Emit the methods that go into the CPP file and implement the methods that
+  /// were declared in the header file.
+  void emitCppMethods(std::ostream &os) const;
+
+  ~NodeBuilder();
+};
+
+class Builder {
+  std::ofstream &hStream;
+  std::ofstream &cStream;
+  std::ofstream &dStream;
+
+public:
+  /// Create a new top-level builder that holds the three output streams that
+  /// point to the header file, cpp file and enum definition file.
+  Builder(std::ofstream &H, std::ofstream &C, std::ofstream &D)
+      : hStream(H), cStream(C), dStream(D) {
+    cStream << "#include \"glow/Graph/Nodes.h\"\n"
+               "#include \"glow/Base/Type.h\"\n"
+               "#include \"glow/Support/Support.h\"\n\n"
+               "using namespace glow;\n";
+    dStream << "#ifndef DEF_NODE\n#error The macro DEF_NODE was not declared.\n"
+               "#endif\n";
+  }
+
+  ~Builder() { dStream << "#undef DEF_NODE"; }
+
+  /// Declare a new node and generate code for it.
+  NodeBuilder newNode(const std::string &name) {
+    dStream << "DEF_NODE(" << name << "Node, " << name << ")\n";
+    return NodeBuilder(hStream, cStream, dStream, name);
+  }
+
+  /// Declare the node in the def file but don't generate code for it.
+  void declareNode(const std::string &name) {
+    dStream << "DEF_NODE(" << name << "Node, " << name << ")\n";
+  }
+};
