fc7f6d596a17b6ae79efbe0878a114a696d3413d
Nadav Rotem <nrotem@fb.com>
2017-11-03 21:11:56 -0700

    Implement the logic for copying from and to the device.

--- src/glow/Backends/OpenCL/OpenCL.cpp
+++ src/glow/Backends/OpenCL/OpenCL.cpp
@@ -15,10 +15,10 @@ Backend *glow::createOCLBackend(Module *M) { return new OCLBackend(M); }
 
 const char *ReluSrc =
     "__kernel void op(__global float* dest, __global float* src)"
-    "{ size_t i = get_global_id(0); output[i] = max(input[i], 0); }";
+    "{ size_t i = get_global_id(0); dest[i] = fmax(src[i], 0); }";
 const char *RegressionSrc =
     "__kernel void op(__global float* dest, __global float* src)"
-    "{ size_t i = get_global_id(0); output[i] = input[i]; }";
+    "{ size_t i = get_global_id(0); dest[i] = src[i]; }";
 
 using Kind = Kinded::Kind;
 using kernelSrcEnum = struct {
@@ -27,6 +27,24 @@ using kernelSrcEnum = struct {
 };
 kernelSrcEnum shaders[] = {{Kind::ReluInstKind, ReluSrc},
                            {Kind::RegressionInstKind, RegressionSrc}};
+
+static void dumpCompileLog(cl_device_id dev, cl_program prog) {
+#ifndef NDEBUG
+  // Determine the size of the log.
+  size_t log_size;
+  clGetProgramBuildInfo(prog, dev, CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
+
+  // Allocate memory for the log.
+  char *log = (char *)malloc(log_size);
+
+  // Get the log.
+  clGetProgramBuildInfo(prog, dev, CL_PROGRAM_BUILD_LOG, log_size, log, NULL);
+
+  // Print the log.
+  std::cout << log << "\n";
+  free(log);
+#endif
+}
 
 using namespace glow;
 OCLBackend::OCLBackend(Module *M) : M_(M) {
@@ -40,12 +58,16 @@ OCLBackend::OCLBackend(Module *M) : M_(M) {
   commands_ = clCreateCommandQueue(context_, deviceId_, 0, &err);
   GLOW_ASSERT(commands_ && "clCreateCommandQueue Failed.");
 
+  // Compile all of the shaders:
   for (auto SH : shaders) {
     err = CL_SUCCESS;
     cl_program program =
         clCreateProgramWithSource(context_, 1, &SH.src, NULL, &err);
     GLOW_ASSERT(program && "clCreateProgramWithSource Failed.");
     err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+    if (err) {
+      dumpCompileLog(deviceId_, program);
+    }
     GLOW_ASSERT(err == CL_SUCCESS && "clBuildProgram Failed.");
     assert(!programs_.count(SH.kind) && "Opcode already registered");
     programs_[SH.kind] = program;
@@ -64,14 +86,69 @@ OCLBackend::~OCLBackend() {
   clear();
 }
 
-void OCLBackend::doForwardPass(bool isTrain) {}
+void OCLBackend::doForwardPass(bool isTrain) {
+  copyWeightsToDevice();
+
+  copyWeightsFromDevice();
+}
+
+void OCLBackend::copyWeightsToDevice() {
+  for (auto it : tensors_) {
+    assert(externalTensors_.count(it.first) && "Unknown weight!");
+    Tensor *T = externalTensors_[it.first];
+    size_t sizeInBytes = T->getType().getSizeInBytes();
+    // Issue a non-blocking command to copy the buffer to the device.
+    cl_int err =
+        clEnqueueWriteBuffer(commands_, it.second, CL_FALSE, 0, sizeInBytes,
+                             T->getUnsafePtr(), 0, nullptr, nullptr);
+    GLOW_ASSERT(err == CL_SUCCESS && "Unable to copy data to the device");
+  }
+  // Do it!
+  clFlush(commands_);
+}
+
+void OCLBackend::copyWeightsFromDevice() {
+  for (auto it : tensors_) {
+    assert(externalTensors_.count(it.first) && "Unknown weight!");
+    Tensor *T = externalTensors_[it.first];
+    size_t sizeInBytes = T->getType().getSizeInBytes();
+    // Issue a non-blocking command to copy the buffer from the device.
+    cl_int err =
+        clEnqueueReadBuffer(commands_, it.second, CL_FALSE, 0, sizeInBytes,
+                            T->getUnsafePtr(), 0, nullptr, nullptr);
+    GLOW_ASSERT(err == CL_SUCCESS && "Unable to copy data to the device");
+  }
+  // Do it!
+  clFlush(commands_);
+}
 
 void OCLBackend::registerGraphTensor(const Value *v, Tensor *t) {
   assert(!externalTensors_.count(v) && "The tensor is already registered");
   externalTensors_[v] = t;
 }
 
-void OCLBackend::init() {}
+void OCLBackend::init() {
+  // Copy the weights into the device.
+
+  // For each weight:
+  for (auto it : externalTensors_) {
+    Tensor *T = it.second;
+    size_t sizeInBytes = T->getType().getSizeInBytes();
+
+    // Allocate a new buffer, unless it was allocated in previous compilations.
+    cl_mem buff;
+    auto tt = tensors_.find(it.first);
+    if (tt == tensors_.end()) {
+      buff = clCreateBuffer(context_, CL_MEM_READ_WRITE, sizeInBytes, nullptr,
+                            nullptr);
+    } else {
+      buff = tt->second;
+    }
+    GLOW_ASSERT(buff && "Allocation failed!");
+    // Associate the new buffer with the weight value.
+    tensors_[it.first] = buff;
+  }
+}
 
 void OCLBackend::clear() { externalTensors_.clear(); }
 
