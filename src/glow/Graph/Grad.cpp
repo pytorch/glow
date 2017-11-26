c46c90f96ee7e1819249c68a72396382067543d9
Nadav Rotem <nrotem@fb.com>
2017-11-25 21:16:07 -0800

    AutoGrad: Implement the code that constructs the training graph. This works for most of the nodes, but not all. The commit does not include IRGen or removal of the old code. This code is tested manually using the graph dump methods.

---
+++ src/glow/Graph/Grad.cpp
@@ -0,0 +1,121 @@
+// Copyright 2017 Facebook Inc.  All Rights Reserved.
+
+#include "glow/Base/Train.h"
+#include "glow/Graph/Graph.h"
+#include "glow/Graph/Nodes.h"
+#include "glow/IR/IR.h"
+#include "glow/Support/Support.h"
+
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/Support/Casting.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace glow;
+
+using llvm::cast;
+
+void glow::generateGradientNodes(Graph &G, TrainingConfig &conf) {
+  using Kind = glow::Kinded::Kind;
+  UnownedNodeValueMap map;
+
+  // A list of nodes to add to the graph.
+  std::vector<Node *> toAppend;
+
+  // Generate the gradient nodes for each one of the nodes in the module.
+  auto &nodes = G.getNodes();
+  for (auto it = nodes.rbegin(), e = nodes.rend(); it != e; it++) {
+    Node *N = *it;
+
+#define CONVERT_TO_GRAD_NODE(NodeKind)                                         \
+  if (N->getKind() == Kind::NodeKind##Kind) {                                  \
+    toAppend.push_back(cast<NodeKind>(N)->getGrad(map));                       \
+    continue;                                                                  \
+  }
+
+    CONVERT_TO_GRAD_NODE(ConvolutionNode)
+    CONVERT_TO_GRAD_NODE(PoolNode)
+    CONVERT_TO_GRAD_NODE(FullyConnectedNode)
+    CONVERT_TO_GRAD_NODE(BatchNormalizationNode)
+    CONVERT_TO_GRAD_NODE(LocalResponseNormalizationNode)
+    CONVERT_TO_GRAD_NODE(SoftMaxNode)
+    CONVERT_TO_GRAD_NODE(RegressionNode)
+    CONVERT_TO_GRAD_NODE(ArithmeticNode)
+    CONVERT_TO_GRAD_NODE(ReluNode)
+    CONVERT_TO_GRAD_NODE(SigmoidNode)
+    CONVERT_TO_GRAD_NODE(TanhNode)
+    CONVERT_TO_GRAD_NODE(ConvolutionNode)
+
+    if (N->getKind() == Kind::SaveNodeKind) {
+      // Swap the src and dest.
+      auto *X =
+          new ZeroNode(N->getName(), cast<SaveNode>(N)->getInput()->getType());
+      toAppend.push_back(X);
+      map.insert(cast<SaveNode>(N)->getInput(), X);
+      map.insert(cast<SaveNode>(N)->getVariable(), X);
+      continue;
+    }
+
+    if (N->getKind() == Kind::ReshapeNodeKind) {
+      ReshapeNode *RN = cast<ReshapeNode>(N);
+      NodeValue outputG = map.get(RN->getResult());
+      NodeValue inputW = RN->getInput();
+
+      // Swap the src and dest.
+      auto *X = new ReshapeNode(N->getName(), inputW->getType(), outputG,
+                                inputW->getType()->dims());
+      toAppend.push_back(X);
+      map.insert(RN->getResult(), X);
+      break;
+    }
+
+    if (N->getKind() == Kind::TransposeNodeKind) {
+      TransposeNode *TN = cast<TransposeNode>(N);
+      NodeValue outputG = map.get(TN->getResult());
+      NodeValue inputW = TN->getInput();
+
+      // Generate the reverse shuffle.
+      auto shuffle = TN->getShuffle();
+      std::vector<unsigned> reverseShuffle(shuffle.begin(), shuffle.end());
+      for (unsigned int i = 0; i < shuffle.size(); i++) {
+        reverseShuffle[shuffle[i]] = i;
+      }
+
+      // Swap the src and dest.
+      auto *X = new TransposeNode(N->getName(), inputW->getType(), outputG,
+                                  reverseShuffle);
+      map.insert(TN->getResult(), X);
+      break;
+    }
+
+    if (N->getKind() == Kind::ConcatNodeKind) {
+      auto *CC = cast<ConcatNode>(N);
+      auto inputs = CC->getInputs();
+
+      // We start extracting the shape at (0,0, ... ).
+      std::vector<size_t> offsets(CC->dims().size(), 0);
+      unsigned dim = CC->getDim();
+      for (auto &N : inputs) {
+        auto *X = new SliceNode("extract", N.getType(), N, offsets);
+        // We are stacking the tensors along a specific dimension. This means
+        // that we increase the size of the tensor along this dimension.
+        offsets[dim] += N.dims()[dim];
+        map.insert(N, X);
+      }
+      break;
+    }
+
+    assert(false);
+    glow_unreachable();
+  } // End of the for-each instr loop.
+
+  for (auto &V : G.getVars()) {
+    auto X = new SGDNode(V->getName(), map.get(V), V, conf.L1Decay,
+                         conf.L2Decay, conf.learningRate, conf.momentum);
+    toAppend.push_back(X);
+  }
+
+  // Add all of the new instructions.
+  for (auto &I : toAppend) {
+    nodes.push_back(I);
+  }
+}
---
+++ tests/unittests/GraphAutoGrad.cpp
@@ -0,0 +1,53 @@
+// Copyright 2017 Facebook Inc.  All Rights Reserved.
+
+#include "glow/ExecutionEngine/ExecutionEngine.h"
+#include "glow/Graph/Graph.h"
+#include "glow/IR/IR.h"
+#include "glow/IR/IRBuilder.h"
+#include "glow/IR/Instrs.h"
+
+#include "gtest/gtest.h"
+
+#include <cassert>
+#include <string>
+
+using namespace glow;
+
+TEST(GraphAutoGrad, autoGrad) {
+  ExecutionEngine EE;
+
+  // Construct the network:
+  EE.getConfig().learningRate = 0.001;
+  EE.getConfig().momentum = 0.9;
+  EE.getConfig().L2Decay = 0.001;
+
+  auto &G = EE.getGraph();
+
+  Variable *A = G.createVariable(ElemKind::FloatTy, {10, 28, 28, 1}, "input",
+                                 Variable::InitKind::Extern);
+
+  auto *CV0 = G.createConv("conv", A, 16, 5, 1, 2);
+  auto *RL0 = G.createRELU("relu", CV0);
+  auto *MP0 = G.createPool("pool", RL0, PoolNode::Mode::Max, 3, 3, 0);
+
+  auto *CV1 = G.createConv("conv", MP0, 16, 5, 1, 2);
+  auto *RL1 = G.createRELU("conv", CV1);
+  auto *MP1 = G.createPool("pool", RL1, PoolNode::Mode::Max, 3, 3, 0);
+
+  auto *FCL1 = G.createFullyConnected("fc", MP1, 10);
+  auto *RL2 = G.createRELU("fc", FCL1);
+  Variable *selected = G.createVariable(ElemKind::IndexTy, {10, 1}, +"selected",
+                                        Variable::InitKind::Extern);
+
+  auto *SM = G.createSoftMax("sm", RL2, selected);
+
+  auto *result = G.createSave("return", SM);
+  (void)result;
+
+  generateGradientNodes(G, EE.getConfig());
+
+  G.dump();
+  G.dumpDAG();
+
+  EE.compile(CompilationMode::Train);
+}
