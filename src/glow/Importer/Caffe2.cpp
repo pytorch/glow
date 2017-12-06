257ee8f49316a6b26619df6650a99f81e8c87e29
artemrakhov <artem@fb.com>
2017-12-05 18:42:58 -0800

    Support LRN operator

--- src/glow/Importer/Caffe2.cpp
+++ src/glow/Importer/Caffe2.cpp
@@ -69,7 +69,7 @@ std::vector<size_t> getShape(const ::caffe2::Argument *arg) {
 }
 
 /// Translates the protocol buffer node \p op into a random access map.
-static ArgumentDictionaryTy loadArgumenrMap(const caffe2::OperatorDef &op) {
+static ArgumentDictionaryTy loadArgumentMap(const caffe2::OperatorDef &op) {
   ArgumentDictionaryTy dict;
   for (auto i = 0, e = op.arg_size(); i < e; i++) {
     const caffe2::Argument &arg = op.arg(i);
@@ -135,7 +135,7 @@ bool caffe2ModelLoader::hasNodeByName(const std::string &name) {
 void caffe2ModelLoader::loadOperator(const caffe2::OperatorDef &op) {
   auto &G = EE_.getGraph();
 
-  ArgumentDictionaryTy dict = loadArgumenrMap(op);
+  ArgumentDictionaryTy dict = loadArgumentMap(op);
 
   const std::string &typeName = op.type();
 
@@ -344,6 +344,23 @@ void caffe2ModelLoader::loadOperator(const caffe2::OperatorDef &op) {
     }
     return;
   }
+  if (typeName == "LRN") {
+    auto *in = getOrCreateNodeByName(op.input(0));
+
+    size_t halfWindowSize = loadInt(dict["size"]);
+    float alpha = loadFloat(dict["alpha"]);
+    float beta = loadFloat(dict["beta"]);
+    float k = loadFloat(dict["bias"]);
+
+    auto *node = G.createLocalResponseNormalization(
+        op.name(), in, halfWindowSize, alpha, beta, k);
+
+    // Save the outputs:
+    for (int i = 0, e = op.output_size(); i < e; i++) {
+      nodeByName_[op.output(i)] = node;
+    }
+    return;
+  }
 
   unexpectedNodeError(op, "Could not load the operator.");
 }
@@ -361,7 +378,7 @@ void caffe2ModelLoader::loadNetwork(caffe2::NetDef &net) {
 
 void caffe2ModelLoader::loadWeights(caffe2::NetDef &net) {
   for (auto &op : net.op()) {
-    ArgumentDictionaryTy dict = loadArgumenrMap(op);
+    ArgumentDictionaryTy dict = loadArgumentMap(op);
 
     /// Load tensors with values:
     if (op.type() == "GivenTensorFill") {
