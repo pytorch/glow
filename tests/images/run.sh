e0bf7745cab41c1cfafadab603c18b0a4cec6b15
artemrakhov <artem@fb.com>
2017-12-10 18:57:32 -0800

    Change VGG16 input format from 128to127 to 0to256

--- tests/images/run.sh
+++ tests/images/run.sh
@@ -4,6 +4,6 @@ for png_filename in tests/images/*.png; do
   ./bin/loader $png_filename 0to1 resnet50/predict_net.pb resnet50/init_net.pb
   ./bin/loader $png_filename 128to127 vgg19/predict_net.pb vgg19/init_net.pb
   ./bin/loader $png_filename 128to127 squeezenet/predict_net.pb squeezenet/init_net.pb
-  ./bin/loader $png_filename 128to127 vgg16/predict_net.pb vgg16/init_net.pb
+  ./bin/loader $png_filename 0to256 vgg16/predict_net.pb vgg16/init_net.pb
 done
 
