0fa23c158298762cb55bb63e096897ec2f7f0f33
Adam Weis <aweis@fb.com>
2017-12-13 11:44:29 -0800

    run.sh: fix up this script to use the -d flag on loader
    This was part of Nadav's feedback

--- tests/images/run.sh
+++ tests/images/run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 for png_filename in tests/images/*.png; do
-  ./bin/loader $png_filename -image_mode=0to1 -n=resnet50/predict_net.pb -w=resnet50/init_net.pb
-  ./bin/loader $png_filename -image_mode=128to127 -n=vgg19/predict_net.pb -w=vgg19/init_net.pb
-  ./bin/loader $png_filename -image_mode=128to127 -n=squeezenet/predict_net.pb -w=squeezenet/init_net.pb
-  ./bin/loader $png_filename -image_mode=0to256 -n=vgg16/predict_net.pb -w=vgg16/init_net.pb
+  ./bin/loader $png_filename -image_mode=0to1 -d=resnet50
+  ./bin/loader $png_filename -image_mode=128to127 -d=vgg19
+  ./bin/loader $png_filename -image_mode=128to127 -d=squeezenet
+  ./bin/loader $png_filename -image_mode=0to256 -d=vgg16
 done
 
