64aed490154dfa6d1c97329ae7b84f4a1b00ce37
Roman Levenstein <rlevenstein@fb.com>
2017-12-18 21:14:49 -0800

    Add div in lower instr.

--- src/glow/Optimizer/Lower.cpp
+++ src/glow/Optimizer/Lower.cpp
@@ -8,76 +8,106 @@
 #include "llvm/Support/Casting.h"
 
 using namespace glow;
-using llvm::cast;
 using llvm::dyn_cast;
+
+void lowerArithmeticNode(Graph &graph, ArithmeticGradNode &node) {
+  switch (node.getMode()) {
+  case ArithmeticGradNode::Mode::Add: {
+    /// The chain rule for addition:
+    /// LHS' = OUT'
+    /// RHS' = OUT'
+    auto outG = node.getGradOfOriginalOutputNamedResult();
+    node.getGradOfInputNamedLHS().replaceAllUsesOfWith(outG);
+    node.getGradOfInputNamedRHS().replaceAllUsesOfWith(outG);
+    break;
+  }
+
+  case ArithmeticGradNode::Mode::Mul: {
+    /// The chain rule for multiplication:
+    /// LHS' = RHS * OUT'
+    /// RHS' = LHS * OUT'
+    auto outG = node.getGradOfOriginalOutputNamedResult();
+    NodeValue LHS = node.getLHS();
+    NodeValue RHS = node.getRHS();
+
+    auto lhsResult = graph.createArithmetic("mul.grad.rhs", outG, RHS,
+                                            ArithmeticNode::Mode::Mul);
+    auto rhsResult = graph.createArithmetic("mul.grad.lhs", outG, LHS,
+                                            ArithmeticNode::Mode::Mul);
+    node.getGradOfInputNamedLHS().replaceAllUsesOfWith(lhsResult);
+    node.getGradOfInputNamedRHS().replaceAllUsesOfWith(rhsResult);
+    break;
+  }
+
+  case ArithmeticGradNode::Mode::Sub: {
+    /// The chain rule for subtraction:
+    /// LHS' = OUT'
+    /// RHS' = -OUT'
+    auto outG = node.getGradOfOriginalOutputNamedResult();
+    auto zero = graph.createZero("zero", outG.getType());
+    auto sub = graph.createArithmetic("sub.grad", zero, outG,
+                                      ArithmeticNode::Mode::Sub);
+    node.getGradOfInputNamedLHS().replaceAllUsesOfWith(outG);
+    node.getGradOfInputNamedRHS().replaceAllUsesOfWith(sub);
+    break;
+  }
+
+  case ArithmeticGradNode::Mode::Div: {
+    /// The chain rule for division:
+    /// LHS' = OUT' / RHS
+    /// RHS' = - LHS * OUT' / (RHS * RHS)
+    auto outG = node.getGradOfOriginalOutputNamedResult();
+    NodeValue LHS = node.getLHS();
+    NodeValue RHS = node.getRHS();
+
+    auto lhsResult = graph.createArithmetic("div.grad.rhs", outG, RHS,
+                                            ArithmeticNode::Mode::Div);
+
+    auto zero = graph.createZero("zero", outG.getType());
+    auto subGrad = graph.createArithmetic("sub.grad", zero, outG,
+                                          ArithmeticNode::Mode::Sub);
+    auto mulLhsGrad = graph.createArithmetic("mul.sub.grad.lhs", subGrad, LHS,
+                                             ArithmeticNode::Mode::Mul);
+
+    auto squareRhs = graph.createArithmetic("square.rhs", RHS, RHS,
+                                            ArithmeticNode::Mode::Mul);
+    auto rhsResult = graph.createArithmetic("div.grad", mulLhsGrad, squareRhs,
+                                            ArithmeticNode::Mode::Div);
+
+    node.getGradOfInputNamedLHS().replaceAllUsesOfWith(lhsResult);
+    node.getGradOfInputNamedRHS().replaceAllUsesOfWith(rhsResult);
+    break;
+  }
+  }
+}
+
+void lowerRegressionNode(RegressionNode &node) {
+  auto outG = node.getInput();
+  node.getResult().replaceAllUsesOfWith(outG);
+}
+
+void lowerRegressionGradNode(Graph &graph, RegressionGradNode &node) {
+  auto outG = node.getInput();
+
+  auto inputG =
+      graph.createArithmetic("rgn.grad", node.getInput(), node.getExpected(),
+                             ArithmeticNode::Mode::Sub);
+  auto expG = graph.createZero("exp.grad", node.getExpected().getType());
+
+  node.getGradOfInputNamedInput().replaceAllUsesOfWith(inputG);
+  node.getGradOfInputNamedExpected().replaceAllUsesOfWith(expG);
+}
 
 void glow::lower(Graph &G, CompilationMode mode) {
   auto &nodes = G.getNodes();
 
-  // For each node:
   for (auto const &node : nodes) {
-    // Lower the RegressionNode node:
     if (auto *RN = dyn_cast<RegressionNode>(node)) {
-      auto outG = RN->getInput();
-      RN->getResult().replaceAllUsesOfWith(outG);
-      continue;
+      lowerRegressionNode(*RN);
+    } else if (auto *RGN = dyn_cast<RegressionGradNode>(node)) {
+      lowerRegressionGradNode(G, *RGN);
+    } else if (auto *EMG = dyn_cast<ArithmeticGradNode>(node)) {
+      lowerArithmeticNode(G, *EMG);
     }
-    // Lower the RegressionGradNode node:
-    if (auto *RGN = dyn_cast<RegressionGradNode>(node)) {
-      auto outG = RGN->getInput();
-
-      auto inputG =
-          G.createArithmetic("rgn.grad", RGN->getInput(), RGN->getExpected(),
-                             ArithmeticNode::Mode::Sub);
-      auto expG = G.createSplat("exp.grad", RGN->getExpected().getType(), 0);
-
-      RGN->getGradOfInputNamedInput().replaceAllUsesOfWith(inputG);
-      RGN->getGradOfInputNamedExpected().replaceAllUsesOfWith(expG);
-      continue;
-    }
-
-    // Lower the ArithmeticGradNode node:
-    if (auto *EMG = dyn_cast<ArithmeticGradNode>(node)) {
-      switch (EMG->getMode()) {
-      case ArithmeticGradNode::Mode::Add: {
-        /// The chain rule for Addition:
-        /// LHS' = OUT'
-        /// RHS' = OUT'
-        auto outG = EMG->getGradOfOriginalOutputNamedResult();
-        EMG->getGradOfInputNamedLHS().replaceAllUsesOfWith(outG);
-        EMG->getGradOfInputNamedRHS().replaceAllUsesOfWith(outG);
-        continue;
-      }
-
-      case ArithmeticGradNode::Mode::Mul: {
-        auto outG = EMG->getGradOfOriginalOutputNamedResult();
-        NodeValue LHS = EMG->getLHS();
-        NodeValue RHS = EMG->getRHS();
-        /// The chain rule for multiplication:
-        /// LHS' = RHS' * OUT'
-        /// RHS' = LHS' * OUT'
-        auto LHS1 = G.createArithmetic("mul.grad.lhs", RHS, outG,
-                                       ArithmeticNode::Mode::Mul);
-        auto RHS1 = G.createArithmetic("mul.grad.rhs", LHS, outG,
-                                       ArithmeticNode::Mode::Mul);
-        EMG->getGradOfInputNamedLHS().replaceAllUsesOfWith(LHS1);
-        EMG->getGradOfInputNamedRHS().replaceAllUsesOfWith(RHS1);
-        continue;
-      }
-
-      case ArithmeticGradNode::Mode::Sub: {
-        /// The chain rule for subtraction:
-        /// LHS' =  OUT'
-        /// RHS' =  -OUT'
-        auto outG = EMG->getGradOfOriginalOutputNamedResult();
-        EMG->getGradOfInputNamedLHS().replaceAllUsesOfWith(outG);
-        auto zero = G.createSplat("zero", outG.getType(), 0);
-        auto sub = G.createArithmetic("sub.grad", zero, outG,
-                                      ArithmeticNode::Mode::Sub);
-        EMG->getGradOfInputNamedRHS().replaceAllUsesOfWith(sub);
-        continue;
-      }
-      }
-    } // Arithmetic Grad.
-  }   // For all nodes.
+  }
 }
