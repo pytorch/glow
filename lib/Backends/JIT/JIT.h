e558da739651a4d6115a22ac22bd3e67810c8646
Nadav Rotem <nrotem@fb.com>
2018-01-08 10:05:27 -0800

    [JIT] Add a simple IRBuilder that points to an empty basic block.

--- lib/Backends/JIT/JIT.h
+++ lib/Backends/JIT/JIT.h
@@ -5,6 +5,9 @@
 #include "glow/Base/Tensor.h"
 
 #include "llvm/ADT/ArrayRef.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Module.h"
 
 namespace glow {
 
@@ -15,8 +18,15 @@ class Tensor;
 class Variable;
 
 class JITBackend final : public Backend {
-  /// The Module that holds the IR. This does not own the module.
+  /// The Module that holds the glow IR. This does not own the module.
   Module *M_;
+  /// The LLVM context.
+  llvm::LLVMContext ctx_;
+  /// The LLVM IR module.
+  std::unique_ptr<llvm::Module> llmodule_{nullptr};
+  /// Points to the main function in the jitted code. The function is owned by
+  /// the LLVM module.
+  llvm::Function *func_{nullptr};
 
 public:
   /// Ctor.
