5e75e9a062fd4078b0039b532cc7d2a7017fead9
Nadav Rotem <nrotem@fb.com>
2018-01-08 22:29:01 -0800

    [JIT] teach the JIT how to load the JIT byte code library from disk.

--- lib/Backends/JIT/JIT.cpp
+++ lib/Backends/JIT/JIT.cpp
@@ -7,7 +7,11 @@
 #include "glow/IR/Instrs.h"
 #include "glow/Optimizer/Optimizer.h"
 
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IRReader/IRReader.h"
 #include "llvm/Support/Casting.h"
+#include "llvm/Support/SourceMgr.h"
 
 using namespace glow;
 using llvm::isa;
@@ -17,14 +21,19 @@ JITBackend::~JITBackend() { clear(); }
 void JITBackend::clear() { M_->clear(); }
 
 void JITBackend::init() {
-  llmodule_.reset(new llvm::Module("program", ctx_));
+  llvm::SMDiagnostic Err;
+  // Load the jit library as a new module.
+  llmodule_ = llvm::parseIRFile("libjit.bc", Err, ctx_);
+  GLOW_ASSERT(llmodule_.get() && "Unable to load the JIT library.");
 
+  // Create the 'main' function into the LLVM module.
   llvm::Type *void_type = llvm::Type::getVoidTy(ctx_);
   llvm::FunctionType *jit_func_type =
       llvm::FunctionType::get(void_type, {}, false);
   func_ = llvm::Function::Create(jit_func_type, llvm::Function::ExternalLinkage,
                                  "main", llmodule_.get());
 
+  // Setup the entry basic block and initialize the IR builder.
   llvm::BasicBlock *entry_bb = llvm::BasicBlock::Create(ctx_, "entry", func_);
   llvm::IRBuilder<> builder(entry_bb);
 }
