25501799c02a09fd4a1c93903266bec1e664c127
Nadav Rotem <nrotem@fb.com>
2018-01-09 23:34:40 -0800

    [JIT] Add two methods to the JIT library - splat and elementmax. These methods are needed for the calculation of the RELU operator.

--- lib/Backends/JIT/libjit.c
+++ lib/Backends/JIT/libjit.c
@@ -1,2 +1,17 @@
-// A placeholder for future functions that implement the JIT standard library.
-int empty() { return 0; }
+#include <stddef.h>
+#include <stdint.h>
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+void splat_f(uint8_t *buffer, size_t sz, float val) {
+  for (size_t i = 0; i < sz; i++) {
+    ((float *)buffer)[i] = val;
+  }
+}
+
+void elementmax_f(uint8_t *dest, uint8_t *LHS, uint8_t *RHS, size_t sz) {
+  for (size_t i = 0; i < sz; i++) {
+    ((float *)dest)[i] = MAX(((float *)LHS)[i], ((float *)RHS)[i]);
+  }
+}
