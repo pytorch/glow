ac94dd67c9a2d8c03dd22dbe13c34cef2e6e01d3
Nadav Rotem <nrotem@fb.com>
2018-01-11 23:45:08 -0800

    [JIT] Emit direct calls to floating point operands instead of bit-casting to i8*.

--- lib/Backends/JIT/libjit.c
+++ lib/Backends/JIT/libjit.c
@@ -18,23 +18,20 @@ size_t getXYZ(size_t *dims, size_t x, size_t y, size_t z) {
 /// \returns the index of the element at x,y.
 size_t getXY(size_t *dims, size_t x, size_t y) { return (x * dims[1]) + y; }
 
-void splat_f(uint8_t *buffer, size_t sz, float val) {
+void splat_f(float *buffer, size_t sz, float val) {
   for (size_t i = 0; i < sz; i++) {
     ((float *)buffer)[i] = val;
   }
 }
 
-void elementmax_f(uint8_t *dest, uint8_t *LHS, uint8_t *RHS, size_t sz) {
+void elementmax_f(float *dest, float *LHS, float *RHS, size_t sz) {
   for (size_t i = 0; i < sz; i++) {
-    ((float *)dest)[i] = MAX(((float *)LHS)[i], ((float *)RHS)[i]);
+    dest[i] = MAX(LHS[i], RHS[i]);
   }
 }
 
-void batchedmatmul_f(uint8_t *dest, uint8_t *LHS, uint8_t *RHS,
+void batchedmatmul_f(float *dest, float *LHS, float *RHS,
                      size_t *destDims, size_t *lhsDims, size_t *rhsDims) {
-  float *destF = (float *)dest;
-  float *LHSF = (float *)LHS;
-  float *RHSF = (float *)RHS;
   // For each layer in the batch:
   for (size_t n = 0; n < destDims[0]; n++) {
     // Broadcast tensors with a batch size of 1 by selecting the right slice.
@@ -49,25 +46,22 @@ void batchedmatmul_f(uint8_t *dest, uint8_t *LHS, uint8_t *RHS,
         float sum = 0;
         for (size_t i = 0; i < rhsDims[2]; i++) {
           sum +=
-              LHSF[getXYZ(lhsDims, ln, i, x)] * RHSF[getXYZ(rhsDims, rn, y, i)];
+              LHS[getXYZ(lhsDims, ln, i, x)] * RHS[getXYZ(rhsDims, rn, y, i)];
         }
-        destF[getXYZ(destDims, n, x, y)] = sum;
+        dest[getXYZ(destDims, n, x, y)] = sum;
       }
     }
   } // N
 }
 
-void batchedadd_f(uint8_t *dest, uint8_t *batch, uint8_t *slice,
+void batchedadd_f(float *dest, float *batch, float *slice,
                   size_t numSlice, size_t sliceSize) {
-  float *destF = (float *)dest;
-  float *sliceF = (float *)slice;
-  float *batchF = (float *)batch;
   // For each layer in the batch:
   for (size_t n = 0; n < numSlice; n++) {
     size_t base = n * sliceSize;
     // For each element in the slice.
     for (size_t i = 0; i < sliceSize; i++) {
-      destF[base + i] = batchF[base + i] + sliceF[i];
+      dest[base + i] = batch[base + i] + slice[i];
     }
   }
 }
