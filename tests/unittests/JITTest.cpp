6cf051e237907048934fdd714ddea4fb83b7baed
Nadav Rotem <nrotem@fb.com>
2018-01-17 12:53:36 -0800

    Add a unittest to test the JIT.

---
+++ tests/unittests/JITTest.cpp
@@ -0,0 +1,72 @@
+// Copyright 2017 Facebook Inc.  All Rights Reserved.
+
+#include "BackendTestUtils.h"
+
+#include "glow/ExecutionEngine/ExecutionEngine.h"
+#include "glow/Graph/Graph.h"
+#include "glow/IR/IR.h"
+#include "glow/IR/IRBuilder.h"
+#include "glow/IR/Instrs.h"
+
+#include "gtest/gtest.h"
+
+#include <cassert>
+#include <string>
+
+using namespace glow;
+using llvm::cast;
+
+TEST(JITCorrectnessTest, reluTest) {
+  Tensor inputs(ElemKind::FloatTy, {2, 16});
+  inputs.getHandle().randomize(1);
+  Tensor out1;
+  Tensor out2;
+
+  inferReluNet(&inputs, &out1, BackendKind::JIT);
+  inferReluNet(&inputs, &out2, BackendKind::Interpreter);
+  auto H1 = out1.getHandle();
+  auto H2 = out2.getHandle();
+
+  EXPECT_TRUE(H1.isEqual(H2));
+  if (!H1.isEqual(H2)) {
+    H1.dump();
+    H2.dump();
+  }
+}
+
+
+TEST(JITCorrectnessTest, convOps) {
+  Tensor inputs(ElemKind::FloatTy, {2, 3, 16, 16});
+  inputs.getHandle().randomize(1);
+  Tensor out1;
+  Tensor out2;
+
+  inferBasicConvNet(&inputs, &out1, BackendKind::JIT);
+  inferBasicConvNet(&inputs, &out2, BackendKind::Interpreter);
+  auto H1 = out1.getHandle();
+  auto H2 = out2.getHandle();
+
+  EXPECT_TRUE(H1.isEqual(H2));
+  if (!H1.isEqual(H2)) {
+    H1.dump();
+    H2.dump();
+  }
+}
+
+TEST(JITCorrectnessTest, basicFCNet) {
+  Tensor inputs(ElemKind::FloatTy, {2, 3, 16, 16});
+  inputs.getHandle().randomize(1);
+  Tensor out1;
+  Tensor out2;
+
+  inferBasicFCNet(&inputs, &out1, BackendKind::JIT);
+  inferBasicFCNet(&inputs, &out2, BackendKind::Interpreter);
+  auto H1 = out1.getHandle();
+  auto H2 = out2.getHandle();
+
+  EXPECT_TRUE(H1.isEqual(H2));
+  if (!H1.isEqual(H2)) {
+    H1.dump();
+    H2.dump();
+  }
+}
