ffe19578af82687221f80a8af26417b1a34dc6e9
Nadav Rotem <nrotem@fb.com>
2018-01-17 12:07:48 -0800

    [UnitTest] Refactor the OpenCL unit tests. We are going to reuse the networks in the OCL test to test other backends.

---
+++ tests/unittests/BackendTestUtils.h
@@ -0,0 +1,18 @@
+// Copyright 2017 Facebook Inc.  All Rights Reserved.
+
+#include "glow/ExecutionEngine/ExecutionEngine.h"
+#include "glow/Graph/Graph.h"
+#include "glow/IR/IR.h"
+
+void inferReluNet(glow::Tensor *inputs, glow::Tensor *out,
+                  glow::BackendKind kind);
+
+// Test a simple RELU network. 
+void inferBasicConvNet(glow::Tensor *inputs, glow::Tensor *out,
+                       glow::BackendKind kind);
+
+void inferBasicFCNet(glow::Tensor *inputs, glow::Tensor *out,
+                     glow::BackendKind kind);
+
+void inferMixedNet(glow::Tensor *inputs, glow::Tensor *out,
+                   glow::BackendKind kind);
