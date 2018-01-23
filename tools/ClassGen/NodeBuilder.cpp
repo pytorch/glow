f6e826db46d83814f07a7110f7a672f094c967db
James Hegeman <jwhegeman@fb.com>
2018-01-22 18:57:26 -0800

    [AutoGen] Fix formatting in generated Node code

--- tools/ClassGen/NodeBuilder.cpp
+++ tools/ClassGen/NodeBuilder.cpp
@@ -1,20 +1,21 @@
 // Copyright 2017 Facebook Inc.  All Rights Reserved.
+
 #include "NodeBuilder.h"
 
 void NodeBuilder::emitEnumModePrinters(std::ostream &os) const {
-  os << "const char *" << name_ << "Node::getModeStr(" << name_
+  os << "\nconst char *" << name_ << "Node::getModeStr(" << name_
      << "Node::Mode m) {\n";
-  os << "\tstatic const char *names[] = {";
+  os << "  static const char *names[] = {";
   for (const auto &e : enum_) {
     os << "\"" << e << "\", ";
   }
   os << "nullptr};\n";
-  os << "\treturn names[static_cast<int>(m)];\n";
+  os << "  return names[static_cast<int>(m)];\n";
   os << "}\n";
 }
 
 void NodeBuilder::emitCtor(std::ostream &os) const {
-  os << "\t" << name_ << "Node(llvm::StringRef name";
+  os << "  " << name_ << "Node(llvm::StringRef name";
 
   // Constructor non-standard parameter list:
   for (const auto &op : extraParams_) {
@@ -41,7 +42,7 @@ void NodeBuilder::emitCtor(std::ostream &os) const {
   }
 
   // Initialize the base clases:
-  os << "):\n\t Node(Kinded::Kind::" << name_ << "NodeKind, name)";
+  os << ")\n      : Node(Kinded::Kind::" << name_ << "NodeKind, name)";
 
   // Print the initialization list:
   if (!enum_.empty()) {
@@ -55,70 +56,66 @@ void NodeBuilder::emitCtor(std::ostream &os) const {
 
   // Initialize the members:
   for (const auto &op : members_) {
-    os << ", " << op.second << "_(" << op.second << ") ";
+    os << ", " << op.second << "_(" << op.second << ")";
   }
 
   // The constructor body:
   os << " {\n";
   for (auto &RT : nodeOutputs_) {
-    os << "\taddResult(" << RT.first << ");\n";
+    os << "    addResult(" << RT.first << ");\n";
   }
 
   // Instantiate outputs passed in at runtime for the
   // case of intrinsic output.
   if (hasIntrinsicOutput_) {
-    os << "\tfor (const auto& output : intrinsicOutputs) {";
-    os << "\t\taddResult(output);\n";
-    os << "\t}\n\n";
+    os << "    for (const auto& output : intrinsicOutputs) {\n";
+    os << "      addResult(output);\n";
+    os << "    }\n";
   }
 
-  os << "}\n\n";
+  os << "  }\n";
 }
 
 void NodeBuilder::emitClassMembers(std::ostream &os) const {
   // Emit the type of the enum (which is public).
   if (!enum_.empty()) {
-    os << "\tpublic:\n\tenum class Mode {\n";
+    os << " public:\n  enum class Mode {\n";
     for (const auto &E : enum_) {
-      os << "\t  " << E << ",\n";
+      os << "    " << E << ",\n";
     }
-    os << "\t};\n";
-
-    os << "\tprivate:\n";
+    os << "  };\n\n private:\n";
   }
 
   // Emit class members:
   if (!enum_.empty()) {
-    os << "\tMode mode_;\n";
+    os << "  Mode mode_;\n";
   }
   for (const auto &op : nodeInputs_) {
-    os << "\tNodeValue " << op << "_;\n";
+    os << "  NodeValue " << op << "_;\n";
   }
   for (const auto &op : members_) {
-    os << "\t" << getStorageTypename(op.first) << " " << op.second << "_;\n";
+    os << "  " << getStorageTypename(op.first) << " " << op.second << "_;\n";
   }
-
-  os << "\n";
 }
 
 void NodeBuilder::emitMemberGetter(std::ostream &os, MemberType type,
                                    const std::string &name) const {
   // Synthesize the general getter.
   auto returnTypeStr = getReturnTypename(type);
-  os << "\t" << returnTypeStr << " get" << name << "() const { return " << name
+  os << "  " << returnTypeStr << " get" << name << "() const { return " << name
      << "_; }\n";
 }
 
 void NodeBuilder::emitSettersGetters(std::ostream &os) const {
   // Print the getters/setters.
   for (const auto &inName : nodeInputs_) {
-    os << "\tNodeValue get" << inName << "() const { return " << inName
+    os << "  NodeValue get" << inName << "() const { return " << inName
        << "_; }\n";
   }
 
   unsigned idx = 0;
   for (const auto &op : nodeOutputs_) {
-    os << "\tNodeValue get" << op.second << "() { return NodeValue(this,"
+    os << "  NodeValue get" << op.second << "() { return NodeValue(this, "
        << idx++ << "); }\n";
   }
 
@@ -127,121 +124,122 @@ void NodeBuilder::emitSettersGetters(std::ostream &os) const {
   }
 
   // Synthesize the 'classof' method that enables the non-rtti polymorphism.
-  os << "\nstatic bool classof(const Kinded *k) { return k->getKind() == "
-        "Kinded::Kind::"
-     << name_ << "NodeKind; }\n";
+  os << "\n  static bool classof(const Kinded *k) {\n"
+     << "    return k->getKind() == Kinded::Kind::" << name_ << "NodeKind;\n"
+     << "  }\n\n";
 
   if (!enum_.empty()) {
-    os << "\tMode getMode() const { return mode_; }\n";
+    os << "  Mode getMode() const { return mode_; }\n";
   }
 }
 
 void NodeBuilder::emitEdges(std::ostream &os) const {
-  os << "unsigned " << name_ << "Node::getNumInputs() const {\n"
-     << "\t\treturn " << nodeInputs_.size();
+  os << "\nunsigned " << name_ << "Node::getNumInputs() const {\n"
+     << "  return " << nodeInputs_.size();
   for (const auto &op : members_) {
-    if (op.first != MemberType::VectorNodeValue)
+    if (op.first != MemberType::VectorNodeValue) {
       continue;
+    }
     os << " + " << op.second << "_.size()";
   }
   os << ";\n}\n";
 
-  os << "llvm::StringRef " << name_
+  os << "\nllvm::StringRef " << name_
      << "Node::getInputName(unsigned idx) const {\n";
   for (size_t i = 0; i < nodeInputs_.size(); i++) {
-    os << "\t\tif (idx == " << i << ") { return \"" << nodeInputs_[i]
+    os << "  if (idx == " << i << ") { return \"" << nodeInputs_[i]
        << "\"; }\n";
   }
-  os << "\t\tidx -= " << nodeInputs_.size() << ";\n";
+  os << "  idx -= " << nodeInputs_.size() << ";\n";
   for (const auto &op : members_) {
-    if (op.first != MemberType::VectorNodeValue)
+    if (op.first != MemberType::VectorNodeValue) {
       continue;
-    os << "\t\tif (idx < " << op.second << "_.size()) { return \"" << op.second
+    }
+    os << "  if (idx < " << op.second << "_.size()) { return \"" << op.second
        << "\" + std::to_string(idx); }\n"
-       << "\t\tidx -= " << op.second << "_.size();\n";
+       << "  idx -= " << op.second << "_.size();\n";
   }
-  os << "\t\tllvm_unreachable(\"Invalid index\");\n"
-     << "}\n";
+  os << "  llvm_unreachable(\"Invalid index\");\n}\n";
 
-  os << "NodeValue &" << name_ << "Node::getNthInput(unsigned idx) {\n";
+  os << "\nNodeValue &" << name_ << "Node::getNthInput(unsigned idx) {\n";
   for (size_t i = 0; i < nodeInputs_.size(); i++) {
-    os << "\t\tif (idx == " << i << ") { return " << nodeInputs_[i] << "_; }\n";
+    os << "  if (idx == " << i << ") { return " << nodeInputs_[i] << "_; }\n";
   }
-  os << "\t\tidx -= " << nodeInputs_.size() << ";\n";
+  os << "  idx -= " << nodeInputs_.size() << ";\n";
   for (const auto &op : members_) {
-    if (op.first != MemberType::VectorNodeValue)
+    if (op.first != MemberType::VectorNodeValue) {
       continue;
-    os << "\t\tif (idx < " << op.second << "_.size()) { return " << op.second
-       << "_[idx]; }\n"
-       << "\t\tidx -= " << op.second << "_.size();\n";
+    }
+    os << "  if (idx < " << op.second << "_.size()) { return " << op.second
+       << "_[idx]; }\n  idx -= " << op.second << "_.size();\n";
   }
-  os << "\t\tllvm_unreachable(\"Invalid index\");\n"
-     << "}\n";
+  os << "  llvm_unreachable(\"Invalid index\");\n}\n";
 
-  os << "llvm::StringRef " << name_
+  os << "\nllvm::StringRef " << name_
      << "Node::getOutputName(unsigned idx) const {\n";
   for (size_t i = 0; i < nodeOutputs_.size(); i++) {
-    os << "\t\tif (idx == " << i << ") { return \"" << nodeOutputs_[i].second
+    os << "  if (idx == " << i << ") { return \"" << nodeOutputs_[i].second
        << "\"; }\n";
   }
-  os << "\t\tllvm_unreachable(\"Invalid index\");\n"
-     << "}\n";
+  os << "  llvm_unreachable(\"Invalid index\");\n}\n";
 }
 
 void NodeBuilder::emitPrettyPrinter(std::ostream &os) const {
-  os << "std::string " << name_
-     << "Node::getDebugDesc() const {\n\t\tDescriptionBuilder "
-        "db(getKindName());\n\t\tdb.addParam(\"name\", getName())\n";
+  os << "\nstd::string " << name_ << "Node::getDebugDesc() const {\n"
+     << "  DescriptionBuilder db(getKindName());\n"
+     << "  db.addParam(\"name\", getName())\n";
 
   if (!enum_.empty()) {
-    os << "\t\t.addParam(\"Mode\", getModeStr())\n";
+    os << "    .addParam(\"Mode\", getModeStr())\n";
   }
 
   for (const auto &op : nodeInputs_) {
-    os << "\t\t.addParam(\"" << op << "\", *(get" << op << "().getType()))\n";
+    os << "    .addParam(\"" << op << "\", *(get" << op << "().getType()))\n";
   }
 
   for (const auto &mem : members_) {
     // Don't try to print the node operands directly.
-    if (mem.first == MemberType::VectorNodeValue)
+    if (mem.first == MemberType::VectorNodeValue) {
       continue;
+    }
 
-    os << "\t\t.addParam(\"" << mem.second << "\", get" << mem.second
+    os << "    .addParam(\"" << mem.second << "\", get" << mem.second
        << "())\n";
   }
-  os << "\t\t.addParam(\"users\", getNumUsers());\n";
+  os << "    .addParam(\"users\", getNumUsers());\n";
 
   for (const auto &mem : members_) {
-    if (mem.first != MemberType::VectorNodeValue)
+    if (mem.first != MemberType::VectorNodeValue) {
       continue;
+    }
 
-    os << "\t\tfor (auto II : get" << mem.second << "()) { db.addParam(\""
-       << mem.second << "\", *II->getType()); }\n";
+    os << "  for (auto II : get" << mem.second << "()) {\n"
+       << "    db.addParam(\"" << mem.second << "\", *II->getType());\n  }\n";
   }
 
-  os << "\t\treturn db;\n}\n";
+  os << "  return db;\n}\n";
 }
 
 void NodeBuilder::emitEquator(std::ostream &os) const {
-  os << "bool " << name_ << "Node::isEqual(const " << name_
-     << "Node &other) const {\n\treturn true";
+  os << "\nbool " << name_ << "Node::isEqual(const " << name_
+     << "Node &other) const {\n  return true";
 
   if (!enum_.empty()) {
-    os << " &&\n\t getMode() == other.getMode()";
+    os << " &&\n      getMode() == other.getMode()";
   }
 
   for (const auto &op : nodeInputs_) {
-    os << " &&\n\t " << op << "_ == other." << op << "_";
+    os << " &&\n      " << op << "_ == other." << op << "_";
   }
 
   for (const auto &mem : members_) {
-    os << " &&\n\t " << mem.second << "_ == other." << mem.second << "_";
+    os << " &&\n      " << mem.second << "_ == other." << mem.second << "_";
   }
 
   for (int i = 0, e = nodeOutputs_.size(); i < e; i++) {
-    os << " &&\n\t getType(" << i << ") == other.getType(" << i << ")";
+    os << " &&\n      getType(" << i << ") == other.getType(" << i << ")";
   }
-  os << ";\n }\n";
+  os << ";\n}\n";
 }
 
 static bool isVectorType(MemberType ty) {
@@ -250,8 +248,8 @@ static bool isVectorType(MemberType ty) {
 }
 
 void NodeBuilder::emitHasher(std::ostream &os) const {
-  os << "llvm::hash_code " << name_ << "Node::getHash()"
-     << " const {\n\treturn llvm::hash_combine(";
+  os << "\nllvm::hash_code " << name_ << "Node::getHash() const {\n"
+     << "  return llvm::hash_combine(";
 
   if (enum_.empty() && nodeInputs_.empty() && members_.empty()) {
     os << "0);\n }\n";
@@ -260,54 +258,48 @@ void NodeBuilder::emitHasher(std::ostream &os) const {
 
   auto delim = "";
   if (!enum_.empty()) {
-    os << delim << "\n\t getMode()";
+    os << delim << "\n      getMode()";
     delim = ",";
   }
 
   for (const auto &mem : members_) {
     auto ty = mem.first;
     if (ty == MemberType::Float) {
-      os << delim << "\n\t "
-         << "toBinary(" << mem.second << "_"
-         << ")";
+      os << delim << "\n      toBinary(" << mem.second << "_)";
     } else if (isVectorType(ty)) {
-      os << delim << "\n\t "
-         << "llvm::hash_combine_range(" << mem.second << "_"
-         << ".begin(), " << mem.second << "_"
-         << ".end()"
-         << ")";
+      os << delim << "\n      llvm::hash_combine_range(" << mem.second
+         << "_.begin(), " << mem.second << "_.end())";
     } else {
-      os << delim << "\n\t " << mem.second << "_";
+      os << delim << "\n      " << mem.second << "_";
     }
     delim = ",";
   }
 
   for (const auto &op : nodeInputs_) {
-    os << delim << "\n\t " << op << "_";
+    os << delim << "\n      " << op << "_";
     delim = ",";
   }
 
-  os << ");\n }\n";
+  os << ");\n}\n";
 }
 void NodeBuilder::emitVisitor(std::ostream &os) const {
-  os << "void " << name_
-     << "Node::visit(Node *parent, NodeWalker *visitor) {\n\tif "
-        "(!visitor->shouldVisit(parent, this)) { return; }\n";
+  os << "\nvoid " << name_
+     << "Node::visit(Node *parent, NodeWalker *visitor) {\n"
+     << "  if (!visitor->shouldVisit(parent, this)) { return; }\n"
+     << "  visitor->pre(parent, this);\n";
 
-  os << "\tvisitor->pre(parent, this);\n";
   for (const auto &op : nodeInputs_) {
-    os << "\tget" << op << "()->visit(this, visitor);\n";
+    os << "  get" << op << "()->visit(this, visitor);\n";
   }
 
   for (const auto &op : members_) {
     if (op.first == MemberType::VectorNodeValue) {
-      os << "\tfor (auto &I : " << op.second
-         << "_) { I->visit(this, visitor);}\n";
+      os << "  for (auto &I : " << op.second
+         << "_) { I->visit(this, visitor); }\n";
     }
   }
 
-  os << "\tvisitor->post(parent, this);\n";
-  os << "}\n";
+  os << "  visitor->post(parent, this);\n}\n";
 }
 
 void NodeBuilder::emitDocstring(std::ostream &os) const {
@@ -319,7 +311,7 @@ void NodeBuilder::emitDocstring(std::ostream &os) const {
 }
 
 void NodeBuilder::emitNodeClass(std::ostream &os) const {
-  os << "namespace glow {\n";
+  os << "\nnamespace glow {\n";
 
   emitDocstring(os);
 
@@ -327,40 +319,31 @@ void NodeBuilder::emitNodeClass(std::ostream &os) const {
 
   emitClassMembers(os);
 
-  os << "\tpublic:\n";
+  os << "\n public:\n";
 
   emitCtor(os);
-
   emitSettersGetters(os);
 
-  os << "\tunsigned getNumInputs() const;\n";
-
-  os << "\tllvm::StringRef getInputName(unsigned idx) const;\n";
-
-  os << "\tNodeValue &getNthInput(unsigned idx);\n";
-
-  os << "\tllvm::StringRef getOutputName(unsigned idx) const;\n";
-
-  os << "\tbool hasSideEffects() const { return " << hasSideEffects_ << "; }\n";
-
-  os << "\tstd::string getDebugDesc() const;\n";
-
-  os << "\tbool isEqual(const " << name_ << "Node &other) const;\n";
-
-  os << "\tllvm::hash_code getHash() const;\n";
-
-  os << "\tvoid visit(Node *parent, NodeWalker *visitor);\n";
+  os << "  unsigned getNumInputs() const;\n"
+     << "  llvm::StringRef getInputName(unsigned idx) const;\n"
+     << "  NodeValue &getNthInput(unsigned idx);\n"
+     << "  llvm::StringRef getOutputName(unsigned idx) const;\n"
+     << "  bool hasSideEffects() const { return " << hasSideEffects_ << "; }\n"
+     << "  std::string getDebugDesc() const;\n"
+     << "  bool isEqual(const " << name_ << "Node &other) const;\n"
+     << "  llvm::hash_code getHash() const;\n"
+     << "  void visit(Node *parent, NodeWalker *visitor);\n";
 
   if (!enum_.empty()) {
-    os << "\tconst char *getModeStr() const { return getModeStr(mode_); "
-          "}\n\tstatic const char *getModeStr(Mode m);\n";
+    os << "  const char *getModeStr() const { return getModeStr(mode_); }\n"
+       << "  static const char *getModeStr(Mode m);\n";
   }
 
   for (const auto &m : extraMethods_) {
-    os << "\t" << m << "\n";
+    os << "  " << m << "\n";
   }
 
-  os << "};\n\n} // namespace glow\n";
+  os << "};\n} // namespace glow\n";
 }
 
 void NodeBuilder::emitCppMethods(std::ostream &os) const {
@@ -398,12 +381,11 @@ void NodeBuilder::addGradient() {
   // Construct a factory method that builds the new grad node and add
   // it to the current non-grad instruction.
   std::stringstream ss;
-  ss << name_ + "GradNode* getGrad(GraphGradMapper &builder) {\n";
-  ss << "\tauto * x = new " + name_ + "GradNode(getName()";
+  ss << name_ + "GradNode *getGrad(GraphGradMapper &builder) {\n"
+     << "  auto *x = new " + name_ + "GradNode(getName()";
 
   if (enum_.size()) {
-    ss << ", (" << name_ + "GradNode::Mode"
-       << ")getMode()";
+    ss << ", (" << name_ << "GradNode::Mode)getMode()";
   }
 
   // Add the inputs that we'll use in the grad instruction.
@@ -412,8 +394,8 @@ void NodeBuilder::addGradient() {
   }
 
   for (const auto &out : nodeOutputs_) {
-    ss << ", get" << out.second << "()";
-    ss << ", builder.getGradient(get" << out.second << "())";
+    ss << ", get" << out.second << "(), builder.getGradient(get" << out.second
+       << "())";
   }
 
   // Extra class members:
@@ -426,11 +408,10 @@ void NodeBuilder::addGradient() {
   // Register the result of the new node as the gradients of the original node
   // inputs.
   for (const std::string &in : nodeInputs_) {
-    ss << "\tbuilder.addGradient(get" << in << "(), x->getGradOfInputNamed"
-       << in << "());\n";
+    ss << "  builder.addGradient(get" << in
+       << "(), x->getGradOfInputNamed" << in << "());\n";
   }
-  ss << "\treturn x;\n";
-  ss << " }";
+  ss << "  return x;\n}\n";
   addExtraMethod(ss.str());
 }
 
