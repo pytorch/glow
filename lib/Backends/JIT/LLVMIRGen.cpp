92a964d2162a8affd9be6e78c8478a1d7feb3442
Roman Levenstein <rlevenstein@fb.com>
2018-01-30 11:59:31 -0800

    [jit] Support specialization of functions for constant arguments
    This optimization creates specialized functions from general-purpose function provided a set of constant arguments. For example, it would generate specialized versions of convolutions for specific inputs and filter dimensions. Having such specialized functions allows for better optimizations by the LLVM backend, as it has more information about the values of function arguments.
    
    For now, this optimization is only performed for the batched matrix multiplication and convolutions, but it is easy to support any other operations.
    
    There are two command-line flags controlling this optimization:
    `-jit-specialize-dims` - creates specialized functions for operations with constant dimensions. This option is enabled by default.
    `-jit-specialize` - creates specialized functions for operations with constant arguments. This option is disabled by default.

--- lib/Backends/JIT/LLVMIRGen.cpp
+++ lib/Backends/JIT/LLVMIRGen.cpp
@@ -4,10 +4,31 @@
 
 #include "glow/IR/Instrs.h"
 
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/Debug.h"
+
 using namespace glow;
 using llvm::StringRef;
 using llvm::dyn_cast;
 using llvm::isa;
+
+namespace {
+/// Specialize taking into account only dimensions, but not the buffer
+/// addresses.
+static llvm::cl::opt<bool>
+    jitSpecializeDims("jit-specialize-dims",
+                      llvm::cl::desc("Create specialized functions for "
+                                     "operations with constant dimensions"),
+                      llvm::cl::init(true));
+
+/// Specialize taking into account the whole set of arguments, including buffer
+/// addresses.
+static llvm::cl::opt<bool>
+    jitSpecialize("jit-specialize",
+                  llvm::cl::desc("Create specialized functions for operations "
+                                 "with constant arguments"),
+                  llvm::cl::init(false));
+} // namespace
 
 llvm::Value *JITBackend::emitValueAddress(llvm::IRBuilder<> &builder,
                                           glow::Value *val, ElemKind ptrTy) {
@@ -75,6 +96,73 @@ llvm::Function *JITBackend::getFunction(const std::string &name) {
   return llmodule_->getFunction(name);
 }
 
+llvm::Function *JITBackend::getOrCreateSpecializedFunction(
+    llvm::Function *F, llvm::SmallVectorImpl<size_t> &constParams,
+    std::function<void(llvm::IRBuilder<> &builder,
+                       llvm::SmallVectorImpl<llvm::Value *> &args,
+                       bool emitAllArgs)>
+        constArgsEmitter) {
+  // Bail if there is nothing to do.
+  if (!jitSpecialize && !jitSpecializeDims)
+    return F;
+
+  // Produce a name of the specialized function.
+  std::string name;
+  llvm::raw_string_ostream sb{name};
+  sb << F->getName();
+  for (auto param : constParams) {
+    sb << "_" << param;
+  }
+  auto &specializedName = sb.str();
+
+  // Check if a specialization for these parameters exists already.
+  auto *specializedF = llmodule_->getFunction(specializedName);
+  if (specializedF)
+    return specializedF;
+
+  // Create a specialized function.
+  // This function should have exactly the same type as the original function.
+  // It should forward all its parameters.
+  // The specialized function should be marked as noinline, to avoid code bloat.
+  // FF.hasFnAttribute(llvm::Attribute::AttrKind::NoInline);
+  specializedF = dyn_cast<llvm::Function>(
+      llmodule_->getOrInsertFunction(specializedName, F->getFunctionType()));
+  assert(specializedF && "Could not create a specialized function");
+  // Specialization thunks should not be inlined.
+  specializedF->addFnAttr(llvm::Attribute::AttrKind::NoInline);
+
+  // Generate the code to forward the invocation to the original function.
+
+  // Setup the entry basic block and initialize the IR builder.
+  llvm::BasicBlock *entry_bb =
+      llvm::BasicBlock::Create(ctx_, "entry", specializedF);
+  llvm::IRBuilder<> builder(entry_bb);
+  llvm::SmallVector<llvm::Value *, 16> args;
+
+  if (!jitSpecialize) {
+    // Forward all non-const buffers arguments.
+    for (auto &arg : specializedF->args()) {
+      if (!arg.getType()->isPointerTy())
+        break;
+      auto *elementTy = arg.getType()->getPointerElementType();
+      if (!elementTy->isFloatTy())
+        break;
+      args.push_back(&arg);
+    }
+  }
+
+  // Emit the rest of arguments, namely all constant arguments.
+  constArgsEmitter(builder, args, jitSpecialize);
+
+  // Forward all parameters.
+  builder.CreateCall(F, args);
+  builder.CreateRetVoid();
+  DEBUG(llvm::errs() << "\n\nCreated specialized function " << specializedName
+                     << "\n";
+        specializedF->print(llvm::errs(), nullptr));
+  return specializedF;
+}
+
 void JITBackend::generateLLVMIRForInstr(llvm::IRBuilder<> &builder,
                                         glow::Instruction *I) {
   switch (I->getKind()) {
@@ -128,19 +216,57 @@ void JITBackend::generateLLVMIRForInstr(llvm::IRBuilder<> &builder,
 
   case Kinded::Kind::BatchedMatMulInstKind: {
     BatchedMatMulInst *BMM = llvm::cast<BatchedMatMulInst>(I);
-    auto *destPtr =
-        emitValueAddress(builder, BMM->getDest(), ElemKind::FloatTy);
-    auto *LHSPtr = emitValueAddress(builder, BMM->getLHS(), ElemKind::FloatTy);
-    auto *RHSPtr = emitValueAddress(builder, BMM->getRHS(), ElemKind::FloatTy);
+    auto constArgsEmitter = [&](llvm::IRBuilder<> &builder,
+                                llvm::SmallVectorImpl<llvm::Value *> &args,
+                                bool emitAllArgs) {
+      if (emitAllArgs) {
+        // Emit arguments for buffers.
+        auto *destPtr =
+            emitValueAddress(builder, BMM->getDest(), ElemKind::FloatTy);
+        auto *LHSPtr =
+            emitValueAddress(builder, BMM->getLHS(), ElemKind::FloatTy);
+        auto *RHSPtr =
+            emitValueAddress(builder, BMM->getRHS(), ElemKind::FloatTy);
+
+        args.push_back(destPtr);
+        args.push_back(LHSPtr);
+        args.push_back(RHSPtr);
+      }
+      // Emit arguments for dimensions.
+      auto *destDims = emitValueDims(builder, BMM->getDest());
+      auto *LHSDims = emitValueDims(builder, BMM->getLHS());
+      auto *RHSDims = emitValueDims(builder, BMM->getRHS());
+      args.push_back(destDims);
+      args.push_back(LHSDims);
+      args.push_back(RHSDims);
+    };
+
     auto *F = getFunction("batchedmatmul_f");
     assert(F && "Unable to load the function");
 
-    auto *destDims = emitValueDims(builder, BMM->getDest());
-    auto *LHSDims = emitValueDims(builder, BMM->getLHS());
-    auto *RHSDims = emitValueDims(builder, BMM->getRHS());
+    // Set of constant arguments.
+    llvm::SmallVector<size_t, 16> constArgs{};
+    if (jitSpecialize) {
+      // Buffers have constant addresses, take them into account.
+      constArgs.push_back((size_t)allocatedAddressed_[BMM->getDest()]);
+      constArgs.push_back((size_t)allocatedAddressed_[BMM->getLHS()]);
+      constArgs.push_back((size_t)allocatedAddressed_[BMM->getRHS()]);
+    }
+    constArgs.append(BMM->getDest()->dims().begin(),
+                     BMM->getDest()->dims().end());
+    constArgs.append(BMM->getLHS()->dims().begin(),
+                     BMM->getLHS()->dims().end());
+    constArgs.append(BMM->getRHS()->dims().begin(),
+                     BMM->getRHS()->dims().end());
 
-    builder.CreateCall(F,
-                       {destPtr, LHSPtr, RHSPtr, destDims, LHSDims, RHSDims});
+    // Get the specialized function.
+    auto specializedF =
+        getOrCreateSpecializedFunction(F, constArgs, constArgsEmitter);
+
+    llvm::SmallVector<llvm::Value *, 16> fnArgs{};
+    constArgsEmitter(builder, fnArgs, /* emitAllArgs */ true);
+
+    builder.CreateCall(specializedF, fnArgs);
     break;
   }
 
@@ -194,20 +320,41 @@ void JITBackend::generateLLVMIRForInstr(llvm::IRBuilder<> &builder,
 
   case Kinded::Kind::ConvolutionInstKind: {
     ConvolutionInst *CI = llvm::cast<ConvolutionInst>(I);
-    auto *destPtr = emitValueAddress(builder, CI->getDest(), ElemKind::FloatTy);
-    auto *srcPtr = emitValueAddress(builder, CI->getSrc(), ElemKind::FloatTy);
-    auto *filterPtr =
-        emitValueAddress(builder, CI->getFilter(), ElemKind::FloatTy);
-    auto *biasPtr = emitValueAddress(builder, CI->getBias(), ElemKind::FloatTy);
+    auto constArgsEmitter = [&](llvm::IRBuilder<> &builder,
+                                llvm::SmallVectorImpl<llvm::Value *> &args,
+                                bool emitAllArgs) {
+      if (emitAllArgs) {
+        // Emit arguments for buffers.
+        auto *srcPtr =
+            emitValueAddress(builder, CI->getSrc(), ElemKind::FloatTy);
+        auto *destPtr =
+            emitValueAddress(builder, CI->getDest(), ElemKind::FloatTy);
+        auto *filterPtr =
+            emitValueAddress(builder, CI->getFilter(), ElemKind::FloatTy);
+        auto *biasPtr =
+            emitValueAddress(builder, CI->getBias(), ElemKind::FloatTy);
+        args.push_back(srcPtr);
+        args.push_back(destPtr);
+        args.push_back(filterPtr);
+        args.push_back(biasPtr);
+      }
+      // Emit arguments for dimensions.
+      auto *srcDims = emitValueDims(builder, CI->getSrc());
+      auto *destDims = emitValueDims(builder, CI->getDest());
+      auto *filterDims = emitValueDims(builder, CI->getFilter());
+      auto *biasDims = emitValueDims(builder, CI->getBias());
 
-    auto *destDims = emitValueDims(builder, CI->getDest());
-    auto *srcDims = emitValueDims(builder, CI->getSrc());
-    auto *filterDims = emitValueDims(builder, CI->getFilter());
-    auto *biasDims = emitValueDims(builder, CI->getBias());
-
-    auto *kernel = emitConst(builder, CI->getKernel());
-    auto *stride = emitConst(builder, CI->getStride());
-    auto *pad = emitConst(builder, CI->getPad());
+      auto *kernel = emitConst(builder, CI->getKernel());
+      auto *pad = emitConst(builder, CI->getPad());
+      auto *stride = emitConst(builder, CI->getStride());
+      args.push_back(srcDims);
+      args.push_back(destDims);
+      args.push_back(filterDims);
+      args.push_back(biasDims);
+      args.push_back(kernel);
+      args.push_back(pad);
+      args.push_back(stride);
+    };
 
     const char *kernelName = "convolution_f";
 
@@ -219,9 +366,36 @@ void JITBackend::generateLLVMIRForInstr(llvm::IRBuilder<> &builder,
 
     auto *F = getFunction(kernelName);
     assert(F && "Unable to load the function");
-    builder.CreateCall(F,
-                       {srcPtr, destPtr, filterPtr, biasPtr, srcDims, destDims,
-                        filterDims, biasDims, kernel, pad, stride});
+
+    // Set of constant arguments.
+    llvm::SmallVector<size_t, 16> constArgs{};
+
+    if (jitSpecialize) {
+      // Buffers have constant addresses, take them into account.
+      constArgs.push_back((size_t)allocatedAddressed_[CI->getSrc()]);
+      constArgs.push_back((size_t)allocatedAddressed_[CI->getDest()]);
+      constArgs.push_back((size_t)allocatedAddressed_[CI->getFilter()]);
+      constArgs.push_back((size_t)allocatedAddressed_[CI->getBias()]);
+    }
+
+    constArgs.append(CI->getSrc()->dims().begin(), CI->getSrc()->dims().end());
+    constArgs.append(CI->getDest()->dims().begin(),
+                     CI->getDest()->dims().end());
+    constArgs.append(CI->getFilter()->dims().begin(),
+                     CI->getFilter()->dims().end());
+    constArgs.append(CI->getBias()->dims().begin(),
+                     CI->getBias()->dims().end());
+    constArgs.push_back(CI->getKernel());
+    constArgs.push_back(CI->getPad());
+    constArgs.push_back(CI->getStride());
+
+    // Get the specialized function.
+    auto *specializedF =
+        getOrCreateSpecializedFunction(F, constArgs, constArgsEmitter);
+    llvm::SmallVector<llvm::Value *, 16> fnArgs{};
+    constArgsEmitter(builder, fnArgs, /* emitAllArgs */ true);
+
+    builder.CreateCall(specializedF, fnArgs);
     break;
   }
 
