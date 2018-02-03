37d0f66806ef77846e39a98481f4307b4b4bba5f
Nadav Rotem <nrotem@fb.com>
2018-02-03 00:05:17 -0800

    [Quantization] Implement quantization of 32bit,8bit arithmetic.
    This diff implements the formula that converts float scalar functions
    into a sequence of shifts and multiplications in integer space.

--- tests/unittests/quantizationTest.cpp
+++ tests/unittests/quantizationTest.cpp
@@ -42,4 +42,38 @@ TEST(Quantization, SerializeEmpty) {
   testSerialization(expected);
 }
 
+template <typename From, typename To> static To clip(From in) {
+  static_assert(sizeof(From) >= sizeof(To),
+                "Clip should reduce the variable size");
+  auto mx = std::numeric_limits<To>::max();
+  auto mn = std::numeric_limits<To>::min();
+  return std::max<From>(mn, std::min<From>(mx, in));
+}
+
+TEST(Quantization, quantScaleOffset) {
+  // Test different scale values from 1<<-23 to 1>>1.
+  float scales[] = {
+      0.0000001596f, 0.00000025f, 0.000000995f, 0.0000035f, 0.00000952f,
+      0.00000113f,   0.000721f,   0.0000721f,   0.0000172f, 0.0000951f,
+      0.0000721f,    0.0000341f,  0.0000222f,   0.0000172f, 0.000752f,
+      0.000371f,     0.000321f,   0.000223f,    0.000112f,  0.00852f,
+      0.00671f,      0.00592f,    0.00200f,     0.00107f,   0.0931f,
+      0.0721f,       0.031f,      0.014f,       0.0132f,    0.712f,
+      0.613f,        0.412f,      0.223f,       0.134f,     1.0f,
+      1.13f,         1.612f,      1.523f,       2.0f};
+
+  // Try all scale factors:
+  for (float scale : scales) {
+    // Try all legal integers within the range:
+    for (int8_t input = -128; input < 127; input++) {
+      int32_t sum32num = round(input / scale);
+
+      auto TR = quantizeScaleOffset32To8(scale, 0);
+      int32_t computed = TR.transform(sum32num);
+
+      EXPECT_NEAR(input, computed, 1);
+    }
+  }
+}
+
 } // namespace glow
