fbbd083fa8b5dbb0b958391ef3f977a56ceb9bc7
Nadav Rotem <nrotem@fb.com>
2018-02-06 16:41:02 -0800

    Fix the test by updating the values of the quantized matmul.

--- tests/unittests/OperatorTest.cpp
+++ tests/unittests/OperatorTest.cpp
@@ -271,9 +271,9 @@ TEST(Operator, IntMatMul) {
 
   // The scaling factor 1.4x was carefully selected to make sure we don't
   // overflow or underflow the calculation.
-  Type resTy(ElemKind::Int8QTy, {1, 3, 3}, 1.4, -4.0);
-  Type lhsTy(ElemKind::Int8QTy, {1, 3, 3}, 0.1, 2.0);
-  Type rhsTy(ElemKind::Int8QTy, {1, 3, 3}, 0.1, -1.0);
+  Type resTy(ElemKind::Int8QTy, {1, 3, 3}, 1.3, -4.0);
+  Type lhsTy(ElemKind::Int8QTy, {1, 3, 3}, 0.08, 1.0);
+  Type rhsTy(ElemKind::Int8QTy, {1, 3, 3}, 0.08, -1.0);
 
   auto *res = G.createVariable(ElemKind::FloatTy, {1, 3, 3}, "res");
   auto *lhs = G.createVariable(ElemKind::FloatTy, {1, 3, 3}, "lhs");
@@ -301,21 +301,21 @@ TEST(Operator, IntMatMul) {
 
   /*
    Test the following matrix multiplication:
-   A =   [[[1.0, 2.0, 3.0], [4.0, 5.0, -5.0], [-4.0, -3.0, 9.0]]]
-   B =   [[[0.1, -0.2, 0.3], [9.0, -8.0, 7.0], [6.0, 5.0, 9.0]]]
-   A * B = [ 36.1,  -1.2,  41.3], [ 15.4, -65.8,  -8.8], [ 26.6,  69.8,  58.8]]
+   A = [[1.0, 2.0, 3.0], [4.0, 5.0, -5.0], [-4.0, -3.0, 9.0]]
+   B = [[0.1, -0.2, 0.3], [9.0, -8.0, 7.0], [6.0, 5.0, 9.0]]
+   A x B = [ 36.1,  -1.2,  41.3], [15.4, -65.8, -8.8], [26.6, 69.8,  58.8]]
    */
 
   auto H = res->getPayload().getHandle();
-  EXPECT_FLOAT_EQ(H.at({0, 0, 0}), 35.0);
-  EXPECT_FLOAT_EQ(H.at({0, 0, 1}), 0);
-  EXPECT_FLOAT_EQ(H.at({0, 0, 2}), 40.6);
-  EXPECT_FLOAT_EQ(H.at({0, 1, 0}), 15.4);
-  EXPECT_FLOAT_EQ(H.at({0, 1, 1}), -65.8);
-  EXPECT_FLOAT_EQ(H.at({0, 1, 2}), -8.4);
-  EXPECT_FLOAT_EQ(H.at({0, 2, 0}), 26.6);
-  EXPECT_FLOAT_EQ(H.at({0, 2, 1}), 68.6);
-  EXPECT_FLOAT_EQ(H.at({0, 2, 2}), 58.8);
+  EXPECT_NEAR(H.at({0, 0, 0}), 36.1, 0.7);
+  EXPECT_NEAR(H.at({0, 0, 1}), -1.2, 0.7);
+  EXPECT_NEAR(H.at({0, 0, 2}), 41.3, 0.7);
+  EXPECT_NEAR(H.at({0, 1, 0}), 15.4, 0.7);
+  EXPECT_NEAR(H.at({0, 1, 1}), -65.8, 0.7);
+  EXPECT_NEAR(H.at({0, 1, 2}), -8.8, 0.7);
+  EXPECT_NEAR(H.at({0, 2, 0}), 26.6, 0.7);
+  EXPECT_NEAR(H.at({0, 2, 1}), 69.8, 0.7);
+  EXPECT_NEAR(H.at({0, 2, 2}), 58.8, 0.7);
 }
 
 TEST(Operator, IntBatchedArith) {
