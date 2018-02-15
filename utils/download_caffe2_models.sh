19b864f27548a3646451799de6a7f9de5a356671
Jordan Fix <jfix@fb.com>
2018-02-14 22:26:15 -0800

    Add new Caffe2 model LeNet pre-trained on MNIST

--- utils/download_caffe2_models.sh
+++ utils/download_caffe2_models.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-for model in "resnet50" "vgg16" "vgg19" "inception_v1" "inception_v2" "shufflenet" "squeezenet" ; do
+for model in "resnet50" "vgg16" "vgg19" "inception_v1" "inception_v2" "shufflenet" "squeezenet" "lenet_mnist" ; do
   for file in predict_net.pbtxt predict_net.pb init_net.pb; do
     wget http://fb-glow-assets.s3.amazonaws.com/models/$model/$file -P $model
   done
 done
-
-
