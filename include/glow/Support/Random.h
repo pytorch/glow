88a0fb93f0a96862e26029d1582a99796d1d8fcf
James Hegeman <jwhegeman@fb.com>
2018-02-26 16:40:19 -0800

    Reduce the number of random integer generation functions
    ReduceNumberOfRandFunctions update
    
    Fix initialization of static distribution.
    
    Make single static mt19937 generator
    
    Remove randomizeInt8() from Handle
    
    ReduceNumberOfRandFunctions updates 3
    
    Convert nextRandInt() to wrap nextRand()

--- include/glow/Support/Random.h
+++ include/glow/Support/Random.h
@@ -6,14 +6,8 @@ namespace glow {
 /// \returns the next uniform random number in the range -1..1.
 double nextRand();
 
-/// \returns the next uniform random integer that is either 0 or 1.
-int nextRandInt01();
-
-/// \returns the next uniform random integer in the range -128..127.
-int nextRandInt8();
-
-/// \returns the next uniform random integer that is between 0 and n - 1.
-int nextRandInt(int n);
+/// \returns the next uniform random integer in the closed interval [a, b].
+int nextRandInt(int a, int b);
 
 } // namespace glow
 
