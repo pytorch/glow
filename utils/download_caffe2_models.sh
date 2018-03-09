8903bcdb9d105957360733a9215cff912b50e85e
Bert Maher <bertrand@fb.com>
2018-03-08 22:24:26 -0800

    Add densenet121 from caffe2 model zoo

--- utils/download_caffe2_models.sh
+++ utils/download_caffe2_models.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
-for model in "resnet50" "vgg16" "vgg19" "inception_v1" "inception_v2" "shufflenet" "squeezenet" "lenet_mnist" ; do
+MODELS=$(cat <<EOF
+densenet121
+inception_v1
+inception_v2
+lenet_mnist
+resnet50
+shufflenet
+squeezenet
+vgg16
+vgg19
+EOF
+)
+
+for model in $MODELS; do
   for file in predict_net.pbtxt predict_net.pb init_net.pb; do
     wget http://fb-glow-assets.s3.amazonaws.com/models/$model/$file -P $model
   done
