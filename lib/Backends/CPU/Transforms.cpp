7263bb64ba087afcf704903b3ed389dfe42fb671
Nadav Rotem <nrotem@fb.com>
2018-03-27 14:02:03 -0700

    [CPU] Tile the access to the filter and weight along the channel axis.

--- lib/Backends/CPU/Transforms.cpp
+++ lib/Backends/CPU/Transforms.cpp
@@ -22,10 +22,10 @@ static Node *optimizeCPUConv(ConvolutionNode *CN, Function *F) {
   auto *M = F->getParent();
   auto inChannels = CN->getInput()->dims()[3];
 
-  // The depth dimension must of a multiple of 64 to perform the
+  // The depth dimension must be a multiple of 64 to perform the
   // transformation. This transformation is currently only profitable on
   // low-channel convolutions.
-  if (depth < 64 || depth % 64 || inChannels > 256) {
+  if (depth < 64 || (depth % 64) != 0 || inChannels > 256) {
     return nullptr;
   }
 
