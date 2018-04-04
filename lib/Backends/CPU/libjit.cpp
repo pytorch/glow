4491c351d2b18e4651764a8cb00fa255c1b944f2
Bert Maher <bertrand@fb.com>
2018-04-04 13:39:20 -0700

    [libjit] Move some definitions into a header file

--- lib/Backends/CPU/libjit.cpp
+++ lib/Backends/CPU/libjit.cpp
@@ -7,38 +7,7 @@
 #include <cstring>
 #include <sys/types.h>
 
-typedef float float4 __attribute__((ext_vector_type(4)));
-typedef float float8 __attribute__((ext_vector_type(8)));
-
-/// Loads a simd float8 value from \p ptr.
-#define LoadFloat8(PTR) *((const float8 *)(PTR))
-
-/// Stores the simd float8 value to \p ptr.
-#define StoreFloat8(PTR, VAL) *((float8 *)(PTR)) = (VAL);
-
-/// Accumulate (+=) the simd float8 value to \p ptr.
-#define AddFloat8(PTR, VAL) *((float8 *)(PTR)) += (VAL);
-
-/// Broadcast the input value to a float8.
-#define BroadcastFloat8(VAL) ((float8)(VAL))
-
-#define MIN(a, b) (((a) < (b)) ? (a) : (b))
-#define MAX(a, b) (((a) > (b)) ? (a) : (b))
-#define AT(tensor, dims, numDims, indices, numIndices)                         \
-  tensor[get_element_ptr(tensor, dims, numDims, indices, numIndices)]
-
-/// Perform an unaligned load of a float8 from a float pointer.
-float8 LoaduFloat8(const float *p) {
-  float8 res;
-  memcpy(&res, p, sizeof(float8));
-  return res;
-}
-
-/// Perform an unaligned store to a float pointer.
-void StoreuFloat8(float *p, float8 v) { memcpy(p, &v, sizeof(float8)); }
-
-/// Perform an unaligned addition to a float pointer.
-void AdduFloat8(float *p, float8 v) { StoreuFloat8(p, LoaduFloat8(p) + v); }
+#include "libjit_defs.h"
 
 namespace {
 
