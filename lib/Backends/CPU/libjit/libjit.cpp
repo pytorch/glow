1b4fb627ef7dc32ebaa9fa9018a6e1d780a7d32d
Roman Dzhabarov <rdzhabarov@fb.com>
2018-04-11 21:17:30 -0700

    [clang-format] format codebase.

--- lib/Backends/CPU/libjit/libjit.cpp
+++ lib/Backends/CPU/libjit/libjit.cpp
@@ -479,7 +479,7 @@ DEFINE_DATA_PARALLEL_KERNEL_QUANTIZED(libjit_elementmax_kernel_i8, int8_t,
                                       MAX(lhs, rhs))
 DEFINE_DATA_PARALLEL_KERNEL_QUANTIZED(libjit_elementmin_kernel_i8, int8_t,
                                       MIN(lhs, rhs))
-DEFINE_DATA_PARALLEL_KERNEL_QUANTIZED_M(libjit_element_mul_kernel_i8, lhs * rhs)
+DEFINE_DATA_PARALLEL_KERNEL_QUANTIZED_M(libjit_element_mul_kernel_i8, lhs *rhs)
 DEFINE_DATA_PARALLEL_KERNEL_QUANTIZED_M(libjit_element_div_kernel_i8, lhs / rhs)
 // tanh cannot be vectorized by LLVM yet. Therefore we use the following
 // formula instead: 1 - 2 / (exp(x * 2) + 1), which is also used by Caffe2 and
