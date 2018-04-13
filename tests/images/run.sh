8c4864d26b07167830663bbfffc4744c7fa39c0e
Bert Maher <bertrand@fb.com>
2018-04-13 11:07:58 -0700

    [run.sh] Pass through command line args

--- tests/images/run.sh
+++ tests/images/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-./bin/loader tests/images/imagenet/*.png -image_mode=0to1 -d=resnet50
-./bin/loader tests/images/imagenet/*.png -image_mode=128to127 -d=vgg19
-./bin/loader tests/images/imagenet/*.png -image_mode=128to127 -d=squeezenet
-./bin/loader tests/images/imagenet/*.png -image_mode=0to256 -d=vgg16
+./bin/loader tests/images/imagenet/*.png -image_mode=0to1 -d=resnet50 $@
+./bin/loader tests/images/imagenet/*.png -image_mode=128to127 -d=vgg19 $@
+./bin/loader tests/images/imagenet/*.png -image_mode=128to127 -d=squeezenet $@
+./bin/loader tests/images/imagenet/*.png -image_mode=0to256 -d=vgg16 $@
 
-./bin/loader tests/images/mnist/*.png -image_mode=0to1 -d=lenet_mnist
+./bin/loader tests/images/mnist/*.png -image_mode=0to1 -d=lenet_mnist $@
