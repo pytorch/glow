b1aaa2216a61e8ea6e3656031d3524e995fe0a40
Bert Maher <bertrand@fb.com>
2018-04-20 15:43:04 -0700

    [license] Add Apache 2 license

--- utils/license.sh
+++ utils/license.sh
@@ -1,3 +1,12 @@
+#!/usr/bin/env bash
+
+# Adds the Apache 2.0 license to any source file that doesn't already have it.
+
+function add_license () {
+    input=$1
+    temp=$(mktemp)
+    if ! grep -q 'Apache License' $input; then
+        cat - $input > $temp <<EOF
 /**
  * Copyright (c) 2017-present, Facebook, Inc.
  *
@@ -13,29 +22,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef GLOW_BASE_TRAIN_H
-#define GLOW_BASE_TRAIN_H
+EOF
+        mv $temp $input
+    fi
+}
 
-#include "glow/Base/Tensor.h"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPT_DIR/.."
 
-#include <cstddef>
-#include <cstdint>
-#include <unordered_map>
-
-namespace glow {
-
-class Tensor;
-
-/// This is a list of parameters that the network trainers (such as sgd and
-/// adam) use for training the network.
-struct TrainingConfig {
-  float L1Decay{0};
-  float L2Decay{0};
-  float learningRate{0.01};
-  float momentum{0.0};
-  unsigned batchSize{1};
-};
-
-} // namespace glow
-
-#endif // GLOW_BASE_TRAIN_H
+for i in $(find . -name '*.cpp' -o -name '*.h' | grep -v /googletest/ ); do
+    add_license $i
+done
