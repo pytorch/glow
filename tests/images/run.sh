4eebbe812649423b68f115daa6be66e6474b845f
artemrakhov <artem@fb.com>
2018-04-27 16:59:55 -0700

    [ONNX] Add ONNX resnet50 to images run script

--- tests/images/run.sh
+++ tests/images/run.sh
@@ -4,5 +4,8 @@
 ./bin/loader tests/images/imagenet/*.png -image_mode=128to127 -d=vgg19 $@
 ./bin/loader tests/images/imagenet/*.png -image_mode=128to127 -d=squeezenet $@
 ./bin/loader tests/images/imagenet/*.png -image_mode=0to256 -d=vgg16 $@
+for png_filename in tests/images/imagenet/*.png; do
+  ./bin/loader $png_filename -image_mode=0to1 -o=resnet50/model.onnx $@
+done
 
 ./bin/loader tests/images/mnist/*.png -image_mode=0to1 -d=lenet_mnist $@
