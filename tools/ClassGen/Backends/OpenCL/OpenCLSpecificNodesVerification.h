6c658a81178cc409f26543a65201254e035e2b6f
Roman Levenstein <rlevenstein@fb.com>
2018-05-08 15:40:47 -0700

    [opencl] Add OpenCL-specific nodes and instructions for convolutions and pooling
    These nodes and instructions work with NCHW tensor format, which is more efficient on GPUs for these operations.

---
+++ tools/ClassGen/Backends/OpenCL/OpenCLSpecificNodesVerification.h
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifdef GLOW_WITH_OPENCL
+
+void OCLConvolutionNode::verify() const {
+  ShapeNCHW idim(getInput().getType()->dims());
+  ShapeNCHW odim(getResult().getType()->dims());
+  auto outSz = calculateConvOutputDims(idim.h, idim.w, getKernel(), getStride(),
+                                       getPad());
+  ShapeNCHW exp(idim.n, getBias().dims()[0], outSz.first, outSz.second);
+  (void)exp;
+  assert(exp == odim && "Invalid output dimensions");
+}
+
+void OCLPoolAvgNode::verify() const {}
+
+void OCLPoolMaxNode::verify() const {}
+#endif // GLOW_WITH_OPENCL
--- tools/ClassGen/InstrGen.cpp
+++ tools/ClassGen/InstrGen.cpp
@@ -416,6 +416,7 @@ int main(int argc, char **argv) {
   //===--------------------------------------------------------------------===//
 
 #include "Backends/CPU/CPUSpecificInstrs.h"
+#include "Backends/OpenCL/OpenCLSpecificInstrs.h"
 
   return 0;
 }
