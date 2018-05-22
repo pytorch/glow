760f984e3324b9f951ebd52d4efdecb2ef9d01e4
artemrakhov <artem@fb.com>
2018-05-21 18:34:17 -0700

    [Importer] Reduce code duplication between Caffe2 and ONNX importers.

--- include/glow/Importer/Caffe2.h
+++ include/glow/Importer/Caffe2.h
@@ -13,18 +13,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #ifndef GLOW_IMPORTER_CAFFE2_H
 #define GLOW_IMPORTER_CAFFE2_H
 
 #include "glow/Graph/Graph.h"
+#include "glow/Importer/ProtobufLoader.h"
 
 #include "llvm/ADT/ArrayRef.h"
-#include "llvm/ADT/SmallVector.h"
-#include "llvm/Support/Casting.h"
 
 #include <string>
-#include <unordered_map>
-#include <unordered_set>
 
 namespace caffe2 {
 class OperatorDef;
@@ -37,25 +35,13 @@ class Tensor;
 class Value;
 
 /// Loads caffe2 models.
-class caffe2ModelLoader {
-  /// The graph that we are constructing.
-  Function &G_;
-  /// Saves network nodes by name.
-  std::unordered_map<std::string, Node *> nodeByName_;
-  /// A list of weight tensors indexed by name.
-  std::unordered_map<std::string, Tensor *> tensors_;
-  /// The external output of the network.
-  SaveNode *root_{nullptr};
-
+class caffe2ModelLoader : public ProtobufLoader {
   /// Load the weight tensors from the 'init' file and register them in the map
   /// \p tensors.
   void loadWeights(caffe2::NetDef &net);
 
   /// Load the structure of the network from the 'net' file.
   void loadNetwork(caffe2::NetDef &net);
-
-  /// \returns the tensor that was registered under the name \p name.
-  Tensor *getTensorByName(const std::string &name);
 
   /// Load the operator \p op into the network. This creates one or more nodes
   /// in the network.
@@ -66,31 +52,15 @@ class caffe2ModelLoader {
   bool loadProtoFile(caffe2::NetDef &net, const std::string &filename);
 
 public:
-  /// \returns the node that was registered with the name \p name.
-  Node *getNodeByName(const std::string &name);
-
-  /// \returns the node that was registered with the name \p name or create a
-  /// new Variable node for a tensor with this name.
-  Node *getOrCreateNodeByName(const std::string &name);
-
-  /// \returns True if the node that's registered using \p name exists.
-  bool hasNodeByName(const std::string &name) const;
-
   /// Loads the caffe2 model that's represented by a network description file,
   /// serialized in \p netDescFilename, and weights file, serialized in
-  /// \p netWeightFilename, and populates the network in \p N.
+  /// \p netWeightFilename, and populates the network in \p F.
   /// The tensors in \p tensors are stored with the names in the list of names
   /// \p names and used as inputs to the network.
   caffe2ModelLoader(const std::string &netDescFilename,
                     const std::string &netWeightFilename,
                     llvm::ArrayRef<const char *> names,
-                    llvm::ArrayRef<Tensor *> tensors, Function &G);
-
-  ~caffe2ModelLoader();
-
-  /// \returns the output of the network. This is usually the result of the last
-  /// softmax or regression layer.
-  SaveNode *getRoot() { return root_; }
+                    llvm::ArrayRef<Tensor *> tensors, Function &F);
 };
 
 } // namespace glow
--- include/glow/Importer/ONNX.h
+++ include/glow/Importer/ONNX.h
@@ -13,15 +13,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #ifndef GLOW_IMPORTER_ONNX_H
 #define GLOW_IMPORTER_ONNX_H
 
 #include "glow/Graph/Graph.h"
+#include "glow/Importer/ProtobufLoader.h"
 
 #include "llvm/ADT/ArrayRef.h"
 
 #include <string>
-#include <unordered_map>
 
 namespace onnx {
 class NodeProto;
@@ -30,28 +31,13 @@ class GraphProto;
 
 namespace glow {
 
-class Tensor;
-class Value;
-
 /// Loads ONNX models.
-class ONNXModelLoader {
-  /// The graph that we are constructing.
-  Function &G_;
-  /// Saves network nodes by name.
-  std::unordered_map<std::string, Node *> nodeByName_;
-  /// A list of weight tensors indexed by name.
-  std::unordered_map<std::string, Tensor *> tensors_;
-  /// The external output of the network.
-  SaveNode *root_{nullptr};
-
+class ONNXModelLoader : public ProtobufLoader {
   /// Load the network operators from the GraphProto.
   void loadNetwork(onnx::GraphProto &net);
 
   /// Load the network initializers from the GraphProto.
   void loadInitializers(onnx::GraphProto &net);
-
-  /// \returns the tensor that was registered under the name \p name.
-  Tensor *getTensorByName(const std::string &name);
 
   /// Load the operator \p op into the network. This creates one or more nodes
   /// in the network.
@@ -62,29 +48,13 @@ class ONNXModelLoader {
   bool loadProtoFile(onnx::GraphProto &net, const std::string &filename);
 
 public:
-  /// \returns the node that was registered with the name \p name.
-  Node *getNodeByName(const std::string &name);
-
-  /// \returns the node that was registered with the name \p name or create a
-  /// new Variable node for a tensor with this name.
-  Node *getOrCreateNodeByName(const std::string &name);
-
-  /// \returns True if the node that's registered using \p name exists.
-  bool hasNodeByName(const std::string &name) const;
-
   /// Loads the ONNX model that's represented by a model description file,
-  /// serialized in \p modelDescFilename and populates the network into \p G.
+  /// serialized in \p modelDescFilename and populates the network into \p F.
   /// The tensors in \p tensors are stored with the names in the list of names
   /// \p names and used as inputs to the network.
   ONNXModelLoader(const std::string &modelDescFilename,
                   llvm::ArrayRef<const char *> names,
-                  llvm::ArrayRef<Tensor *> tensors, Function &G);
-
-  ~ONNXModelLoader();
-
-  /// \returns the output of the network. This is usually the result of the last
-  /// softmax or regression layer.
-  SaveNode *getRoot() { return root_; }
+                  llvm::ArrayRef<Tensor *> tensors, Function &F);
 };
 
 } // namespace glow
