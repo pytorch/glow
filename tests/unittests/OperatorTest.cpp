1efae9bc65a74a6bf9b6a31c40e672f129055108
Roman Dzhabarov <rdzhabarov@fb.com>
2018-05-23 21:23:11 -0700

    [quantization] Add missing support for operations in CPU.

--- tests/unittests/OperatorTest.cpp
+++ tests/unittests/OperatorTest.cpp
@@ -1609,7 +1609,7 @@ TEST_P(InterpAndCPU, GroupConvolution) {
   EXPECT_FLOAT_EQ(result.at({0, 1, 0, 5}), (13 + 14 + 15 + 16) * 100000);
 }
 
-TEST_P(InterpOnly, Int8Tanh) {
+TEST_P(InterpAndCPU, Int8Tanh) {
   constexpr size_t size = 10;
   auto *input = mod_.createVariable(ElemKind::FloatTy, {size}, "input");
   input->getHandle().randomize(-10.0, 10.0);
@@ -1644,7 +1644,7 @@ TEST_P(InterpOnly, Int8Tanh) {
   }
 }
 
-TEST_P(InterpOnly, Int8Sigmoid) {
+TEST_P(InterpAndCPU, Int8Sigmoid) {
   constexpr size_t size = 10;
   auto *input = mod_.createVariable(ElemKind::FloatTy, {size}, "input");
   input->getHandle().randomize(-10.0, 10.0);
@@ -1678,7 +1678,7 @@ TEST_P(InterpOnly, Int8Sigmoid) {
   }
 }
 
-TEST_P(InterpOnly, IntLookupTable) {
+TEST_P(InterpAndCPU, IntLookupTable) {
   constexpr size_t size = 6;
   auto *input = mod_.createVariable(ElemKind::Int8QTy, {size}, 1, 0, "input");
   input->getHandle<int8_t>() = {0, 1, 2, 3, 4, 5};
