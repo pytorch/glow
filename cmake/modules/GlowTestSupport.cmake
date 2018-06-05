1116b5a24c1ecf34de545b3ea1de6d1d5b01af08
Quentin Colombet <qcolombet@fb.com>
2018-06-05 14:46:43 -0700

    [cmake][test] Add a add_glow_test function and 'check' target
    Introduce the add_glow_test function that on top of adding the test
    to the list of tests to be run with ctest, it will register the
    dependencies on the actual executable.
    Thanks to this information, the new 'check' target can re-build
    the test executables if needed and then run the test.
    
    In other words, the combination of those two additions act like
    `ninja all && ninja test`, except we don't have to think about it.

---
+++ cmake/modules/GlowTestSupport.cmake
@@ -0,0 +1,34 @@
+# A function to add a test to be driven through the 'check' target.
+# Unlike the 'test' target, the 'check' target rebuilds the executables
+# before invoking the tests.
+function(add_glow_test)
+  set(oneValueArgs NAME)
+  set(multiValueArgs COMMAND DEPENDS)
+  cmake_parse_arguments(ARG "" "${oneValueArgs}"
+                          "${multiValueArgs}" ${ARGN})
+
+  if (NOT ARG_NAME)
+    list(GET ARG_UNPARSED_ARGUMENTS 0 ARG_NAME)
+    list(REMOVE_AT ARG_UNPARSED_ARGUMENTS 0)
+  endif()
+  
+  if (NOT ARG_NAME)
+    message(FATAL_ERROR "Name mandatory")
+  endif()
+
+  if (NOT ARG_COMMAND)
+    set(ARG_COMMAND ${ARG_UNPARSED_ARGUMENTS})
+  endif()
+
+  if (NOT ARG_COMMAND)
+    message(FATAL_ERROR "Command mandatory")
+  endif()
+
+  list(GET ARG_COMMAND 0 TEST_EXEC)
+  list(APPEND ARG_DEPENDS ${TEST_EXEC})
+  
+  set_property(GLOBAL APPEND PROPERTY GLOW_TEST_DEPENDS ${ARG_DEPENDS})
+
+  # Produce the specific test rule using the default built-in.
+  add_test(NAME ${ARG_NAME} COMMAND ${ARG_COMMAND})
+endfunction()
