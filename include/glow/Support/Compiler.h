cd437a635af3da145b55fe618bb4559f24158103
Roman Levenstein <rlevenstein@fb.com>
2018-06-28 18:43:25 -0700

    Remove the unused definition of GLOW_GNUC_ATLEAST

--- include/glow/Support/Compiler.h
+++ include/glow/Support/Compiler.h
@@ -22,14 +22,6 @@
 #define __has_builtin(builtin) 0
 #endif
 
-#if defined(__GNUC__)
-#define GLOW_GNUC_ATLEAST(major, minor, patch)                                 \
-  (__GNUC__ >= (major) || __GNUC_MINOR__ >= (minor) ||                         \
-   __GNUC_PATCHLEVEL__ >= (patch))
-#else
-#define GLOW_GNUC_ATLEAST(major, minor, patch) 0
-#endif
-
 #define GLOW_ASSERT(e)                                                         \
   ((void)((e) ? ((void)0) : GLOW_ASSERT_IMPL(#e, __FILE__, __LINE__)))
 #define GLOW_ASSERT_IMPL(e, file, line)                                        \
