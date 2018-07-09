9d4e1c719051f6d099dc0b34b90945abceddc33a
Bert Maher <bertrand@fb.com>
2018-07-09 13:19:27 -0700

    [cpu] Factor out BundleSaver

---
+++ lib/Backends/CPU/BundleSaver.h
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef GLOW_BACKENDS_CPU_BUNDLESAVER_H
+#define GLOW_BACKENDS_CPU_BUNDLESAVER_H
+
+#include "AllocationsInfo.h"
+#include "LLVMIRGen.h"
+
+#include "glow/IR/IR.h"
+
+namespace glow {
+
+class BundleSaver final {
+  /// The Module that holds the glow IR. This does not own the module.
+  const IRFunction *F_;
+  /// Information about allocations.
+  AllocationsInfo allocationsInfo_;
+  /// The LLVM IR code generator.
+  LLVMIRGen irgen_;
+
+  /// Perform memory allocation for a bundle.
+  void performBundleMemoryAllocation();
+  /// Save weights for the bundle.
+  void saveWeights(llvm::StringRef weightsFileName);
+  /// Produce a bundle.
+  void produceBundle(llvm::StringRef outputDir);
+  /// Emit config for a bundle.
+  void emitBundleConfig();
+  /// Emit the symbol table for a bundle.
+  void emitSymbolTable();
+  /// Emit the entry function for the bundle.
+  void emitBundleEntryFunction();
+
+public:
+  /// Ctor.
+  explicit BundleSaver(const IRFunction *F);
+  /// Save code bundle built for \p target to \p outputDir.
+  void save(llvm::StringRef target, llvm::StringRef outputDir);
+};
+
+} // namespace glow
+
+#endif // GLOW_BACKENDS_CPU_BUNDLESAVER_H
