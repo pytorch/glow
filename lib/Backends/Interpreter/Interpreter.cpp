77e90547bab9d8ec0386ee1004f1caa46c312c74
Bert Maher <bertrand@fb.com>
2018-07-11 09:55:06 -0700

    [interpreter] Use CompiledFunction internally

--- lib/Backends/Interpreter/Interpreter.cpp
+++ lib/Backends/Interpreter/Interpreter.cpp
@@ -15,6 +15,7 @@
  */
 
 #include "Interpreter.h"
+#include "InterpreterFunction.h"
 
 #include "glow/Graph/Graph.h"
 #include "glow/Graph/Nodes.h"
@@ -26,81 +27,12 @@
 using namespace glow;
 using llvm::isa;
 
-Interpreter::~Interpreter() {
-  // Delete the tensors that are owned by this backend.
-  for (auto p : tensors_) {
-    delete p.second;
-  }
-  tensors_.clear();
-  externalTensors_.clear();
-}
-
 void Interpreter::init(std::unique_ptr<IRFunction> IR) {
-  F_ = std::move(IR);
-  for (auto &v : F_->getGraph()->getParent()->getVars()) {
-    auto *w = F_->getWeightForNode(v);
-    assert(!externalTensors_.count(w) && "The tensor is already registered");
-    externalTensors_[w] = &v->getPayload();
-  }
-
-  for (auto *W : F_->getWeights()) {
-    getOrCreateTensor(W);
-  }
+  function_ = llvm::make_unique<InterpreterFunction>(std::move(IR));
 }
 
-Tensor *Interpreter::getTensor(const Value *v) const {
-  auto it = tensors_.find(v);
-  if (it != tensors_.end()) {
-    return it->second;
-  }
-
-  auto ie = externalTensors_.find(v);
-  assert(ie != externalTensors_.end() && "Unknown key Value.");
-  return ie->second;
-}
-
-Tensor *Interpreter::getOrCreateTensor(const Value *v) {
-  auto ie = externalTensors_.find(v);
-  if (ie != externalTensors_.end()) {
-    return ie->second;
-  }
-
-  // Pick the tensor.
-  auto it = tensors_.find(v);
-  if (it == tensors_.end()) {
-    auto *T = new Tensor(v->getType());
-    tensors_[v] = T;
-    return T;
-  }
-  return it->second;
-}
-
-Tensor *Interpreter::getOrCreateUnownedTensor(const Value *v, const Value *src,
-                                              llvm::ArrayRef<size_t> offsets) {
-  assert(isa<TensorViewInst>(v) && "Expected a tensor view");
-
-  // Pick the tensor.
-  auto it = tensors_.find(v);
-
-  // Release unowned tensors before re-creating them.
-  if (it != tensors_.end()) {
-    deleteTensor(v);
-  }
-
-  auto *T = new Tensor();
-  *T = getTensor(src)->getUnowned(v->dims(), offsets);
-  tensors_[v] = T;
-  return T;
-}
-
-void Interpreter::deleteTensor(const Value *v) {
-  auto it = tensors_.find(v);
-  if (it == tensors_.end()) {
-    return;
-  }
-
-  delete it->second;
-  tensors_.erase(it);
+void Interpreter::doForwardPass() {
+  function_->doForwardPass();
 }
 
 bool Interpreter::isOpSupported(Kinded::Kind opKind, ElemKind elementTy) const {
@@ -147,26 +79,6 @@ bool Interpreter::shouldLower(const Node *N) const {
   if (N->getKind() == Kinded::Kind::ConvolutionNodeKind)
     return false;
   return true;
-}
-
-void Interpreter::doForwardPass() {
-// Do the forward pass.
-#define DEF_VALUE(CLASS, NAME)
-#define DEF_INSTR(CLASS, NAME)                                                 \
-  case Kinded::Kind::CLASS##Kind: {                                            \
-    fwd##CLASS(llvm::cast<CLASS>(&I));                                         \
-    break;                                                                     \
-  }
-#define DEF_BACKEND_SPECIFIC_INSTR(CLASS, NAME)
-  // Dispatch the interpreter on each instruction in the program:
-  for (const auto &I : F_->getInstrs()) {
-    switch (I.getKind()) {
-#include "AutoGenInstr.def"
-
-    default:
-      llvm_unreachable("Invalid instruction.");
-    }
-  }
 }
 
 namespace glow {
