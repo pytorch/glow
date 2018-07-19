8290b28bfb14f103e5c9c63dfd1ff2334ef57dcb
Man Wang <wangm@fb.com>
2018-07-19 13:03:46 -0700

    [run.sh] Suport inception_v1 and inception_v2 modles.

--- tests/images/run.sh
+++ tests/images/run.sh
@@ -6,6 +6,9 @@
 ./bin/image-classifier tests/images/imagenet/*.png -image_mode=0to256 -m=zfnet512 $@
 ./bin/image-classifier tests/images/imagenet/*.png -image_mode=0to1 -m=densenet121 $@
 ./bin/image-classifier tests/images/imagenet/*.png -image_mode=0to1 -m=shufflenet $@
+./bin/image-classifier tests/images/mnist/*.png -image_mode=0to1 -m=lenet_mnist $@
+./bin/image-classifier tests/images/imagenet/*.png -image_mode=0to256 -m=inception_v1 $@
+./bin/image-classifier tests/images/imagenet/*.png -image_mode=0to256 -m=bvlc_alexnet $@
 for png_filename in tests/images/imagenet/*.png; do
   ./bin/image-classifier $png_filename -image_mode=0to1 -m=resnet50/model.onnx $@
 done
@@ -22,9 +25,12 @@ for png_filename in tests/images/imagenet/*.png; do
   ./bin/image-classifier $png_filename -image_mode=0to1 -m=densenet121/model.onnx $@
 done
 ./bin/image-classifier tests/images/imagenet/zebra_340.png -image_mode=0to1 -m=shufflenet/model.onnx $@
-
-./bin/image-classifier tests/images/mnist/*.png -image_mode=0to1 -m=lenet_mnist $@
-
 for png_filename in tests/images/mnist/*.png; do
   ./bin/image-classifier $png_filename -image_mode=0to1 -m=mnist.onnx $@
+done
+for png_filename in tests/images/imagenet/*.png; do
+  ./bin/image-classifier $png_filename -image_mode=0to256 -m=inception_v1/model.onnx $@
+done
+for png_filename in tests/images/imagenet/*.png; do
+  ./bin/image-classifier $png_filename -image_mode=0to256 -m=bvlc_alexnet/model.onnx $@
 done
