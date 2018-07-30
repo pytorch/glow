82233dd8a6773d568fb698cb8e447dc4e7f1922b
Man Wang <wangm@fb.com>
2018-07-30 15:34:47 -0700

    Importer

---
+++ tests/unittests/caffe2ImporterTest.cpp
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "glow/ExecutionEngine/ExecutionEngine.h"
+#include "glow/Graph/Graph.h"
+#include "glow/Importer/Caffe2.h"
+#include "gtest/gtest.h"
+
+using namespace glow;
+
+static void getImageData(Tensor *result, size_t n, size_t c, size_t h,
+                         size_t w) {
+  result->reset(ElemKind::FloatTy, {n, c, h, w});
+  auto RH = result->getHandle<>();
+  for (size_t i = 0, e = n * c * h * w; i < e; i++)
+    RH.raw(i) = i;
+}
+
+/// Test loading conv op from a Caffe2 model.
+/// The input is N*C*H*W (1*1*3*3), the kernel is 2,
+/// stride is 1, pad is 1, group is 1.
+TEST(caffe2, importConv) {
+  ExecutionEngine EE{BackendKind::Interpreter};
+  auto &mod = EE.getModule();
+  Function *F = mod.createFunction("main");
+
+  std::string NetDescFilename("tests/models/caffe2Models/predict_net.pbtxt");
+  std::string NetWeightFilename("tests/models/caffe2Models/init_net.pbtxt");
+
+  SaveNode *output;
+  // Destroy the loader after the graph is loaded since the following execution
+  // will not depend on anyting from the loader.
+  {
+    Tensor data;
+    getImageData(&data, 1, 1, 3, 3);
+    caffe2ModelLoader caffe2LD(NetDescFilename, NetWeightFilename, {"data"},
+                               {&data}, *F);
+    output = caffe2LD.getSingleOutput();
+  }
+
+  EE.compile(CompilationMode::Infer, F);
+  EE.run({}, {});
+  auto result = output->getVariable()->getHandle();
+  std::vector<size_t> expectedDims = {1, 1, 4, 4};
+  std::vector<float> expectedValues = {2,  3,  5,  4,  5, 10, 14, 9,
+                                       11, 22, 26, 15, 8, 15, 17, 10};
+  EXPECT_TRUE(result.dims().vec() == expectedDims);
+  for (size_t i = 0; i < 4 * 4; i++)
+    EXPECT_FLOAT_EQ(result.raw(i), expectedValues[i]);
+}
