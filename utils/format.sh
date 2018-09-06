61e12cbe681c285d2ea03739d78f92cffcf7b4a4
Roman Dzhabarov <rdzhabarov@fb.com>
2018-09-05 21:45:40 -0700

    [format.sh] Allow format.sh to check whether the code base does not comply with the clang-format.

--- utils/format.sh
+++ utils/format.sh
@@ -5,12 +5,60 @@ set -euo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR/.."
 
-if ! command -v clang-format; then
-    echo "ERROR: can't find clang-format in your path."
+CLANG_COMMAND=${CLANG_COMMAND:-clang-format}
+
+print_usage() {
+  echo "Missing format option:"
+  echo "  Run: './format.sh fix' if you want to fix format."
+  echo "  Run: './format.sh check' if you want to check format."
+}
+
+fix_format() {
+  find lib tests/unittests/ tools/ include examples \
+    -name \*.h -print0 \
+    -o -name \*.cpp -print0 \
+  | xargs -0 -P8 -n1 $CLANG_COMMAND -i;
+}
+
+check_format() {
+  touch pre.status
+  touch post.status
+
+  git status > pre.status
+  fix_format
+  git status > post.status
+
+  if ! diff pre.status post.status; then
+    echo "ERROR: files need to be formatted"
+    echo "***pre.status***"
+    cat pre.status
+    echo "***post.status***"
+    cat post.status
+    echo "***git diff***"
+    git diff master
+
     exit 1
+  fi
+}
+
+if [[ -n ${1:0} ]]; then
+  if ! command -v $CLANG_COMMAND; then
+    echo "ERROR: can't find clang-format in your path. Tried: $CLANG_COMMAND"
+    exit 1
+  fi
+
+  if [ "$1" = "fix" ]; then
+    echo "Running fix format."
+    fix_format
+  elif [ "$1" = "check" ]; then
+    echo "Running check format."
+    check_format
+  else
+    print_usage
+    exit 1
+  fi
+else
+  print_usage
+  exit 1
 fi
 
-find lib tests/unittests/ tools/ include examples \
-     -name \*.h -print0 \
-     -o -name \*.cpp -print0 \
-    | xargs -0 -P8 -n1 clang-format -i
