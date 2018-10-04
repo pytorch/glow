47cafb2257684d0d88248147169661d69a81e646
Quentin Colombet <qcolombet@fb.com>
2018-10-03 20:47:38 -0700

    [Backend][Interpreter] Implement FP16 BatchedAdd
    *Description*
    This commit templatizes the implementation of BatchedAdd and adds the
    dispatch code to execute the single precision or half precision
    variant for floating point types.
    
    *Testing*
    Added test case
    
    Related to #1329

--- lib/Backends/Interpreter/InterpreterFunction.h
+++ lib/Backends/Interpreter/InterpreterFunction.h
@@ -123,6 +123,7 @@ private:
   void fwdElementMaxInst_FloatImpl(const ElementMaxInst *I);
 
   void fwdBatchedAddInst_I8Impl(const BatchedAddInst *I);
+  template <typename ElemTy>
   void fwdBatchedAddInst_FloatImpl(const BatchedAddInst *I);
   ///@}
 };
