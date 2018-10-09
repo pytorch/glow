7ac63ef6dc4842dab4bf228670e8150e7eebd742
Quentin Colombet <qcolombet@fb.com>
2018-10-09 14:07:45 -0700

    [Converter] Add a helper class to converter a function from type A to type B
    *Description*
    This patch specializes the FunctionConverter class to feature a converter
    from one element type to another.
    In essence this new TypeAToTypeBFunctionConverter class scans the
    function, morph the node from TypeA to TypeB and inserts the right
    conversions to/from typeA and typeB around the converted nodes.
    
    This initial version follows the same schema as the quantization
    of function, meaning there are no attempt to eliminate useless conversion.
    This feature will be added later.
    
    *Testing*
    Tests added.

---
+++ include/glow/Converter/TypeAToTypeBFunctionConverter.h
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2018-present, Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef GLOW_CONVERTER_TYPEATOTYPEBFUNCTIONCONVERTER_H
+#define GLOW_CONVERTER_TYPEATOTYPEBFUNCTIONCONVERTER_H
+
+#include "FunctionConverter.h"
+
+#include "glow/Base/Traits.h" // For KindSet.
+#include "glow/Base/Type.h"
+
+namespace glow {
+
+class Function;
+class Module;
+
+/// This helper class converts values of typeA into values of typeB.
+/// The nodes producing these values are morphed into the new type
+/// and proper conversions from/to typeA and typeB are inserted.
+class TypeAToTypeBFunctionConverter : public FunctionConverter {
+protected:
+  /// Module of the function to be converted.
+  Module &mod_;
+  /// Destination type of the conversions.
+  ElemKind dstKind_;
+  /// Source type of the conversions. I.e., the values of this
+  /// element type are going to be converted.
+  ElemKind srcKind_;
+  /// Set of node kinds that should not be converted.
+  KindSet doNotConvertKinds_;
+
+  /// If the element type of \p out is srcKind_ returns a similarly shaped type
+  /// using dstKind_. Otherwise returns nullptr.
+  /// \see FunctionConverter::getTargetTypeForOutput to know how this
+  /// is used.
+  TypeRef getTargetTypeForOutput(const NodeValue &out) const override;
+
+  /// If the element type of the \p idx-th input of \p use is srcKind_
+  /// returns a similarly shaped type using dstKind_. Otherwise returns nullptr.
+  /// \see FunctionConverter::getTargetTypeForInput to know how this
+  /// is used.
+  TypeRef getTargetTypeForInput(const Node &use, unsigned idx) const override;
+
+  /// Create a node that converts \p val to \p destTy.
+  /// \p val and \p destTy must have the same shape.
+  Node *createConversion(NodeValue &val, TypeRef destTy) override;
+
+  /// Check if \p node can be converted.
+  bool canConvert(const Node &node) const override;
+
+public:
+  /// Create a type converter from \p fromKind to \p toKind for \p F.
+  /// If \p doNotConvertKinds is not nullptr, the nodes which kind
+  /// is in this set won't be converted.
+  TypeAToTypeBFunctionConverter(Function &F, ElemKind fromKind, ElemKind toKind,
+                                const KindSet *doNotConvertKinds = nullptr);
+};
+} // namespace glow
+#endif
---
+++ lib/Converter/TypeAToTypeBFunctionConverter.cpp
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2018-present, Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "glow/Converter/TypeAToTypeBFunctionConverter.h"
+
+#include "glow/Graph/Graph.h"
+
+using namespace glow;
+
+TypeAToTypeBFunctionConverter::TypeAToTypeBFunctionConverter(
+    Function &F, ElemKind fromKind, ElemKind toKind,
+    const KindSet *doNotConvertKinds)
+    : FunctionConverter(F), mod_(*F.getParent()), dstKind_(toKind),
+      srcKind_(fromKind) {
+  if (doNotConvertKinds) {
+    doNotConvertKinds_ = *doNotConvertKinds;
+  }
+}
+
+bool TypeAToTypeBFunctionConverter::canConvert(const Node &node) const {
+  if (doNotConvertKinds_.count(node.getKind())) {
+    return false;
+  }
+  return FunctionConverter::canConvert(node);
+}
+
+TypeRef TypeAToTypeBFunctionConverter::getTargetTypeForOutput(
+    const NodeValue &out) const {
+  if (out.getType()->getElementType() != srcKind_) {
+    return nullptr;
+  }
+  return mod_.uniqueType(dstKind_, out.dims());
+}
+
+TypeRef
+TypeAToTypeBFunctionConverter::getTargetTypeForInput(const Node &use,
+                                                     unsigned idx) const {
+  return getTargetTypeForOutput(use.getNthInput(idx));
+}
+
+Node *TypeAToTypeBFunctionConverter::createConversion(NodeValue &val,
+                                                      TypeRef destTy) {
+  assert(((destTy->getElementType() == dstKind_ &&
+           val.getType()->getElementType() == srcKind_) ||
+          (destTy->getElementType() == srcKind_ &&
+           val.getType()->getElementType() == dstKind_)) &&
+         "Unexpected conversion type");
+  return function_.createConvertTo(val.getNode()->getName(), val, destTy);
+}
