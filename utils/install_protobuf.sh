a4cd6622f6a93761b243e3d2effff82f0b9dd103
Roman Dzhabarov <rdzhabarov@fb.com>
2018-10-09 19:33:32 -0700

    [travis] Cache protobuf compilation.

--- utils/install_protobuf.sh
+++ utils/install_protobuf.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -ex
+
+pb_dir="$HOME/.cache/pb"
+mkdir -p "$pb_dir"
+
 wget https://github.com/google/protobuf/releases/download/v2.6.1/protobuf-2.6.1.tar.gz
-tar -xzf protobuf-2.6.1.tar.gz
-cd protobuf-2.6.1 && ./configure CC=gcc CXX=g++ CXX_FOR_BUILD=g++ --prefix=/usr && make && sudo make install
+tar -xzf protobuf-2.6.1.tar.gz -C "$pb_dir" --strip-components 1
+cd "$pb_dir" && ./configure CC=gcc CXX=g++ CXX_FOR_BUILD=g++ --prefix=/usr && make -j2 && sudo make install
