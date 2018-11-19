bc88c56b4d3788cb5ec66c0bb923d46b43d40ab2
Bert Maher <bertrand@fb.com>
2018-11-19 10:05:53 -0800

    nits

--- tools/png2bin/png2bin.cpp
+++ tools/png2bin/png2bin.cpp
@@ -1,3 +1,19 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "glow/Base/IO.h"
 #include "glow/Base/Image.h"
 #include "glow/Base/Tensor.h"
@@ -18,4 +34,5 @@ int main(int argc, char **argv) {
   TensorQuantizationParams TQP{1.0f / 127.0f, 0};
   Tensor qpng = quantization::quantizeTensor(png, TQP);
   writeToFile(qpng, outfile);
+  return 0;
 }
