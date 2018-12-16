ad7c0f8821e15ffb23e9dd33c40253237cf81f1b
Jordan Fix <jfix@fb.com>
2018-12-16 13:10:35 +0200

    Allow for creating quantized Tanh directly
    This allows the OperatorTest for quantized Tanh to be specified correctly. Previously it directly created the lookup table, however some backends may want to implement it differently/prevent lowering.

--- tools/ClassGen/InstrGen.cpp
+++ tools/ClassGen/InstrGen.cpp
@@ -391,7 +391,8 @@ int main(int argc, char **argv) {
           "Src",
       })
       .dataParallel()
-      .autoVerify(VerifyKind::SameType, {"Dest", "Src"})
+      .autoVerify(VerifyKind::SameElementType, {"Dest", "Src"})
+      .autoVerify(VerifyKind::SameShape, {"Dest", "Src"})
       .autoIRGen();
 
   //===--------------------------------------------------------------------===//
