30ab996bb9ab2e3559879574d4a339e30821b0fd
Bert Maher <bertrand@fb.com>
2018-12-21 15:49:44 -0800

    [utils] Utility for running caffe2 model w/o input

---
+++ utils/caffe2_model_runner.py
@@ -0,0 +1,64 @@
+from caffe2.proto import caffe2_pb2
+from caffe2.python import workspace
+from google.protobuf import text_format
+import argparse
+
+
+def fix_tensor_fills(init_net_file):
+    init_net_pb = open(init_net_file, "rb").read()
+    init_net = caffe2_pb2.NetDef()
+    init_net.ParseFromString(init_net_pb)
+    for op in init_net.op:
+        if any("indices" in x for x in op.output):
+            op.type = "GivenTensorInt64Fill"
+        elif any("lengths" in x for x in op.output):
+            op.type = "GivenTensorIntFill"
+    open(init_net_file + "txt", "w").write(
+        text_format.MessageToString(init_net)
+    )
+    open(init_net_file, "wb").write(init_net.SerializeToString())
+
+
+def read_init_net_pbtxt(init_net_file):
+    init_net_txt = open(init_net_file, "r").read()
+    init_net = caffe2_pb2.NetDef()
+    text_format.Merge(init_net_txt, init_net)
+    return init_net
+
+
+def read_init_net(init_net_file):
+    init_net_pb = open(init_net_file, "rb").read()
+    init_net = caffe2_pb2.NetDef()
+    init_net.ParseFromString(init_net_pb)
+    return init_net
+
+
+def read_predict_net(predict_net_file):
+    predict_net_txt = open(predict_net_file, "r").read()
+    predict_net = caffe2_pb2.NetDef()
+    predict_net.name = "the_model"
+    text_format.Merge(predict_net_txt, predict_net)
+    return predict_net
+
+
+def run(predict_net, init_net):
+    workspace.ResetWorkspace()
+    workspace.RunNetOnce(init_net)
+    workspace.CreateNet(predict_net)
+    workspace.RunNet(predict_net.name)
+    out = workspace.FetchBlob(predict_net.external_output[0])
+    print(out)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("predict_net", default="predict_net.pbtxt", nargs="?")
+    parser.add_argument("init_net", default="init_net.pb", nargs="?")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    init_net = read_init_net(args.init_net)
+    predict_net = read_predict_net(args.predict_net)
+    run(predict_net, init_net)
