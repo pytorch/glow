ce26c597b353dee292f3ef34b698a3e9cdaddd8b
Garret Catron <gcatron@fb.com>
2019-01-23 16:35:52 -0800

    Added Provisioner and unittest

---
+++ lib/Runtime/Provisioner/Provisioner.cpp
@@ -0,0 +1,111 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "glow/Runtime/Provisioner/Provisioner.h"
+#include "glow/Backends/BackendUtils.h"
+#include "glow/Backends/CompiledFunction.h"
+#include "glow/Graph/Graph.h"
+
+#include <future>
+#include <queue>
+
+using namespace glow;
+using namespace runtime;
+
+ResultCode
+Provisioner::provision(std::vector<std::unique_ptr<DAGNode>> &networks,
+                       DeviceIDtoManagerMapTy &devices, Module &module) {
+  // For the first pass we will just assign and load devices in order and update
+  // the deviceID field of the node.
+  std::queue<std::vector<DAGNode *>> nextNode;
+  // Process head node, this does not contain a function but serves as an entry
+  // point for the network. We build a vector of nodes, containing all
+  // sub-functions that use the same constants. Later we will group by
+  // logicalDevice.
+  for (unsigned int i = 0; i < networks[0]->children.size(); i++) {
+    std::vector<DAGNode *> newSet;
+    for (auto &node : networks) {
+      newSet.push_back(node->children[i]);
+    }
+    nextNode.push(newSet);
+  }
+  auto currDevice = devices.begin();
+  while (!nextNode.empty()) {
+    FunctionMapTy compiledFunctions;
+    std::vector<std::unique_ptr<CompiledFunction>> functions;
+    auto nodes = nextNode.front();
+    nextNode.pop();
+
+    // Add child nodes to the queue.
+    for (unsigned int i = 0; i < nodes[0]->children.size(); i++) {
+      std::vector<DAGNode *> newSet;
+      for (auto node : nodes) {
+        newSet.push_back(node->children[i]);
+      }
+      nextNode.push(newSet);
+    }
+    // Assign collection of nodes to a device, compile and load the device.
+    // We will do a round robin assignment of nodes. If there is not space we
+    // will return an error.
+    // TODO Add ability to try against another device when currDevice has
+    // insufficient space.
+
+    // Set backend to match the device.
+    backend_.reset(createBackend(currDevice->second->getBackendKind()));
+
+    // Iterate over the nodes, compile them and add them to compiledFunctions.
+    for (auto node : nodes) {
+      node->deviceID = currDevice->first;
+      Function *function = module.getFunction(node->name);
+      auto compiled = backend_->compile(function);
+      node->runtimeBundle = compiled->getRuntimeBundle();
+      compiledFunctions.emplace(node->name, compiled.get());
+      functions.push_back(std::move(compiled));
+    }
+
+    // Check if sufficient space on device. Currently requiring a buffer
+    // over the size of constants determined by NETWORK_PADDING_FACTOR.
+    auto availableMemory = currDevice->second->getAvailableMemory();
+    auto requiredMemory = NETWORK_PADDING_FACTOR *
+                          nodes[0]->runtimeBundle.getConstantWeightSize();
+    if (availableMemory < requiredMemory) {
+      return ResultCode::Failed;
+    }
+
+    // Load functions on device. Create a promise that is passed into the
+    // callback that is passed to the deviceManager alongside a reference to the
+    // module and a map of the functions. Then wait on the future for the
+    // results.
+    std::promise<bool> addNetwork;
+    auto ready = addNetwork.get_future();
+    currDevice->second->addNetwork(
+        &module, compiledFunctions,
+        [&addNetwork](const Module *, ResultCode result) {
+          addNetwork.set_value(result == ResultCode::Ready);
+        });
+    auto result = ready.get();
+    if (!result) {
+      return ResultCode::Failed;
+    }
+
+    currDevice++;
+    // Handle wrapping around to start of devices again.
+    if (currDevice == devices.end()) {
+      currDevice = devices.begin();
+    }
+  }
+  return ResultCode::Ready;
+};
---
+++ tests/unittests/ProvisionerTest.cpp
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "glow/Runtime/Provisioner/Provisioner.h"
+#include "CPUDeviceManager.h"
+
+#include "gtest/gtest.h"
+
+using namespace glow;
+using namespace glow::runtime;
+using DAGNodePairTy = std::pair<std::vector<std::unique_ptr<DAGNode>>,
+                                std::vector<std::unique_ptr<DAGNode>>>;
+
+class ProvisionerTest : public ::testing::Test {};
+std::unique_ptr<Module> setupModule(uint functionCount) {
+  std::unique_ptr<Module> module = std::make_unique<Module>();
+  for (unsigned int i = 0; i < functionCount; i++) {
+    Function *F = module->createFunction("function" + std::to_string(i));
+    auto *X = module->createPlaceholder(ElemKind::FloatTy, {3},
+                                        "X" + std::to_string(i), false);
+    auto *pow = F->createPow("Pow" + std::to_string(i), X, 2.0);
+    F->createSave("save" + std::to_string(i), pow);
+  }
+  return module;
+}
+
+DAGNodePairTy setupDAG(uint rootCount, uint childCount) {
+  std::vector<std::unique_ptr<DAGNode>> networks;
+  std::vector<std::unique_ptr<DAGNode>> children;
+  uint currentFunction = 0;
+  for (unsigned int root = 0; root < rootCount; root++) {
+    auto rootNode = std::make_unique<DAGNode>();
+    auto firstNode = std::make_unique<DAGNode>();
+    rootNode->name = "root" + std::to_string(root);
+    rootNode->children.push_back(firstNode.get());
+    firstNode->name = "function" + std::to_string(currentFunction);
+    currentFunction++;
+    for (unsigned int child = 0; child < childCount; child++) {
+      auto newChild = std::make_unique<DAGNode>();
+      newChild->name = "function" + std::to_string(currentFunction);
+      currentFunction++;
+      firstNode->children.push_back(newChild.get());
+      children.push_back(std::move(newChild));
+    }
+    networks.push_back(std::move(rootNode));
+    children.push_back(std::move(firstNode));
+  }
+  return std::make_pair(std::move(networks), std::move(children));
+}
+
+TEST_F(ProvisionerTest, provisionDag) {
+  auto mod = setupModule(6);
+  auto networks = setupDAG(2, 0);
+  auto provisioner = Provisioner();
+  std::map<DeviceIDTy, std::unique_ptr<DeviceManager>> devices;
+  for (int i = 0; i < 6; i++) {
+    std::unique_ptr<DeviceManager> device(new CPUDeviceManager);
+    devices.emplace(i, std::move(device));
+  }
+  auto result = provisioner.provision(networks.first, devices, *mod.get());
+  EXPECT_EQ(result, ResultCode::Ready);
+}
