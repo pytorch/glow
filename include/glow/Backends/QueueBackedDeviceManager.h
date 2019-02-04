3fddae49663c0807a3ce0171386be49e100d9a6a
Bert Maher <bertmaher@users.noreply.github.com>
2019-02-04 12:43:41 -0800

    [device manager] Reorganize libs for compatibility with Buck (#2341)
    * [device manager] Reorganize libs for compatibility with Buck
    
    * DeviceManager exposes ThreadPool

--- include/glow/Backends/QueueBackedDeviceManager.h
+++ include/glow/Backends/QueueBackedDeviceManager.h
@@ -17,6 +17,7 @@
 #define GLOW_BACKENDS_QUEUEBACKEDDEVICEMANAGER_H
 
 #include "glow/Backends/DeviceManager.h"
+#include "glow/Runtime/RuntimeTypes.h"
 #include "glow/Support/ThreadPool.h"
 
 #include <atomic>
@@ -32,20 +33,32 @@ protected:
   std::atomic<runtime::RunIdentifierTy> nextIdentifier_{1};
 
 public:
-  QueueBackedDeviceManager(BackendKind backend, llvm::StringRef name);
-  virtual ~QueueBackedDeviceManager();
+  QueueBackedDeviceManager(BackendKind backend, llvm::StringRef name)
+      : DeviceManager(backend, name), workThread_(1) {}
+
+  virtual ~QueueBackedDeviceManager() {
+    stop(true); // will join workThread_
+  }
 
   /// Initialize the device.
-  void init() override;
+  void init() override {}
 
   /// Load the provided module into the device, readyCB will be called when
   /// ready to use
   void addNetwork(const Module *module, FunctionMapTy functions,
-                  ReadyCBTy readyCB) override;
+                  ReadyCBTy callback) override {
+    workThread_.submit([this, module, f = std::move(functions),
+                        c = std::move(callback)]() mutable {
+      addNetworkImpl(module, std::move(f), std::move(c));
+    });
+  }
 
   /// Remove (and delete) the provided network and all it's functions, freeing
   /// up space on the device.
-  void evictNetwork(llvm::StringRef functionName) override;
+  void evictNetwork(llvm::StringRef functionName) override {
+    workThread_.submit(
+        [this, functionName] { evictNetworkImpl(functionName); });
+  }
 
   /// Execute the named Function in an already provided network on the device.
   /// functionName must match the name of a function already added.
@@ -53,10 +66,20 @@ public:
   /// with the Context results filled.
   runtime::RunIdentifierTy runFunction(std::string functionName,
                                        std::unique_ptr<Context> ctx,
-                                       ResultCBTy resultCB) override;
+                                       ResultCBTy callback) override {
+
+    runtime::RunIdentifierTy id = nextIdentifier_++;
+    workThread_.submit([this, id, functionName = std::move(functionName),
+                        ctx = std::move(ctx),
+                        callback = std::move(callback)]() mutable {
+      runFunctionImpl(id, std::move(functionName), std::move(ctx),
+                      std::move(callback));
+    });
+    return id;
+  }
 
   /// Stops execution and shuts down the Device.
-  void stop(bool block = true) override;
+  void stop(bool block = true) override { workThread_.stop(block); }
 
 protected:
   /// Operator handling methods to be implemented in subclasses (i.e. per Device
