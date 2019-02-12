06bb84a3e7b3b523c3837defc8db4d1499e1c257
Gabriel Agamennoni <gabriel.agamennoni@oculus.com>
2019-02-12 13:48:39 -0800

    Update Caffe2ImporterTest.cpp to check weight and bias values

--- tests/unittests/Caffe2ImporterTest.cpp
+++ tests/unittests/Caffe2ImporterTest.cpp
@@ -612,6 +612,36 @@ TEST(caffe2, FC) {
       llvm::dyn_cast<FullyConnectedNode>(saveNode->getInput().getNode());
   ASSERT_TRUE(fcNode);
 
+  // Check the numerical values of the weights and biases.
+  {
+    const Constant* constant = mod.getConstantByName("weights");
+    ASSERT_TRUE(constant);
+    const Tensor& weights = constant->getPayload();
+    const std::vector<size_t> expectedDimensions = {3, 4};
+    const std::vector<float> expectedValues = {1.0f, 4.0f, 7.0f, 10.0f,   //
+                                               2.0f, 5.0f, 8.0f, 11.0f,   //
+                                               3.0f, 6.0f, 9.0f, 12.0f};
+    EXPECT_EQ(expectedDimensions, weights.dims().vec());
+    ASSERT_EQ(expectedValues.size(), weights.size());
+    const auto& elements = weights.getHandle();
+    for (size_t i = 0; i < expectedValues.size(); ++i) {
+      EXPECT_FLOAT_EQ(expectedValues.at(i), elements.raw(i)) << "Where i = " << i;
+    }
+  }
+  {
+    const Constant* constant = mod.getConstantByName("biases");
+    ASSERT_TRUE(constant);
+    const Tensor& bias = constant->getPayload();
+    const std::vector<size_t> expectedDimensions = {4};
+    const std::vector<float> expectedValues = {0.1f, 0.2f, 0.3f, 0.4f};
+    EXPECT_EQ(expectedDimensions, bias.dims().vec());
+    ASSERT_EQ(expectedValues.size(), bias.size());
+    const auto& elements = bias.getHandle();
+    for (size_t i = 0; i < expectedValues.size(); ++i) {
+      EXPECT_FLOAT_EQ(expectedValues.at(i), elements.raw(i)) << "Where i = " << i;
+    }
+  }
+
   // We don't actually check that the output is correct, because this is
   // already covered in the Operator.FC/* tests.
 }
@@ -654,6 +684,36 @@ TEST(caffe2, FCWithFlatten) {
   ASSERT_TRUE(fcNode);
   auto *reshape = llvm::dyn_cast<ReshapeNode>(fcNode->getInput());
   ASSERT_TRUE(reshape);
+
+  // Check the numerical values of the weights and biases.
+  {
+    const Constant* constant = mod.getConstantByName("weights");
+    ASSERT_TRUE(constant);
+    const Tensor& weights = constant->getPayload();
+    const std::vector<size_t> expectedDimensions = {3, 4};
+    const std::vector<float> expectedValues = {1.0f, 4.0f, 7.0f, 10.0f,   //
+                                               2.0f, 5.0f, 8.0f, 11.0f,   //
+                                               3.0f, 6.0f, 9.0f, 12.0f};
+    EXPECT_EQ(expectedDimensions, weights.dims().vec());
+    ASSERT_EQ(expectedValues.size(), weights.size());
+    const auto& elements = weights.getHandle();
+    for (size_t i = 0; i < expectedValues.size(); ++i) {
+      EXPECT_FLOAT_EQ(expectedValues.at(i), elements.raw(i)) << "Where i = " << i;
+    }
+  }
+  {
+    const Constant* constant = mod.getConstantByName("biases");
+    ASSERT_TRUE(constant);
+    const Tensor& bias = constant->getPayload();
+    const std::vector<size_t> expectedDimensions = {4};
+    const std::vector<float> expectedValues = {0.1f, 0.2f, 0.3f, 0.4f};
+    EXPECT_EQ(expectedDimensions, bias.dims().vec());
+    ASSERT_EQ(expectedValues.size(), bias.size());
+    const auto& elements = bias.getHandle();
+    for (size_t i = 0; i < expectedValues.size(); ++i) {
+      EXPECT_FLOAT_EQ(expectedValues.at(i), elements.raw(i)) << "Where i = " << i;
+    }
+  }
 
   // We don't actually check that the output is correct, because this is
   // already covered in the Operator.FCWithFlatten/* tests.
