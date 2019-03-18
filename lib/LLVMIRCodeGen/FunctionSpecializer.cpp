acdfd24ea88170298f79bd3834891c2e20a61f16
Roman Levenstein <rlevenstein@fb.com>
2019-03-18 11:27:00 -0700

    [function-specializer] Do not try to specialize function declarations

--- lib/LLVMIRCodeGen/FunctionSpecializer.cpp
+++ lib/LLVMIRCodeGen/FunctionSpecializer.cpp
@@ -247,8 +247,13 @@ class FunctionSpecializer {
            "Only calls inside the entry function are specialized");
     (void)caller;
     // Do not specialize any LLVM internal functions.
-    if (callee && callee->getName().startswith("llvm."))
+    if (callee && callee->getName().startswith("llvm.")) {
       return false;
+    }
+    // Do not specialize declarations.
+    if (callee && callee->isDeclaration()) {
+      return false;
+    }
     // Do not specialize noinline functions, because it does not improve
     // anything.
     return callee != nullptr &&
