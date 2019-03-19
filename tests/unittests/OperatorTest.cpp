d26846240555f7eecb18b5e2b937394dd5059773
Jordan Fix <1198212+jfix71@users.noreply.github.com>
2019-03-18 20:35:41 -0700

    [OperatorTest] Add fp16 for ReluSimple

--- tests/unittests/OperatorTest.cpp
+++ tests/unittests/OperatorTest.cpp
@@ -882,27 +882,41 @@ TEST_P(OperatorTest, weightedSum) {
   EXPECT_NEAR(resultH.at({1, 1}), 80.4, 1E-5);
 }
 
-/// Verify that the RELU operator works correctly.
-TEST_P(OperatorTest, ReluSimple) {
-  auto *in = mod_.createPlaceholder(ElemKind::FloatTy, {7}, "in", false);
-  auto *relu = F_->createRELU("relu", in);
-  auto *save = F_->createSave("relu", relu);
-  auto *result = bindings_.allocate(save->getPlaceholder());
+/// Helper to test ReluSimple using \p DTy.
+template <typename DataType>
+static void testReluSimple(glow::PlaceholderBindings &bindings,
+                           glow::Module &mod, glow::Function *F,
+                           glow::ExecutionEngine &EE, ElemKind DTy) {
+  auto *in = mod.createPlaceholder(DTy, {7}, "in", false);
+  auto *relu = F->createRELU("relu", in);
+  auto *save = F->createSave("relu", relu);
+  auto *result = bindings.allocate(save->getPlaceholder());
 
-  bindings_.allocate(in)->getHandle() = {0, -1, -2, -3, 4, 5, 6};
+  bindings.allocate(in)->getHandle<DataType>() = {0, -1, -2, -3, 4, 5, 6};
 
-  EE_.compile(CompilationMode::Infer, F_);
-  EE_.run(bindings_);
+  EE.compile(CompilationMode::Infer, F);
+  EE.run(bindings);
 
-  auto resultH = result->getHandle();
+  auto resultH = result->getHandle<DataType>();
 
   for (size_t i = 0; i < 7; i++) {
     if (i < 4) {
-      EXPECT_EQ(resultH.raw(i), 0);
+      EXPECT_EQ(resultH.raw(i), static_cast<DataType>(0));
     } else {
-      EXPECT_EQ(resultH.raw(i), i);
+      EXPECT_EQ(resultH.raw(i), static_cast<DataType>(i));
     }
   }
+}
+
+/// Verify that the RELU operator works correctly for Float.
+TEST_P(OperatorTest, ReluSimple_Float) {
+  testReluSimple<float>(bindings_, mod_, F_, EE_, ElemKind::FloatTy);
+}
+
+/// Verify that the RELU operator works correctly for Float16.
+TEST_P(OperatorTest, ReluSimple_Float16) {
+  ENABLED_BACKENDS(Interpreter);
+  testReluSimple<float16_t>(bindings_, mod_, F_, EE_, ElemKind::Float16Ty);
 }
 
 TEST_P(OperatorTest, TopK) {
