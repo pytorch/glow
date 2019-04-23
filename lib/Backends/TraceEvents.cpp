5221165c2e30029dcfab35824f52cb09d2eb1b25
Nick Gibson <nickg@fb.com>
2019-04-23 16:47:14 -0700

    [tracing] [trivial] switch to std::system_clock for timestamps (#2778)

--- lib/Backends/TraceEvents.cpp
+++ lib/Backends/TraceEvents.cpp
@@ -66,7 +66,7 @@ void TraceEvent::dumpTraceEvents(
 
 uint64_t TraceEvent::now() {
   return std::chrono::duration_cast<std::chrono::microseconds>(
-             std::chrono::steady_clock::now().time_since_epoch())
+             std::chrono::system_clock::now().time_since_epoch())
       .count();
 }
 
