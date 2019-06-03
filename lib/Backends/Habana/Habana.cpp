57f7bc8cd7fd5d43d3be3ecb74fa7628131896b4
Bert Maher <bertrand@fb.com>
2019-06-03 12:23:11 -0700

    Use pooling param structure from SDK headers (#3022)
    Summary:
    ns_SpatialReduction is the vendor-provided struct; no need to define
    our own.
    Pull Request resolved: https://github.com/pytorch/glow/pull/3022
    
    Differential Revision: D15575080
    
    fbshipit-source-id: 2d2e47ea7761b0adb7adf553bb65b46b868a46db

--- lib/Backends/Habana/Habana.cpp
+++ lib/Backends/Habana/Habana.cpp
@@ -93,28 +93,6 @@ static std::string getKernelName(llvm::StringRef kernelBase, ElemKind kind) {
 }
 
 namespace {
-/// Parameters for pooling operation.
-struct synPoolParams {
-  // Padding
-  int pWbegin;
-  int pWend;
-  int pHbegin;
-  int pHend;
-  // Kernel
-  int kW;
-  int kH;
-  // Stride
-  int sW;
-  int sH;
-  // Dilation
-  int dilW;
-  int dilH;
-  int poolingConvention;
-
-  synPoolParams()
-      : pWbegin(0), pWend(0), pHbegin(0), pHend(0), kW(1), kH(1), sW(1), sH(1),
-        dilW(1), dilH(1), poolingConvention(0) {}
-};
 
 /// Enum describing how the tensor will be used by the model.
 enum class IOType {
@@ -537,26 +515,26 @@ static std::unique_ptr<synConvolutionParams> makeSynConvolutionParams(
   return params;
 }
 
-static std::unique_ptr<synPoolParams>
+static std::unique_ptr<ns_SpatialReduction::Params>
 makeSynPoolParams(llvm::ArrayRef<unsigned_t> kernel,
                   llvm::ArrayRef<unsigned_t> stride,
                   llvm::ArrayRef<unsigned_t> pad) {
-  auto params = llvm::make_unique<synPoolParams>();
+  auto params = llvm::make_unique<ns_SpatialReduction::Params>();
 
   // Kernel
-  params->kW = kernel[0];
-  params->kH = kernel[1];
+  params->kernel_w = kernel[0];
+  params->kernel_h = kernel[1];
   // Stride
-  params->sW = stride[0];
-  params->sH = stride[1];
+  params->stride_w = stride[0];
+  params->stride_h = stride[1];
   // Padding
-  params->pHbegin = pad[0];
-  params->pWbegin = pad[1];
-  params->pHend = pad[2];
-  params->pWend = pad[3];
+  params->pad_h_begin = pad[0];
+  params->pad_w_begin = pad[1];
+  params->pad_h_end = pad[2];
+  params->pad_w_end = pad[3];
   // Dilation
-  params->dilW = 1;
-  params->dilH = 1;
+  params->dilation_w = 1;
+  params->dilation_h = 1;
 
   return params;
 }
@@ -736,7 +714,7 @@ HabanaBackend::compile(Function *F, const BackendOptions &opts) const {
   // until the compilation is done.
   std::vector<std::unique_ptr<synConvolutionParams>> convParams;
   std::vector<std::unique_ptr<synTransposeParams>> transposeParams;
-  std::vector<std::unique_ptr<synPoolParams>> poolParams;
+  std::vector<std::unique_ptr<ns_SpatialReduction::Params>> poolParams;
   std::vector<std::unique_ptr<synFCParams>> fcParams;
   std::vector<std::unique_ptr<ns_FullyConnected::Params>> fcFp32Params;
   std::vector<std::unique_ptr<ns_Reduction::Params>> reductionParams;
@@ -871,7 +849,7 @@ HabanaBackend::compile(Function *F, const BackendOptions &opts) const {
     }
     case Kinded::Kind::MaxPoolNodeKind: {
       auto *PI = llvm::cast<MaxPoolNode>(&I);
-      std::unique_ptr<synPoolParams> params =
+      auto params =
           makeSynPoolParams(PI->getKernels(), PI->getStrides(), PI->getPads());
       chk(synCreateGenericNode(
           &tensors[PI->getInput()].get(), &tensors[PI].get(), 1, 1,
@@ -883,7 +861,7 @@ HabanaBackend::compile(Function *F, const BackendOptions &opts) const {
     }
     case Kinded::Kind::AvgPoolNodeKind: {
       auto *PI = llvm::cast<AvgPoolNode>(&I);
-      std::unique_ptr<synPoolParams> params =
+      auto params =
           makeSynPoolParams(PI->getKernels(), PI->getStrides(), PI->getPads());
       chk(synCreateGenericNode(
           &tensors[PI->getInput()].get(), &tensors[PI].get(), 1, 1,
