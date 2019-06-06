c01b99dac45174f4a4ad0d6b8ca519eca545ead6
Saleem Abdulrasool <compnerd@compnerd.org>
2019-06-06 08:53:43 -0700

    build: try to goad CMake into understanding dependencies (#3017)
    Summary:
    Change the build rules for the JIT to use some more trickery to have CMake track
    dependencies itself.  First, we swap out the compiler around the definition of
    JIT module to ensure that we build with Clang.  Second, we build the library as
    a module to control the link rule invocation.  Third, we change the linker
    language to LLIR and provide a custom linker rule to invoke the LLVM linker.
    This should hopefully provide more reliable null builds.  As a happy side
    effect, the logic is easier to read.
    
    Documentation:
    Pull Request resolved: https://github.com/pytorch/glow/pull/3017
    
    Reviewed By: bertmaher
    
    Differential Revision: D15582438
    
    Pulled By: compnerd
    
    fbshipit-source-id: 9a5be1ba400e5c5fe11014ef4c8b5ab4acc2ac98

--- lib/Backends/CPU/CPUBackend.cpp
+++ lib/Backends/CPU/CPUBackend.cpp
@@ -36,7 +36,7 @@ using namespace glow;
 /// The resulting file is included here to compile the bitcode image into our
 /// library.
 static const unsigned char libjit_bc[] = {
-#include "glow/CPU/libjit_bc.inc"
+#include "libjit.inc"
 };
 static const size_t libjit_bc_size = sizeof(libjit_bc);
 
