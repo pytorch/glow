437a45dcb51f3af98c8d104f410b12421adb3953
Nick Gibson <nickg@fb.com>
2019-06-25 16:57:14 -0700

    [trivial] switch new OCL event to Complete event

--- lib/Backends/OpenCL/OpenCL.cpp
+++ lib/Backends/OpenCL/OpenCL.cpp
@@ -459,9 +459,11 @@ void OpenCLFunction::executeConvolution(const OCLConvolutionInst *CC,
     src.append(reinterpret_cast<const char *>(kernels_fwd_conv_cl_src),
                kernels_fwd_conv_cl_src_size);
   }
-  TRACE_EVENT_BEGIN(executionContext, TraceLevel::RUNTIME, "convCreateProgram");
+
+  TRACE_EVENT_SCOPE_NAMED(executionContext->getTraceContext(),
+                          TraceLevel::RUNTIME, "convCreateProgram", cpEvent);
   auto prog = createProgram(src, options, commands_);
-  TRACE_EVENT_END(executionContext, TraceLevel::RUNTIME, "convCreateProgram");
+  TRACE_EVENT_SCOPE_END_NAMED(cpEvent);
 
   auto kernelName = isQuantized ? "conv_forward_mem_i8" : "conv_forward_mem";
   auto kernel = createKernel(kernelName, prog);
