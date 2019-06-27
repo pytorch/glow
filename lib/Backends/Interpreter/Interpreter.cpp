8315c492b9ad493353898f4eddf1f9fc19c56902
Max Kazantsev <maximkaz@cadence.com>
2019-06-27 14:48:53 -0700

    Support casts i32<->i64<->float<->float16 in Interpreter (#3179)
    Summary:
    The main purpose of that is constant-folding, that is why only the interpreter
    backend is supported so far. Can be done for CPU backend separately if needed.
    Pull Request resolved: https://github.com/pytorch/glow/pull/3179
    
    Test Plan:
    Added unit tests.
    
    Please see a detailed explanation of how to fill out the fields in the relevant sections in PULL_REQUEST.md.
    
    Differential Revision: D16016739
    
    Pulled By: rdzhabarov
    
    fbshipit-source-id: 92eb37df000d31cc03b1010b431db9073d74a14b

--- lib/Backends/Interpreter/Interpreter.cpp
+++ lib/Backends/Interpreter/Interpreter.cpp
@@ -308,16 +308,21 @@ bool Interpreter::isOpSupported(const NodeInfo &NI) const {
   case Kinded::Kind::IntLookupTableNodeKind:
     return NI.allInputsAndOutputsHaveSameElemKind({ElemKind::Int8QTy});
 
-  case Kinded::Kind::ConvertToNodeKind:
-    return ((NI.getInElemTy(ConvertToNode::InputIdx) == ElemKind::FloatTy) &&
-            ((NI.getOutElemTy(ConvertToNode::ResultIdx) ==
-              ElemKind::Float16Ty) ||
-             (NI.getOutElemTy(ConvertToNode::ResultIdx) ==
-              ElemKind::Int64ITy))) ||
-           ((NI.getInElemTy(ConvertToNode::InputIdx) == ElemKind::Float16Ty) &&
-            (NI.getOutElemTy(ConvertToNode::ResultIdx) == ElemKind::FloatTy)) ||
-           ((NI.getInElemTy(ConvertToNode::InputIdx) == ElemKind::Int64ITy) &&
-            (NI.getOutElemTy(ConvertToNode::ResultIdx) == ElemKind::FloatTy));
+  case Kinded::Kind::ConvertToNodeKind: {
+    auto isConversionSupportedFor = [](ElemKind kind) {
+      switch (kind) {
+      case ElemKind::Float16Ty:
+      case ElemKind::FloatTy:
+      case ElemKind::Int32ITy:
+      case ElemKind::Int64ITy:
+        return true;
+      default:
+        return false;
+      }
+    };
+    return isConversionSupportedFor(NI.getInElemTy(ConvertToNode::InputIdx)) &&
+           isConversionSupportedFor(NI.getOutElemTy(ConvertToNode::ResultIdx));
+  }
 
   case Kinded::Kind::TopKNodeKind:
     return NI.allInputsAndOutputsHaveSameElemKind(
