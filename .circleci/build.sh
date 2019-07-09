9019a07e886448ce97ffebbf080616ed93ce4ffd
zrphercule <zrphercule@gmail.com>
2019-07-09 10:57:21 -0700

    checkout to 7fcfed19e7c4805405f3bec311fc056803ca7afb

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -127,6 +127,7 @@ elif [[ "$CIRCLE_JOB" == "PYTORCH" ]]; then
     cd ..
     git clone https://github.com/pytorch/pytorch.git --recursive
     cd pytorch
+	git checkout 7fcfed19e7c4805405f3bec311fc056803ca7afb
     pip install -r requirements.txt
     sudo /usr/bin/python3.6 setup.py install
     cd ${GLOW_DIR}
