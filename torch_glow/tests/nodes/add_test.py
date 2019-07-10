db6804cc2d8d7c286c5c3f6063339d5922bc2589
Jack Montgomery <jackm321@gmail.com>
2019-07-10 16:17:05 -0700

    Run resnet (#3202)
    Summary:
    Run PyTorch Resnet/Resnext models on Glow except for AdaptiveAvgPool and FC
    
    Documentation:
    doxygen
    Pull Request resolved: https://github.com/pytorch/glow/pull/3202
    
    Test Plan:
    added operator tests
    
    `python setup.py test`
    
    to run example:
    `python setup.py develop --run_cmake --release`
    then
    `python examples/resnet_example.py`
    
    Differential Revision: D16190914
    
    Pulled By: jackm321
    
    fbshipit-source-id: d20b63ce31e4b2dff3fdbec36954ca1cff1412b0

--- torch_glow/tests/nodes/add_test.py
+++ torch_glow/tests/nodes/add_test.py
@@ -14,3 +14,15 @@ def test_add_basic():
   y = torch.randn(4)
 
   jitVsGlow(add_basic, x, y)
+
+# Test of the PyTorch add_ Node on Glow.
+def test_add_inplace():
+
+  def add_inplace(a, b):
+        c = a.add_(b)
+        return c.add_(c)
+
+  x = torch.randn(4)
+  y = torch.randn(4)
+
+  jitVsGlow(add_inplace, x, y)
