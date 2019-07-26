e7eeae78e8564970477d3b4c166b0d03e868e34f
Meghan Lele <meghanl@fb.com>
2019-07-25 18:07:24 -0700

    [PyTorch] Add support for sqrt operator
    Summary:
    This commit adds support for the elementwise sqrt operator to the
    PyTorchModelLoader so that torch.sqrt(a) can be executed on Glow.
    
    Test Plan:
    This commit adds a unit test that runs a graph with two sqrt nodes on Glow and
    the PyTorch CPU backend and checks that the results are identical.

--- torch_glow/src/PyTorchModelLoader.cpp
+++ torch_glow/src/PyTorchModelLoader.cpp
@@ -231,6 +231,17 @@ void PyTorchModelLoader::loadExp(const torch::jit::Node *ptNode) {
   addGlowNodeValue(outputs[0], glowNode->getResult());
 }
 
+void PyTorchModelLoader::loadSqrt(const torch::jit::Node *ptNode) {
+  auto inputs = ptNode->inputs();
+  auto outputs = ptNode->outputs();
+  assert(inputs.size() == 1);
+  assert(outputs.size() == 1);
+
+  glow::NodeValue input = getGlowNodeValue(inputs[0]);
+  glow::PowNode *glowNode = f_->createPow("sqrt", input, /*exp=*/0.5);
+  addGlowNodeValue(outputs[0], glowNode->getResult());
+}
+
 void PyTorchModelLoader::loadConvolution(const torch::jit::Node *ptNode) {
   auto inputs = ptNode->inputs();
   auto outputs = ptNode->outputs();
@@ -641,6 +652,11 @@ void PyTorchModelLoader::populateNodeLoaderMapping() {
 
   nodeLoaderMapping_[at::Symbol::fromQualString("aten::exp")] =
       [this](const torch::jit::Node *node) { return loadExp(node); };
+
+  nodeLoaderMapping_[at::Symbol::fromQualString("aten::sqrt")] =
+      [this](const torch::jit::Node *node) { return loadSqrt(node); };
+  nodeLoaderMapping_[at::Symbol::fromQualString("aten::sqrt_")] =
+      [this](const torch::jit::Node *node) { return loadSqrt(node); };
 
   nodeLoaderMapping_[at::Symbol::fromQualString("aten::_convolution")] =
       [this](const torch::jit::Node *node) { return loadConvolution(node); };
