pytorch/glow

commit cd686e48b9505cf20c6cd52c68e4669d26799140

@allow-large-files [Glow/FB] Torchifi TARGETS (#3308)
Summary:
allow-large-files
Pull Request resolved: https://github.com/pytorch/glow/pull/3308

FB TARGETS for torch_glow (Torchifi)

Reviewed By: jackm321

Differential Revision: D16151602

fbshipit-source-id: 09e38f4b7cc9e913d336272e767f09d3b2ef1ae7

torch_glow/src/CMakeLists.txt
@@ -13,6 +13,7 @@ link_directories(${PYTORCH_DIR}/lib)
 
 pybind11_add_module(_torch_glow
                       binding.cpp
+                      PyTorchFileLoader.cpp
                       PyTorchModelLoader.cpp
                       CachingGraphRunner.cpp)
 
@@ -25,7 +26,7 @@ target_link_libraries(_torch_glow
                         ExecutionEngine
                         Graph
                         Importer
-                        Support
+                        Interpreter
                         Backends
                         pybind11)
 

torch_glow/src/CachingGraphRunner.cpp
@@ -20,41 +20,31 @@
 
 #include "glow/Support/Support.h"
 
-GraphInfo::GraphInfo(const torch::jit::Node *node)
-    : subgraph(node->g(at::attr::Subgraph)) {}
-
 void CachingGraphRunner::runGraph(const torch::jit::Node *node,
                                   torch::jit::Stack &stack) {
   // If this is the first time this subgraph has been run then create a new
   // GraphInfo object to store information about it.
-  if (!jitNodeToInfoMap_.count(node)) {
-    GraphInfo graphInfo(node);
-    jitNodeToInfoMap_.insert({node, std::move(graphInfo)});
-  }
 
-  auto &graphInfo = jitNodeToInfoMap_.at(node);
-
-  const at::ArrayRef<torch::jit::Value *> &graphInputs =
-      graphInfo.subgraph->inputs();
+  const std::shared_ptr<torch::jit::Graph> graph = node->g(at::attr::Subgraph);
+  const auto &graphInputs = graph->inputs();
   const auto numInputs = graphInputs.size();
+  auto inputs = torch::jit::last(stack, numInputs);
+  const char *const functionName = "PyTorchFunction";
 
-  at::ArrayRef<torch::jit::IValue> inputs = torch::jit::last(stack, numInputs);
+  glow::Function *f = nullptr;
+  auto &mod = executionEngine_.getModule();
+  if ((f = mod.getFunction(functionName))) {
+    mod.eraseFunction(f);
+  }
+  f = mod.createFunction(functionName);
+  std::vector<glow::Placeholder *> inputPlaceholders;
+  std::vector<glow::Placeholder *> outputPlaceholders;
 
-  // TODO: cache loaderResult so we don't have to recompile every run.
-  PyTorchModelLoader loader(&executionEngine_.getModule(),
-                            graphInfo.subgraph.get(), &inputs);
-  loader.load();
-
-  glow::Function *f = loader.getFunction();
-  std::vector<glow::Placeholder *> inputPlaceholders =
-      loader.getInputPlaceholders();
-  std::vector<glow::Placeholder *> outputPlaceholders =
-      loader.getOutputPlaceholders();
+  PyTorchModelLoader loader(*f, *graph, inputs, inputPlaceholders,
+                            outputPlaceholders);
 
   glow::CompilationContext cctx;
   executionEngine_.compile(f, cctx);
-
-  assert(inputs.size() == inputPlaceholders.size());
 
   glow::PlaceholderBindings bindings;
   for (size_t i = 0; i < inputs.size(); ++i) {

torch_glow/src/CachingGraphRunner.h
@@ -22,18 +22,10 @@
 
 #include "glow/ExecutionEngine/ExecutionEngine.h"
 
-/// All information per graph.
-struct GraphInfo {
-  std::shared_ptr<torch::jit::Graph> subgraph;
-  GraphInfo(const torch::jit::Node *node);
-};
-
 /// Responsible for maintaining a mapping from PyTorch subgraphs and their
 /// unique input types to compiled Glow Functions.
 class CachingGraphRunner {
-  /// Map of from PyTorch JIT Node containing contracted JIT subgraph to
-  /// to GraphInfo containing information relevent to Glow about that subgraph.
-  std::unordered_map<const torch::jit::Node *, GraphInfo> jitNodeToInfoMap_;
+  /// Glow ExecutionEngine.
   glow::ExecutionEngine executionEngine_;
 
 public:

torch_glow/src/PyTorchFileLoader.cpp
@@ -1,0 +1,42 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "PyTorchFileLoader.h"
+#include "glow/Support/Error.h"
+#include "glow/Support/Support.h"
+
+using namespace glow;
+
+PyTorchFileLoader::PyTorchFileLoader(
+    const std::string &fileName,
+    std::shared_ptr<torch::jit::script::Module> &module, llvm::Error *errPtr) {
+  auto setup = [&]() -> llvm::Error {
+    try {
+      module = std::make_shared<torch::jit::script::Module>(
+          torch::jit::load(fileName));
+    } catch (const std::exception &x) {
+      RETURN_ERR(strFormat("Cannot load model from file: %s, , reason: %s",
+                           fileName.c_str(), x.what()));
+    }
+    return llvm::Error::success();
+  };
+
+  if (errPtr) {
+    *errPtr = setup();
+  } else {
+    EXIT_ON_ERR(setup());
+  }
+}

torch_glow/src/PyTorchFileLoader.h
@@ -1,0 +1,33 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef GLOW_IMPORTER_PYTORCH_PYTORCHFILELOADER_H
+#define GLOW_IMPORTER_PYTORCH_PYTORCHFILELOADER_H
+
+#include "llvm/Support/Error.h"
+#include <torch/csrc/jit/import.h>
+
+/// Loads PyTorch model from file to JIT IR subgraphs.
+class PyTorchFileLoader {
+public:
+  /// Takes a model file \p fileName, loads model into torch Module \p module,
+  /// reports error assigning \p errPtr.
+  PyTorchFileLoader(const std::string &fileName,
+                    std::shared_ptr<torch::jit::script::Module> &module,
+                    llvm::Error *errPtr);
+};
+
+#endif // GLOW_IMPORTER_PYTORCH_PYTORCHFILELOADER_H

torch_glow/src/PyTorchLoaderTest.cpp
@@ -1,0 +1,160 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "PyTorchFileLoader.h"
+#include "PyTorchModelLoader.h"
+#include "glow/ExecutionEngine/ExecutionEngine.h"
+#include "gtest/gtest.h"
+#include <torch/csrc/jit/pass_manager.h>
+#include <torch/csrc/jit/passes/graph_fuser.h>
+
+#ifndef GLOW_DATA_PATH
+#define GLOW_DATA_PATH
+#endif
+
+namespace {
+static thread_local glow::Function *localFunction = nullptr;
+
+// Find all Save nodes and their placeholders.
+void findInputAndOutputPlaceholders(
+    glow::Function &f, std::vector<glow::Placeholder *> &inputPlaceholders,
+    std::vector<glow::Placeholder *> &outputPlaceholders) {
+  std::unordered_set<glow::Placeholder *> out;
+  for (const auto &node : f.getNodes()) {
+    if (const glow::SaveNode *SN = llvm::dyn_cast<glow::SaveNode>(&node)) {
+      out.insert(llvm::cast<glow::Placeholder>(SN->getOutput().getNode()));
+    }
+  }
+
+  for (auto *ph : f.findPlaceholders()) {
+    if (out.count(ph)) {
+      outputPlaceholders.push_back(ph);
+    } else {
+      inputPlaceholders.push_back(ph);
+    }
+  }
+}
+
+// Convert JIT Graph into Glow Function.
+void convertJitGraphToGlowFunction(torch::jit::Stack &stack,
+                                   torch::jit::Graph &graph,
+                                   glow::Function &f) {
+  const auto &graphInputs = graph.inputs();
+  const auto numInputs = graphInputs.size();
+  auto inputs = torch::jit::last(stack, numInputs);
+
+  // Load JIT Graph into Glow Function.
+  std::vector<glow::Placeholder *> inputPlaceholders;
+  std::vector<glow::Placeholder *> outputPlaceholders;
+  PyTorchModelLoader loader(f, graph, inputs, inputPlaceholders,
+                            outputPlaceholders);
+  // Remove from stack input parameters.
+  torch::jit::drop(stack, numInputs);
+  // Lookup placeholders for the output shapes.
+  for (auto *ph : outputPlaceholders) {
+    std::vector<int64_t> sizes;
+    for (auto size : ph->dims()) {
+      sizes.push_back(static_cast<int64_t>(size));
+    }
+    // Create an empty tensor with the correct shape.
+    auto ptT = at::empty(sizes);
+    auto var = torch::autograd::make_variable(ptT);
+    // Add to stack output parameter.
+    stack.push_back(at::IValue(var));
+  }
+}
+} // namespace
+
+TEST(PyTorchModelLoader, Model) {
+  const std::string fileName{GLOW_DATA_PATH
+                             "tests/models/pytorchModels/resnet18.pt"};
+  std::shared_ptr<torch::jit::script::Module> module;
+  llvm::Error errPtr = llvm::Error::success();
+  PyTorchFileLoader fileLoader(fileName, module, &errPtr);
+  EXPECT_FALSE(errPtr);
+
+  std::vector<torch::jit::IValue> vec;
+  auto emptyTensor = at::empty({1, 3, 224, 224});
+  vec.push_back(torch::autograd::make_variable(emptyTensor));
+  module->forward(vec);
+
+  std::shared_ptr<torch::jit::Graph> subgraph =
+      torch::jit::lastExecutedOptimizedGraph();
+
+  glow::Module mod;
+  auto *f = mod.createFunction("PyTorchFunction");
+  std::vector<glow::Placeholder *> inputPlaceholders;
+  std::vector<glow::Placeholder *> outputPlaceholders;
+
+  // Add self parameter.
+  vec.insert(vec.begin(), module->module_object());
+
+  at::ArrayRef<torch::jit::IValue> inputs(vec);
+  PyTorchModelLoader loader(*f, *subgraph, inputs, inputPlaceholders,
+                            outputPlaceholders);
+}
+
+TEST(PyTorchModelLoader, Fuser) {
+  const std::string fileName{GLOW_DATA_PATH
+                             "tests/models/pytorchModels/resnet18.pt"};
+  std::shared_ptr<torch::jit::script::Module> module;
+  llvm::Error errPtr = llvm::Error::success();
+  PyTorchFileLoader fileLoader(fileName, module, &errPtr);
+  EXPECT_FALSE(errPtr);
+  std::shared_ptr<torch::jit::Graph> graph;
+  static auto testSymbol = at::Symbol::fromQualString("glow::LoaderTest");
+
+  glow::Module mod;
+  localFunction = mod.createFunction("GlowFunction");
+
+  LOG(INFO) << "Register operator and pass";
+  {
+    auto options = c10::OperatorOptions();
+    options.setAliasAnalysis(at::AliasAnalysisKind::PURE);
+    torch::jit::RegisterOperators op({torch::jit::Operator(
+        testSymbol,
+        [](const torch::jit::Node *node) {
+          return [node](torch::jit::Stack &stack) {
+            // Get JIT Graph.
+            auto graph = node->g(at::attr::Subgraph);
+            convertJitGraphToGlowFunction(stack, *graph, *localFunction);
+            return 0;
+          };
+        },
+        options)});
+
+    torch::jit::RegisterPass pass([&](std::shared_ptr<torch::jit::Graph> &g) {
+      torch::jit::CustomFuseGraph(g, PyTorchModelLoader::isNodeSupported,
+                                  testSymbol);
+    });
+  }
+
+  LOG(INFO) << "Running forward method...";
+  std::vector<torch::jit::IValue> vec;
+  auto emptyTensor = at::empty({1, 3, 224, 224});
+  vec.push_back(torch::autograd::make_variable(emptyTensor));
+
+  module->forward(vec);
+
+  // Find all Save nodes and their placeholders.
+  std::vector<glow::Placeholder *> inputPlaceholders;
+  std::vector<glow::Placeholder *> outputPlaceholders;
+  findInputAndOutputPlaceholders(*localFunction, inputPlaceholders,
+                                 outputPlaceholders);
+
+  EXPECT_EQ(inputPlaceholders.size(), 1);
+  localFunction = nullptr;
+}

torch_glow/src/PyTorchModelLoader.h
@@ -24,77 +24,37 @@
 
 /// Loads PyTorch JIT IR subgraphs as a Glow Function.
 class PyTorchModelLoader {
-  /// Glow module in which the loaded Glow Function will be created.
-  glow::Module *mod_ = nullptr;
-
-  /// The PyTorch subgraph being loaded.
-  torch::jit::Graph *subgraph_ = nullptr;
-
-  /// The reference PyTorch inputs used for loading. This is required for shape
-  /// information.
-  at::ArrayRef<torch::jit::IValue> *inputs_ = nullptr;
-
-  /// The Glow function that will created.
-  glow::Function *f_ = nullptr;
-
-  /// Glow Placeholders corresponding to stack inputs from PyTorch.
-  std::vector<glow::Placeholder *> inputPlaceholders_;
-
-  /// Glow Placeholders corresponding to stack outputs to back PyTorch.
-  std::vector<glow::Placeholder *> outputPlaceholders_;
+  /// Glow Function created outside this class.
+  glow::Function &F_;
 
   /// Mapping from PyTorch Values to Glow NodeValues created during loading.
   std::unordered_map<const torch::jit::Value *, glow::NodeValue> valueMap_;
 
-  /// A mapping from jit Symbols representing jit operators to the
-  /// PyTorchModelLoader method that is used to load that operator.
-  std::unordered_map<torch::jit::Symbol,
-                     std::function<void(const torch::jit::Node *)>>
-      nodeLoaderMapping_;
+  /// Defines type for mapping Symbols to PyTorchModelLoader member functions
+  /// for loading torch::jit::Node * objects.
+  using MappingOfMemberFunctions =
+      std::unordered_map<torch::jit::Symbol, void (PyTorchModelLoader::*)(
+                                                 const torch::jit::Node *)>;
 
 public:
-  PyTorchModelLoader();
-
-  /// Takes a glow::Module \p mod, a jit::Graph \p subgraph to load, and a stack
-  /// of \p inputs for the subgraph to be loaded and retains references to those
-  /// things to be used during loading.
-  PyTorchModelLoader(glow::Module *mod, torch::jit::Graph *subgraph,
-                     at::ArrayRef<torch::jit::IValue> *inputs);
-
-  /// Creates a glow::Function that represents the loader's subgraph imported
-  /// for the shapes seen in the given inputs stack.
-  // TODO: return and error upon failure.
-  void load();
+  /// Takes a glow::Function \p F, a jit::Graph \p subgraph to load, and a
+  /// stack of \p inputs for the subgraph to be loaded. Output parameters
+  /// \p inputPlaceholders and \p outputPlaceholders are filled out.
+  PyTorchModelLoader(glow::Function &F, torch::jit::Graph &subgraph,
+                     at::ArrayRef<torch::jit::IValue> &inputs,
+                     std::vector<glow::Placeholder *> &inputPlaceholders,
+                     std::vector<glow::Placeholder *> &outputPlaceholders);
 
   /// Returns whether or not a PyTorch node is supported.
   /// NOTE: For now this is just an enumeration of all type of PyTorch nodes
-  /// that the loader knows about but doesn't really guarantee that loading will
+  /// that the loader knows about but doesn't really guarantee that loading
   /// will succeed because determining this requires more informations such as
   /// shape info that isn't yet available when this is run.
   static bool isNodeSupported(const torch::jit::Node *node);
 
-  /// Get the Glow function that this loader has created.
-  glow::Function *getFunction() { return f_; }
-
-  /// \returns the Glow input placeholders for the loaded function in the order
-  /// that is expected by the stack of PyTorch inputs when running the function.
-  const std::vector<glow::Placeholder *> &getInputPlaceholders() {
-    return inputPlaceholders_;
-  }
-
-  /// \returns the Glow output placeholders for the loaded function in the order
-  /// that is expected by the stack of PyTorch inputs when running the function.
-  const std::vector<glow::Placeholder *> &getOutputPlaceholders() {
-    return outputPlaceholders_;
-  }
-
 private:
-  /// Populates nodeLoaderMapping_ with a mapping from jit Symbols representing
-  /// jit operators to the PyTorchModelLoader method that is used to load that
-  /// operator.
-  /// NOTE: This must be called by all PyTorchModelLoader constructors so that
-  /// this mapping is available when loading begins.
-  void populateNodeLoaderMapping();
+  /// Save access to the mapping.
+  static const MappingOfMemberFunctions &getSymbolsMapping();
 
   /// Find the Glow NodeValue that maps to a given PyTorch value \p value.
   glow::NodeValue getGlowNodeValue(const torch::jit::Value *value) const;
@@ -120,7 +80,7 @@ private:
   glow::Placeholder *loadValue(const torch::jit::Value *value);
 
   /// Load a given PyTorch Node \p ptNode.
-  void loadNode(const torch::jit::Node *ptNode);
+  bool loadNode(const torch::jit::Node *ptNode);
 
   /// Load a PyTorch Constant node as a Glow Constant.
   void loadConstant(const torch::jit::Node *ptNode);

commit f97478c12ec014e3376ca53d1c7f1188540bca9a

[PyTorch] Unbreak torch_glow
Summary:
torch_glow was inadvertently broken by #3308 which removed
`PyTorchModelLoader::populateNodeLoaderMapping` and created a static map
in its place. Support for exp, sqrt and max operators was added and
merged while PR #3308 was still a work in progress and it must have
gotten lost in the process of resolving merge conflicts.

Test Plan:
The unit tests for exp, sqrt and max pass again.

torch_glow/src/CMakeLists.txt
@@ -26,7 +26,7 @@ target_link_libraries(_torch_glow
                         ExecutionEngine
                         Graph
                         Importer
-                        Interpreter
+			 Support
                         Backends
                         pybind11)
 

torch_glow/src/PyTorchModelLoader.cpp
@@ -129,6 +129,11 @@ PyTorchModelLoader::getSymbolsMapping() {
       {at::Symbol::fromQualString("aten::t_"),
        &PyTorchModelLoader::loadTranspose},
       {at::Symbol::fromQualString("aten::min"), &PyTorchModelLoader::loadMin},
+      {at::Symbol::fromQualString("aten::max"), &PyTorchModelLoader::loadMax},
+      {at::Symbol::fromQualString("aten::exp"), &PyTorchModelLoader::loadExp},
+      {at::Symbol::fromQualString("aten::sqrt"), &PyTorchModelLoader::loadSqrt},
+      {at::Symbol::fromQualString("aten::sqrt_"),
+       &PyTorchModelLoader::loadSqrt},
   };
 
   return symbolLoaderMapping;