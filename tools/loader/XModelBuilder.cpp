b5c7d67e1884785d3e560a99f8cc79ea52a4ab01
William Yessen <wnyessen@gmail.com>
2019-08-02 16:46:28 -0700

    Added the correct copyright notices.

--- tools/loader/XModelBuilder.cpp
+++ tools/loader/XModelBuilder.cpp
@@ -1,7 +1,16 @@
-/**
- * Copyright (c) 2019-present, XPERI Corporation
- * 
- * Author: William Yessen <william.yessen@xperi.com>
+/** Copyright 2019 Xperi Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License”); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include "Loader.h"
