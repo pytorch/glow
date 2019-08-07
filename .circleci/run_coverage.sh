fa17ffe0b3eedc15c7874ed972d4318f489532c3
Roman Dzhabarov <rdzhabarov@fb.com>
2019-08-06 21:30:25 -0700

    [ci] Fix uploading coverage report. Add report on per PR basis.

--- .circleci/run_coverage.sh
+++ .circleci/run_coverage.sh
@@ -8,15 +8,11 @@ if [ ! -f "${COVERAGE_FILE}" ]; then
 fi
 
 # Upload coverage only on master branch.
-if [ "${CIRCLE_BRANCH}" == "master" ]; then
-  echo "INFO: Uploading coverage to S3."
+echo "INFO: Uploading coverage to S3."
 
-  BRANCH_NAME="${CIRCLE_BRANCH}"
-  COVERAGE_DIR="$(dirname "${COVERAGE_FILE}")"
-  UPLOAD_LOCATION="fb-glow-assets/coverage/coverage-${BRANCH_NAME}"
+BRANCH_NAME="${CIRCLE_BRANCH}"
+COVERAGE_DIR="$(dirname "${COVERAGE_FILE}")"
+UPLOAD_LOCATION="fb-glow-assets/coverage/coverage-${BRANCH_NAME}"
 
-  aws s3 cp "${COVERAGE_DIR}" "s3://${UPLOAD_LOCATION}" --recursive --acl public-read
-  echo "INFO: Coverage report for branch '${BRANCH_NAME}': https://fb-glow-assets.s3.amazonaws.com/coverage/coverage-${BRANCH_NAME}/index.html"
-else
-  echo "WARNING: Coverage cannot be uploaded to s3 for PR from a fork, branch: ${CIRCLE_BRANCH}."
-fi
+aws s3 cp "${COVERAGE_DIR}" "s3://${UPLOAD_LOCATION}" --recursive --acl public-read
+echo "INFO: Coverage report for branch '${BRANCH_NAME}': https://fb-glow-assets.s3.amazonaws.com/coverage/coverage-${BRANCH_NAME}/index.html"
