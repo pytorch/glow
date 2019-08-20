451a81aee9d392b1ff6225712e6a0ac71d2cb6b0
Jack Montgomery <jackm321@gmail.com>
2019-08-19 17:56:27 -0700

    Use ProfiledTensorType (#3435)
    Summary:
    Fixes glow build.
    All the other tensor types went away so we'll use this one. Eventually we should enable (at least under a flag) the profiling executor but for now we just create ProfiledTensorTypes manually from graph inputs.
    
    Documentation:
    n/a
    Pull Request resolved: https://github.com/pytorch/glow/pull/3435
    
    Test Plan: python setup.py test
    
    Differential Revision: D16908763
    
    Pulled By: jackm321
    
    fbshipit-source-id: 9e9d562cc5a173bdbd053171be6be278dad5e8f9

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -128,7 +128,7 @@ elif [[ "$CIRCLE_JOB" == "PYTORCH" ]]; then
     source venv/bin/activate
     git clone https://github.com/pytorch/pytorch.git --recursive
     cd pytorch
-    git checkout 3b5daef6dec7b541a74e33a9a0b0646941b0440d
+    git checkout 7f86fb8995d9990fe1eac5a18335523cb3da7f09
     pip install -r requirements.txt
     python setup.py install
     cd ${GLOW_DIR}
