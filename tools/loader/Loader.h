dd53884aac3cf4cad4f864a5be032f77f07fd54d
Iancu Ciprian Mindru <iancu.mindru@nxp.com>
2019-09-10 15:26:44 +0300

    Move getModelInput utility in Loader class. Add extra validation for unique name. Add default type to model input parser.

--- tools/loader/Loader.h
+++ tools/loader/Loader.h
@@ -95,6 +95,12 @@ public:
   /// \pre (modelPathOpt.size() == 1)
   static llvm::StringRef getModelOptDir();
 
+  /// Get the description of the model inputs provided through the command
+  /// line interface to the Loader by providing separately the names in the
+  /// vector \p inputNames and the input types in the vector \p inputTypes.
+  static void getModelInputs(std::vector<std::string> &inputNames,
+                             std::vector<Type> &inputTypes);
+
   /// Compiles the Function F_. Handles quantization, emitting bundles, and
   /// dumping debug information. \p bindings bind specific
   /// placeholders to concrete tensors. The concrete tensors include
--- tools/loader/ModelCompiler.cpp
+++ tools/loader/ModelCompiler.cpp
@@ -16,128 +16,10 @@
 
 #include "Loader.h"
 
-#include "glow/Converter/TypeAToTypeBFunctionConverter.h"
-#include "glow/Graph/Nodes.h"
 #include "glow/Importer/Caffe2ModelLoader.h"
 #include "glow/Importer/ONNXModelLoader.h"
 
-#include "llvm/Support/CommandLine.h"
-
 using namespace glow;
-
-namespace {
-
-/// Model compiler options.
-llvm::cl::OptionCategory modelCompilerCat("Model Compiler Options");
-
-llvm::cl::list<std::string> modelInputs(
-    "model-input", llvm::cl::ZeroOrMore,
-    llvm::cl::desc(
-        "For ONNX models the inputs of the graph can be inferred   \n"
-        "automatically and hence this option is not mandatory.     \n"
-        "For Caffe2 models the graph definition does not contain   \n"
-        "the description of the inputs and hence must be provided  \n"
-        "explicitly using this option. One or more model inputs    \n"
-        "are provided using the following format:                  \n"
-        "   -model-input=<inputName1>,<inputType1>,<inputShape1>   \n"
-        "   -model-input=<inputName2>,<inputType2>,<inputShape2>   \n"
-        "   ....................................................   \n"
-        "For quantized types the format is slightly different since\n"
-        "the scale and offset parameters should also be provided:  \n"
-        "   -model-input=<name>,<type>,<scale>,<offset>,<shape>    \n"
-        "For example we can can provide one or more inputs:        \n"
-        "   -model-input=input_03_data,float,[1]                   \n"
-        "   -model-input=data_bias,int32,[1,32,32]                 \n"
-        "   -model-input=data,int8q,0.123,-13,[1,10]               \n"
-        "The supported types are:                                  \n"
-        "   - float, float16 (floating point types)                \n"
-        "   - int32, int64 (integer types)                         \n"
-        "   - int8q, int16q, int32q (integer quantized types)      \n"
-        "   - bool (logic type)\n"),
-    llvm::cl::value_desc("name,type,shape"), llvm::cl::cat(modelCompilerCat));
-
-} // namespace
-
-/// Parse the strings and get the model input names and types.
-/// The expected format is one of the following:
-/// <name>,<type>,<shape> for non-quantized types.
-/// <name>,<type>,<scale>,<offset>,<shape> for quantized types.
-static void getModelInputs(std::vector<std::string> &inputNames,
-                           std::vector<Type> &inputTypes) {
-  for (const auto &str : modelInputs) {
-    auto strPair = llvm::StringRef(str).split(',');
-    llvm::StringRef name = strPair.first;
-    strPair = strPair.second.split(',');
-    llvm::StringRef type = strPair.first;
-    CHECK(name.size()) << "Model input name empty";
-    CHECK(type.size()) << "Model input type empty";
-
-    // Parse type string.
-    ElemKind kind;
-    if (type.equals("float")) {
-      kind = ElemKind::FloatTy;
-    } else if (type.equals("float16")) {
-      kind = ElemKind::Float16Ty;
-    } else if (type.equals("int8q")) {
-      kind = ElemKind::Int8QTy;
-    } else if (type.equals("int16q")) {
-      kind = ElemKind::Int16QTy;
-    } else if (type.equals("int32q")) {
-      kind = ElemKind::Int32QTy;
-    } else if (type.equals("int32")) {
-      kind = ElemKind::Int32ITy;
-    } else if (type.equals("int64")) {
-      kind = ElemKind::Int64ITy;
-    } else if (type.equals("bool")) {
-      kind = ElemKind::BoolTy;
-    } else {
-      LOG(FATAL) << strFormat("Model input type %s not supported",
-                              std::string(type).c_str());
-    }
-
-    // For quantized type get scale and offset.
-    double scale;
-    int32_t offset;
-    if (isQuantizedElemKind(kind)) {
-      strPair = strPair.second.split(',');
-      CHECK(strPair.first.size()) << "Model input scale empty";
-      CHECK(!strPair.first.getAsDouble(scale))
-          << "Model input scale parameter invalid";
-      strPair = strPair.second.split(',');
-      CHECK(strPair.first.size()) << "Model input offset empty";
-      CHECK(!strPair.first.getAsInteger(0, offset))
-          << "Model input offset parameter invalid";
-    }
-
-    // Parse shape string.
-    llvm::StringRef shape = strPair.second;
-    CHECK(shape.size()) << "Model input shape empty";
-    ShapeVector dims;
-    CHECK_EQ(shape.front(), '[') << "First shape char should be [";
-    shape = shape.drop_front();
-    CHECK_EQ(shape.back(), ']') << "First shape char should be ]";
-    shape = shape.drop_back();
-    CHECK(shape.size()) << "Model input shape empty";
-    size_t val;
-    while (shape.contains(',')) {
-      auto splitRes = shape.split(',');
-      CHECK(!splitRes.first.getAsInteger(0, val))
-          << "Model input shape integer invalid";
-      dims.push_back(val);
-      shape = splitRes.second;
-    }
-    CHECK(!shape.getAsInteger(0, val)) << "Model input shape integer invalid";
-    dims.push_back(val);
-
-    // Push data.
-    inputNames.push_back(name);
-    if (isQuantizedElemKind(kind)) {
-      inputTypes.push_back(Type(kind, dims, (float)scale, offset));
-    } else {
-      inputTypes.push_back(Type(kind, dims));
-    }
-  }
-}
 
 int main(int argc, char **argv) {
 
@@ -159,7 +41,7 @@ int main(int argc, char **argv) {
     // explicitly. Get model input names and types.
     std::vector<std::string> inputNames;
     std::vector<Type> inputTypes;
-    getModelInputs(inputNames, inputTypes);
+    Loader::getModelInputs(inputNames, inputTypes);
     std::vector<const char *> inputNameRefs;
     std::vector<TypeRef> inputTypeRefs;
     for (size_t idx = 0, e = inputNames.size(); idx < e; idx++) {
