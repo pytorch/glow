2d7b7cbbe90368395efafb7e3606c9d22c3e996c
Yinghai Lu <yinghai@fb.com>
2019-09-16 10:31:14 -0700

    Better error message (#3514)
    Summary:
    Give better error message when input tensor is larger than the compiled spec. Adding shape will help on which dimension we got wrong.
    
    Documentation:
    
    [Optional Fixes #issue]
    Pull Request resolved: https://github.com/pytorch/glow/pull/3514
    
    Test Plan: Please see a detailed explanation of how to fill out the fields in the relevant sections in PULL_REQUEST.md.
    
    Differential Revision: D17382475
    
    Pulled By: yinghai
    
    fbshipit-source-id: 2ee87b67e7523ea4c3821fc9c8e8ab3b22e633c5

--- lib/Onnxifi/Base.cpp
+++ lib/Onnxifi/Base.cpp
@@ -157,8 +157,18 @@ onnxStatus Graph::setIOAndRun(uint32_t inputsCount,
     }
 
     if (inOnnxTensorSize > inPhPtr->getType()->size()) {
+      std::stringstream ss;
+      for (const auto j : inOnnxTensorDims) {
+        ss << j << ", ";
+      }
+      ss << " vs ";
+      auto sizes = inPhPtr->getType()->dims();
+      for (const auto j : sizes) {
+        ss << j << ", ";
+      }
       LOG(ERROR) << "Input tensor is too large: " << inOnnxTensorSize << " vs "
-                 << inPhPtr->getType()->size() << ": " << inOnnxTensor.name;
+                 << inPhPtr->getType()->size() << ": " << inOnnxTensor.name
+                 << ", shape: " << ss.str();
       return ONNXIFI_STATUS_INVALID_SHAPE;
     }
 
