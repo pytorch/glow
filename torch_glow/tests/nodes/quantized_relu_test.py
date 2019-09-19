4f39c7a9365a9c31b94c86dc709a0cf6ed6d4691
zrphercule <zrphercule@gmail.com>
2019-09-19 10:50:48 -0700

    add torch relu test

--- torch_glow/tests/nodes/quantized_relu_test.py
+++ torch_glow/tests/nodes/quantized_relu_test.py
@@ -4,16 +4,18 @@ import torch
 
 from tests.utils import jitVsGlow
 
+
 def test_quantized_relu():
     """Basic test of the PyTorch quantized::relu Node on Glow."""
 
     def test_f(a):
         q = torch.nn.quantized.Quantize(0.015, 5, torch.quint8)
         dq = torch.nn.quantized.DeQuantize()
-        return dq(torch.nn.quantized.ReLU(q(a)))
+        re = torch.nn.quantized.ReLU()
+        return dq(re(q(a)))
 
     x = torch.randn([5, 5])
 
-    jitVsGlow(test_f, x, expected_fused_ops={"quantized::add",
-                                                "aten::quantize_linear",
-                                                "aten::dequantize"})
+    jitVsGlow(test_f, x, expected_fused_ops={"aten::relu",
+                                             "aten::quantize_linear",
+                                             "aten::dequantize"})
