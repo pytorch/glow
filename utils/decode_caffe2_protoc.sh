705804c93c09a559192a3150f04189566b19234f
Arun Moorthy <arunm-git@users.noreply.github.com>
2019-10-14 12:48:50 -0700

    NFC: Update Copyright line. (#3623)
    Summary:
    Description
    
    As above. Added CONTRIBUTORS file (see contents on how it was
    generated).
    Replaces existing copyright lines with reference to this file.
    Pull Request resolved: https://github.com/pytorch/glow/pull/3623
    
    Differential Revision: D17913295
    
    Pulled By: arunm-git
    
    fbshipit-source-id: e738f1cb6e986d9d695d73e1d19426800ce0353c

--- utils/decode_caffe2_protoc.sh
+++ utils/decode_caffe2_protoc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2017-present, Facebook, Inc.
+# Copyright (c) Glow Contributors. See CONTRIBUTORS file.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
