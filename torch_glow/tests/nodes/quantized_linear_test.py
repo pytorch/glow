50b996ddf6cec9b0b1688b71d6a77dac0abce976
zrphercule <zrphercule@gmail.com>
2019-10-25 12:21:13 -0700

    Add skipped tests for accuracy debuggings (#3653)
    Summary:
    Add a few skipped tests which caused by glow&pytorch accuracy for debugging.
    cc hyuen
    Pull Request resolved: https://github.com/pytorch/glow/pull/3653
    
    Differential Revision: D18128219
    
    Pulled By: zrphercule
    
    fbshipit-source-id: 5eec458c86d5740943aa87c81e971026d86b8c40

--- torch_glow/tests/nodes/quantized_linear_test.py
+++ torch_glow/tests/nodes/quantized_linear_test.py
@@ -28,3 +28,43 @@ def test_quantized_linear():
     jitVsGlow(test_f, inputs, weights, bias, expected_fused_ops={
               "glow::unpacked_quantized_linear", "aten::quantize_per_tensor",
               "aten::dequantize"})
+
+
+def test_quantized_linear_random_input():
+    """Basic test of the PyTorch quantized::linear Node on Glow."""
+
+    def test_f(inputs, weights, bias=None):
+        q_int = torch.nn.quantized.Quantize(
+            scale=1/13, zero_point=0, dtype=torch.qint8)
+        q_uint = torch.nn.quantized.Quantize(
+            scale=1/13, zero_point=120, dtype=torch.quint8)
+
+        dq = torch.nn.quantized.DeQuantize()
+
+        q_inputs = q_uint(inputs)
+        q_weights = q_int(weights)
+
+        return dq(torch.nn.quantized.functional.linear(q_inputs, q_weights, bias))
+
+    inputs = torch.randn(7, 7)
+    weights = torch.randn(7, 7)
+
+    # TODO zrphercule: Right now this test will not pass on random float bias,
+    # due to the known numerical problem.
+    # Therefore we provide a few meaningful biases here.
+    # When debugging, it would be helpful in jitVsGlow to output
+    # torch_res - glow_res.
+
+    # This is our final goal
+    # bias = torch.randn(7)
+
+    # This would be very useful when debugging the numerical problem
+    # bias = torch.tensor([1, 2, 3, 4, 5, 6, 7], dtype=torch.float) * 0.1
+    # bias = torch.tensor([1, 0, 0, 0, 0, 0, 0], dtype=torch.float) * 0.1
+
+    # Test would pass on integer bias
+    bias = torch.tensor([1, 2, 3, 4, 5, 6, 7], dtype=torch.float)
+
+    jitVsGlow(test_f, inputs, weights, bias, expected_fused_ops={
+              "glow::unpacked_quantized_linear", "aten::quantize_per_tensor",
+              "aten::dequantize"})
