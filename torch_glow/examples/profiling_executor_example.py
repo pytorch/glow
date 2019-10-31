922464ae1357a89f45f2a9da2718a760a323e81b
Jack Montgomery <jackm321@gmail.com>
2019-10-31 10:41:19 -0700

    Add profiling executor example (#3705)
    Summary:
    Add a quick example of using the profiling executor to run scripted modules with Glow.
    
    Documentation:
    comments
    Pull Request resolved: https://github.com/pytorch/glow/pull/3705
    
    Test Plan: `python examples/profiling_executor_example.py`
    
    Differential Revision: D18248493
    
    Pulled By: jackm321
    
    fbshipit-source-id: 26b14b332e3cfe62776743d62ff75d999a7ab7d6

---
+++ torch_glow/examples/profiling_executor_example.py
@@ -0,0 +1,36 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import torch
+import torch.nn as nn
+import torch_glow
+
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.linear = nn.Linear(10, 2)
+
+    def forward(self, x):
+        return self.linear(x)
+
+
+torch._C._jit_set_profiling_mode(True)
+torch_glow.enableFusionPass()
+
+m = Model()
+
+m_jit = torch.jit.script(m)
+
+x = torch.randn(10)
+
+# No Glow fusion node
+print("initial jit ir")
+print(m_jit.graph_for(x))
+
+m_jit(x)
+m_jit(x)
+m_jit(x)
+
+# Contains Glow fusion node
+print("final jit ir")
+print(m_jit.graph_for(x))
