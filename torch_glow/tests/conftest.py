ae853c5b745ec2c1f213a75956db8723820f4630
Mike Neilly <mike.neilly@esperantotech.com>
2019-12-18 21:17:02 -0800

    Fix whitespace formatting

--- torch_glow/tests/conftest.py
+++ torch_glow/tests/conftest.py
@@ -7,7 +7,7 @@ import torch_glow
 def pytest_addoption(parser):
     parser.addoption("--backend", action="store", default=None)
 
-    
+
 def pytest_sessionstart(session):
     backend = session.config.getoption("--backend")
     if backend:
