9cfcdb33826d0215fd17ba67718052d8d74d5d61
Nick Gibson <nickg@fb.com>
2020-01-21 11:05:49 -0800

    make Interpreter claim support for ClipNode (#4037)
    Summary:
    Pull Request resolved: https://github.com/pytorch/glow/pull/4037
    
    The Interpreter supports ClipNode via lowering, but says that it doesn't support the op in isOpSupported. This is because isOpSupported is a bit weird. After discussing it with Jordan Fix decided not to attempt a big refactor of isOpSupported and just add in ClipNode. Not super happy about it though.
    
    Reviewed By: jfix71
    
    Differential Revision: D19500784
    
    fbshipit-source-id: 25f66b1e1200d64b94a1b64624f9058ff98f8568

--- lib/Backends/Interpreter/Interpreter.cpp
+++ lib/Backends/Interpreter/Interpreter.cpp
@@ -80,6 +80,7 @@ bool Interpreter::isOpSupported(const NodeInfo &NI) const {
   case Kinded::Kind::MulNodeKind:
   case Kinded::Kind::MaxNodeKind:
   case Kinded::Kind::MinNodeKind:
+  case Kinded::Kind::ClipNodeKind:
     return NI.allInputsAndOutputsHaveSameElemKind(
         {ElemKind::FloatTy, ElemKind::Float16Ty, ElemKind::Int8QTy,
          ElemKind::Int32ITy, ElemKind::Int64ITy});
