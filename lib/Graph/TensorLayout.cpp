46ac201522273b2f82aa884589a8bf4976995f3b
Thierry Lepley <tlepley@cadence.com>
2020-02-10 15:57:59 +0100

    [hotfix] fix the formating of TensorLayout.cpp

--- lib/Graph/TensorLayout.cpp
+++ lib/Graph/TensorLayout.cpp
@@ -665,9 +665,7 @@ static bool acceptsAnyInputLayout(const glow::Node *node) {
   case Kinded::Kind::SGDNodeKind: {
     return true;
   }
-  default: {
-    return false;
-  }
+  default: { return false; }
   }
 }
 
