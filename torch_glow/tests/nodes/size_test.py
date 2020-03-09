8041670231bd4a33ff9bb3464edb03238329867d
Rui Zhu <zrphercule@fb.com>
2020-03-09 10:50:53 -0700

    Comment skipped testcase for not triggering CI
    Summary: Resolve T63580684
    
    Reviewed By: jackm321
    
    Differential Revision: D20330538
    
    fbshipit-source-id: 3083d8bb2e089232900f66577c6eed0bd540dadb

--- torch_glow/tests/nodes/size_test.py
+++ torch_glow/tests/nodes/size_test.py
@@ -8,28 +8,29 @@ import unittest
 
 class TestSize(unittest.TestCase):
     # Need to be able to export lists from Glow fused nodes
-    @unittest.skip(reason="not ready")
-    def test_size_basic(self):
-        """Test of the PyTorch aten::size Node on Glow."""
+    # Commented out both test cases for not triggering internal CI
+    # @unittest.skip(reason="not ready")
+    # def test_size_basic(self):
+    #    """Test of the PyTorch aten::size Node on Glow."""
 
-        def test_f(a):
-            b = a + a.size(0)
-            return b
+    #    def test_f(a):
+    #        b = a + a.size(0)
+    #        return b
 
-        x = torch.zeros([4], dtype=torch.int32)
+    #    x = torch.zeros([4], dtype=torch.int32)
 
-        jitVsGlow(test_f, x, expected_fused_ops={"aten::size"})
+    #    jitVsGlow(test_f, x, expected_fused_ops={"aten::size"})
 
-    @unittest.skip(reason="not ready")
-    def test_size_neg_dim(self):
-        """Test negative dimension index for the PyTorch aten::size Node on Glow."""
+    # @unittest.skip(reason="not ready")
+    # def test_size_neg_dim(self):
+    #    """Test negative dimension index for the PyTorch aten::size Node on Glow."""
 
-        def test_f(a):
-            return a.size(-1)
+    #    def test_f(a):
+    #        return a.size(-1)
 
-        x = torch.randn(2, 3, 4, dtype=torch.float32)
+    #    x = torch.randn(2, 3, 4, dtype=torch.float32)
 
-        jitVsGlow(test_f, x, expected_fused_ops={"aten::size"})
+    #    jitVsGlow(test_f, x, expected_fused_ops={"aten::size"})
 
     def test_size_oob_neg_dim(self):
         """Test out of bounds negative dimension index for the PyTorch aten::size Node on Glow."""
