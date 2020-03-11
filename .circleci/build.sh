f8beb74f0a8965d6b26a547f744adf9b38d5194b
Jordan Fix <1198212+jfix71@users.noreply.github.com>
2020-03-11 00:32:06 -0700

    Add support for https apt sources (#4284)
    Summary:
    CI appears to be broken due to issues with https sources.
    Pull Request resolved: https://github.com/pytorch/glow/pull/4284
    
    Differential Revision: D20381890
    
    Pulled By: jfix71
    
    fbshipit-source-id: 0967ef4ded82672d4a48b79225b59baaee69c4d0

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -4,6 +4,12 @@
 # By default, we run with the enabled CPU backend and disabled OpenCL backend.
 set -ex
 
+# Add support for https apt sources.
+wget http://security.ubuntu.com/ubuntu/pool/main/a/apt/apt-transport-https_1.2.29ubuntu0.1_amd64.deb
+echo "960a44449fa1ec082a75adee8d6f6fe15577627570edc722a1822e495a8b8c57  apt-transport-https_1.2.29ubuntu0.1_amd64.deb" | sha256sum -c
+sudo dpkg -i apt-transport-https_1.2.29ubuntu0.1_amd64.deb
+rm apt-transport-https_1.2.29ubuntu0.1_amd64.deb
+
 export MAX_JOBS=8
 if [ "${CIRCLE_JOB}" != "COVERAGE" ]; then
     if hash sccache 2>/dev/null; then
