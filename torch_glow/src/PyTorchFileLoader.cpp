pytorch/glow

commit 461783a1d23811149995405c9da0b0eb382a5cde

kill script namespace (#34515)
Summary:
Pull Request resolved: https://github.com/pytorch/pytorch/pull/34515

Once upon a time we thought this was necessary. In reality it is not, so
removing it.

For backcompat, our public interface (defined in `api/`) still has
typedefs to the old `script::` names.

There was only one collision: `Pass` as a `Stmt` and `Pass` as a graph
transform. I renamed one of them.

Test Plan: Imported from OSS

Differential Revision: D20353503

Pulled By: suo

fbshipit-source-id: 48bb911ce75120a8c9e0c6fb65262ef775dfba93

torch_glow/src/PyTorchFileLoader.cpp
@@ -99,7 +99,7 @@ Error loadJitGraphToGlowFunction(
 
 /// Runs Module forward pass, triggers custom fusion pass if local thread
 /// Glow function is set.
-Error evaluateModuleGraph(std::shared_ptr<torch::jit::script::Module> &module,
+Error evaluateModuleGraph(std::shared_ptr<torch::jit::Module> &module,
                           const std::vector<torch::jit::IValue> &inputs) {
   try {
     module->forward(inputs);
@@ -145,9 +145,9 @@ struct RegisterCustomFusionPass {
 /*static*/
 Error PyTorchFileLoader::loadPyTorchModel(
     const std::string &fileName,
-    std::shared_ptr<torch::jit::script::Module> &module) {
+    std::shared_ptr<torch::jit::Module> &module) {
   try {
-    module = std::make_shared<torch::jit::script::Module>(
+    module = std::make_shared<torch::jit::Module>(
         torch::jit::load(fileName));
   } catch (const std::exception &x) {
     RETURN_ERR(strFormat("Cannot load model from file: %s, , reason: %s",
@@ -169,7 +169,7 @@ Error PyTorchFileLoader::loadPyTorchGraph(
   settings.weightFreezingEnabled = false;
 
   // Convert PyTorch model into JIT Module.
-  std::shared_ptr<torch::jit::script::Module> module;
+  std::shared_ptr<torch::jit::Module> module;
   RETURN_IF_ERR(PyTorchFileLoader::loadPyTorchModel(fileName, module));
 
   // Disable gradient nodes generation.
@@ -192,7 +192,7 @@ Error PyTorchFileLoader::parsePyTorchGraphForOnnxTraining(
     std::vector<glow::Placeholder *> &outputPlaceholders) {
 
   // Convert PyTorch model into JIT Module.
-  std::shared_ptr<torch::jit::script::Module> module;
+  std::shared_ptr<torch::jit::Module> module;
   RETURN_IF_ERR(PyTorchFileLoader::loadPyTorchModel(fileName, module));
 
   // Disable gradient nodes generation.

torch_glow/src/PyTorchFileLoader.h
@@ -35,7 +35,7 @@ public:
   /// \returns error if any.
   static Error
   loadPyTorchModel(const std::string &fileName,
-                   std::shared_ptr<torch::jit::script::Module> &module);
+                   std::shared_ptr<torch::jit::Module> &module);
 
   /// Takes a model file \p fileName, loads optimized graph and a
   /// stack of \p inputs into Glow Function \p F and fills out input \p

torch_glow/tests/unittests/PyTorchLoaderTest.cpp
@@ -27,7 +27,7 @@
 TEST(ModelLoaderTest, Loader) {
   const std::string fileName{GLOW_DATA_PATH
                              "tests/models/pytorchModels/resnet18.pt"};
-  std::shared_ptr<torch::jit::script::Module> module;
+  std::shared_ptr<torch::jit::Module> module;
   glow::Error err = glow::PyTorchFileLoader::loadPyTorchModel(fileName, module);
   EXPECT_FALSE(err);
 }

commit f1b7a2a40d5412e8d136ca5261427970ce30a1c3

Fix clang-format (#4292)
Summary:
Broken in https://github.com/pytorch/glow/commit/461783a1d23811149995405c9da0b0eb382a5cde
Pull Request resolved: https://github.com/pytorch/glow/pull/4292

Reviewed By: yinghai

Differential Revision: D20415813

Pulled By: jfix71

fbshipit-source-id: 7be01d6fd3033d61f01c0538742dc3e02e6650a9

torch_glow/src/PyTorchFileLoader.cpp
@@ -144,11 +144,9 @@ struct RegisterCustomFusionPass {
 
 /*static*/
 Error PyTorchFileLoader::loadPyTorchModel(
-    const std::string &fileName,
-    std::shared_ptr<torch::jit::Module> &module) {
+    const std::string &fileName, std::shared_ptr<torch::jit::Module> &module) {
   try {
-    module = std::make_shared<torch::jit::Module>(
-        torch::jit::load(fileName));
+    module = std::make_shared<torch::jit::Module>(torch::jit::load(fileName));
   } catch (const std::exception &x) {
     RETURN_ERR(strFormat("Cannot load model from file: %s, , reason: %s",
                          fileName.c_str(), x.what()));

torch_glow/src/PyTorchFileLoader.h
@@ -33,9 +33,8 @@ class PyTorchFileLoader {
 public:
   /// Takes a model file \p fileName, loads model into torch Module \p module,
   /// \returns error if any.
-  static Error
-  loadPyTorchModel(const std::string &fileName,
-                   std::shared_ptr<torch::jit::Module> &module);
+  static Error loadPyTorchModel(const std::string &fileName,
+                                std::shared_ptr<torch::jit::Module> &module);
 
   /// Takes a model file \p fileName, loads optimized graph and a
   /// stack of \p inputs into Glow Function \p F and fills out input \p