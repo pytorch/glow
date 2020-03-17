e1a2399c899ea226bc9fc3637d2155dd0e287f1e
Jordan Fix <jfix@fb.com>
2020-03-17 10:08:37 -0700

    Remove initialization/quantization of embeddings (#4282)
    Summary:
    Pull Request resolved: https://github.com/pytorch/glow/pull/4282
    
    We don't need the values to be initialized at all. This saves a couple minutes per run.
    
    Reviewed By: ckkim1
    
    Differential Revision: D20375415
    
    fbshipit-source-id: a90a0a5b099f006a7182c4b3ce3f5a9d696fa9fe

--- tests/benchmark/SLSBench.cpp
+++ tests/benchmark/SLSBench.cpp
@@ -90,14 +90,12 @@ class SLSBench : public Benchmark {
   std::string backendStr_;
   std::vector<SLSParam> params_;
   std::string devId_;
-  float tableValue;
 
 public:
   SLSBench(dim_t batchSize_, dim_t asyncLaunchSize_, std::string backendStr_,
            std::vector<SLSParam> params_, std::string devId_ = std::string(""))
       : batchSize_(batchSize_), asyncLaunchSize_(asyncLaunchSize_),
-        backendStr_(backendStr_), params_(params_), devId_(devId_),
-        tableValue(1.0f) {}
+        backendStr_(backendStr_), params_(params_), devId_(devId_) {}
 
   double countSLSGbytes(SLSParam param) const {
 
@@ -153,25 +151,20 @@ public:
 
   void addSLSNode(std::unique_ptr<Module> &mod, Function *fn, SLSParam param) {
 
-    // Create and initialize data tensor
-    Tensor data(ElemKind::FloatTy,
-                {param.numTableEntries, param.numElementsPerRow});
-    data.getHandle().clear(tableValue);
-    tableValue += 1.0f;
-
     // Constant needed for Non-quantized case
-    Constant *dataConstant = nullptr;
+    Tensor dataConstantTensor;
     if ((param.slsKind == NONQUANTIZED_WEIGHTED) ||
         (param.slsKind == NONQUANTIZED_UNWEIGHTED)) {
-      Tensor dataConstantTensor(
-          param.dtype, {param.numTableEntries, param.numElementsPerRow});
-      if (param.dtype == ElemKind::FloatTy) {
-        dataConstantTensor.getHandle<float>().clear(1.0f);
-      } else {
-        dataConstantTensor.getHandle<float16_t>().clear(1.0f);
-      }
-      dataConstant = mod->createConstant("SLSData", dataConstantTensor);
+      dataConstantTensor =
+          Tensor(param.dtype, {param.numTableEntries, param.numElementsPerRow});
+    } else {
+      // If RWQ then we need to account for per-row scale/offset in the shape.
+      const dim_t numTotalColumns =
+          param.numElementsPerRow + 2 * sizeof(float16_t);
+      dataConstantTensor = Tensor(
+          param.fusedDtype, {param.numTableEntries, numTotalColumns}, 1.0, 0);
     }
+    Constant *dataConstant = mod->createConstant("SLSData", dataConstantTensor);
 
     // Create placeholders for weights, indices and lengths
     auto *weights = mod->createPlaceholder(
@@ -244,13 +237,12 @@ public:
     Node *R = nullptr;
     if (param.slsKind == QUANTIZED_UNWEIGHTED) {
       R = fn->createFusedRowwiseQuantizedSparseLengthsSum(
-          getSLSDescription(param), data, indices, lengths, param.fusedDtype,
+          getSLSDescription(param), dataConstant, indices, lengths,
           param.useFP16Accumulation, LM, param.numIndicesPerBatch);
     } else if (param.slsKind == QUANTIZED_WEIGHTED) {
       R = fn->createFusedRowwiseQuantizedSparseLengthsWeightedSum(
-          getSLSDescription(param), data, weights, indices, lengths,
-          param.fusedDtype, param.useFP16Accumulation, LM,
-          param.numIndicesPerBatch);
+          getSLSDescription(param), dataConstant, weights, indices, lengths,
+          param.useFP16Accumulation, LM, param.numIndicesPerBatch);
     } else if (param.slsKind == NONQUANTIZED_WEIGHTED) {
       R = fn->createSparseLengthsWeightedSum(
           getSLSDescription(param), dataConstant, weights, indices, lengths, LM,
