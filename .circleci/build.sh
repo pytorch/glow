893962d4a5b885842368f3e0950e0d3e254a41fd
neildhar <neildhar@users.noreply.github.com>
2020-03-27 10:31:09 -0700

    fix rustup install

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -5,7 +5,7 @@
 set -ex
 
 apt-get update && apt-get -y install sudo apt-transport-https curl
-curl https://sh.rustup.rs -sSf | sh
+curl -sSf sh.rustup.rs | sh -s -- -y
 cargo install sccache
 
 export MAX_JOBS=8
