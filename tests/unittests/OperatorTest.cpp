2da2fd92abb7e547cbf34e8085965a4e91e1e042
Jordan Fix <jfix@fb.com>
2020-03-29 01:19:21 -0700

    Relax LayerNorm_Float16 a little bit
    Summary: att
    
    Differential Revision: D20723438
    
    fbshipit-source-id: 6adb528f2853e0259adbadd93dc4681722e269c3

--- tests/unittests/OperatorTest.cpp
+++ tests/unittests/OperatorTest.cpp
@@ -12656,7 +12656,7 @@ TEST_P(OperatorStatelessTest, LayerNorm_Float) {
 TEST_P(OperatorStatelessTest, LayerNorm_Float16) {
   CHECK_IF_ENABLED();
   compareAgainstInterpreter(getBackendName(), createAndInitLayerNormTest,
-                            ElemKind::FloatTy, ElemKind::Float16Ty, 0.005f,
+                            ElemKind::FloatTy, ElemKind::Float16Ty, 0.01f,
                             parCloneCountOpt);
 }
 
