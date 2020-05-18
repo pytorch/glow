31cbb251d272a221c7fa1f45e253b12759ff31cb
Summer Deng <summerdeng@fb.com>
2020-05-17 23:28:39 -0700

    Int8Conv2dParallelBench
    Summary: Conv2d microbenchmark for CPU and NNPI with representative input shapes
    
    Reviewed By: jackm321
    
    Differential Revision: D21489018
    
    fbshipit-source-id: 5724d4c789855d83c526de294d81440108a6c0dc

---
+++ tests/benchmark/ConvUtils.h
@@ -0,0 +1,139 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+#pragma once
+
+#include <array>
+#include <string>
+#include <type_traits>
+
+/*
+ * Copied from fbgemm/ConvUtils.h to avoid the dependency on fbgemm
+ */
+template <int N, int... Vals>
+constexpr
+    typename std::enable_if<N == sizeof...(Vals), std::array<int, N>>::type
+    array_of_ones() {
+  return std::array<int, N>{{Vals...}};
+}
+
+template <int N, int... Vals>
+constexpr
+    typename std::enable_if<N != sizeof...(Vals), std::array<int, N>>::type
+    array_of_ones() {
+  return array_of_ones<N, Vals..., 1>();
+}
+
+/**
+ * @brief A struct to conveniently store all convolution parameters.
+ */
+template <int SPATIAL_DIM = 2> struct conv_param_t {
+  int MB;                              ///< Mini Batch size
+  int IC;                              ///< Number of Input Channels
+  int OC;                              ///< Number of Output Channels
+  std::array<int, SPATIAL_DIM> IN_DIM; ///< Input Image Dimension
+  int G;                               ///< Number of Groups
+  std::array<int, SPATIAL_DIM> K;      ///< Filter (Kernel) dimensions
+  std::array<int, SPATIAL_DIM> stride; //< Strides
+  std::array<int, SPATIAL_DIM * 2>
+      pad; //< Padding (first SPATIAL_DIM is for prev/top/left padding, second
+           // SPATIAL_DIM is for next/bottom/right padding)
+  std::array<int, SPATIAL_DIM> dilation; //< Kernel dilation
+
+  // The following are derived parameters
+  std::array<int, SPATIAL_DIM> OUT_DIM; //< Output Image Dimension
+  std::array<int, SPATIAL_DIM> IN_DIMP; //< Input Image Dimension Padded
+
+  /**
+   * @brief Constructor for initializing the convolution parameters.
+   */
+  conv_param_t(
+      int mb, int ic, int oc, std::array<int, SPATIAL_DIM> in_dim, int g,
+      std::array<int, SPATIAL_DIM> k, std::array<int, SPATIAL_DIM> strd,
+      std::array<int, SPATIAL_DIM * 2> pd,
+      std::array<int, SPATIAL_DIM> dilations = array_of_ones<SPATIAL_DIM>())
+      : MB(mb), IC(ic), OC(oc), IN_DIM(in_dim), G(g), K(k), stride(strd),
+        pad(pd), dilation(dilations) {
+    if (ic % g != 0) {
+      throw std::runtime_error(
+          "groups = " + std::to_string(g) +
+          " does not divide number of input channels = " + std::to_string(ic));
+    }
+    if (oc % g != 0) {
+      throw std::runtime_error(
+          "groups = " + std::to_string(g) +
+          " does not divide number of output channels = " + std::to_string(oc));
+    }
+
+    for (int d = 0; d < SPATIAL_DIM; ++d) {
+      IN_DIMP[d] = IN_DIM[d] + pad[d] + pad[SPATIAL_DIM + d];
+      OUT_DIM[d] = (IN_DIMP[d] - dilation[d] * (K[d] - 1) - 1) / stride[d] + 1;
+    }
+  }
+
+  /**
+   * @brief Helper function to get convolution parameters as string.
+   */
+  std::string toString() const {
+    std::string dim_string[3] = {"T", "H", "W"};
+
+    std::string out = "";
+    out += "MB:" + std::to_string(MB) + ", ";
+    out += "IC:" + std::to_string(IC) + ", ";
+    out += "OC:" + std::to_string(OC) + ", ";
+    if (SPATIAL_DIM <= 3) {
+      for (int d = 0; d < SPATIAL_DIM; ++d) {
+        out += "I" + dim_string[3 - SPATIAL_DIM + d] + ":" +
+               std::to_string(IN_DIM[d]) + ", ";
+      }
+    } else {
+      for (int d = 0; d < SPATIAL_DIM; ++d) {
+        out += "I" + std::to_string(d) + ":" + std::to_string(IN_DIM[d]) + ", ";
+      }
+    }
+    out += "G:" + std::to_string(G) + ", ";
+    if (SPATIAL_DIM <= 3) {
+      for (int d = 0; d < SPATIAL_DIM; ++d) {
+        out += "K" + dim_string[3 - SPATIAL_DIM + d] + ":" +
+               std::to_string(K[d]) + ", ";
+      }
+      for (int d = 0; d < SPATIAL_DIM; ++d) {
+        out += "stride_" + dim_string[3 - SPATIAL_DIM + d] + ":" +
+               std::to_string(stride[d]) + ", ";
+      }
+      for (int d = 0; d < SPATIAL_DIM * 2; ++d) {
+        out += "pad_" + dim_string[3 - SPATIAL_DIM + (d % SPATIAL_DIM)] + ":" +
+               std::to_string(pad[d]) + ", ";
+      }
+      for (int d = 0; d < SPATIAL_DIM; ++d) {
+        out += "dilation_" + dim_string[3 - SPATIAL_DIM + d] + ":" +
+               std::to_string(dilation[d]);
+        if (d < SPATIAL_DIM - 1) {
+          out += ", ";
+        }
+      }
+    } else {
+      for (int d = 0; d < SPATIAL_DIM; ++d) {
+        out += "K" + std::to_string(d) + ":" + std::to_string(K[d]) + ", ";
+      }
+      for (int d = 0; d < SPATIAL_DIM; ++d) {
+        out += "stride_" + std::to_string(d) + ":" + std::to_string(stride[d]) +
+               ", ";
+      }
+      for (int d = 0; d < SPATIAL_DIM; ++d) {
+        out += "pad_" + std::to_string(d) + ":" + std::to_string(pad[d]);
+        if (d < SPATIAL_DIM * 2 - 1) {
+          out += ", ";
+        }
+      }
+      for (int d = 0; d < SPATIAL_DIM; ++d) {
+        out += "dilation_" + std::to_string(d) + ":" +
+               std::to_string(dilation[d]) + ", ";
+      }
+    }
+    return out;
+  }
+};
---
+++ tests/benchmark/Int8Conv2dParallelBench.cpp
@@ -0,0 +1,284 @@
+/**
+ * Copyright (c) Glow Contributors. See CONTRIBUTORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <array>
+#include <cstdlib>
+#include <future>
+#include <random>
+
+#include "Bench.h"
+
+#include "ConvUtils.h"
+#include "glow/ExecutionEngine/ExecutionEngine.h"
+#include "glow/Optimizer/GraphOptimizer/GraphOptimizer.h"
+
+using namespace glow;
+using namespace std;
+
+vector<vector<conv_param_t<2>>> shapes_2d = {
+    // MB, IC, OC, IH, IW, G, KH, KW, stride_h, stride_w,
+    // pad_h_top, pad_w_left, pad_h_bottom, pad_w_right
+    // 2D convolutions
+    // regular
+    {conv_param_t<>(1, 128, 128, {56, 56}, 1, {3, 3}, {1, 1}, {1, 1, 1, 1})},
+    // groupwise
+    {conv_param_t<>(1, 128, 128, {56, 56}, 32, {3, 3}, {1, 1}, {1, 1, 1, 1})},
+    // DW
+    {conv_param_t<>(1, 272, 272, {47, 125}, 272, {3, 3}, {1, 1}, {1, 1, 1, 1})},
+    // Pointwise
+    {conv_param_t<>(1, 128, 128, {56, 56}, 1, {1, 1}, {1, 1}, {0, 0, 0, 0})},
+    // bottleneck blocks
+    {conv_param_t<>(1, 256, 128, {56, 56}, 1, {1, 1}, {1, 1}, {0, 0, 0, 0}),
+     conv_param_t<>(1, 128, 128, {56, 56}, 32, {3, 3}, {1, 1}, {1, 1, 1, 1}),
+     conv_param_t<>(1, 128, 256, {56, 56}, 1, {1, 1}, {1, 1}, {0, 0, 0, 0})},
+    {conv_param_t<>(1, 512, 256, {28, 28}, 1, {1, 1}, {1, 1}, {0, 0, 0, 0}),
+     conv_param_t<>(1, 256, 256, {28, 28}, 32, {3, 3}, {1, 1}, {1, 1, 1, 1}),
+     conv_param_t<>(1, 256, 512, {28, 28}, 1, {1, 1}, {1, 1}, {0, 0, 0, 0})},
+    {conv_param_t<>(1, 1024, 512, {14, 14}, 1, {1, 1}, {1, 1}, {0, 0, 0, 0}),
+     conv_param_t<>(1, 512, 512, {14, 14}, 32, {3, 3}, {1, 1}, {1, 1, 1, 1}),
+     conv_param_t<>(1, 512, 1024, {14, 14}, 1, {1, 1}, {1, 1}, {0, 0, 0, 0})},
+    {conv_param_t<>(1, 2048, 1024, {7, 7}, 1, {1, 1}, {1, 1}, {0, 0, 0, 0}),
+     conv_param_t<>(1, 1024, 1024, {7, 7}, 32, {3, 3}, {1, 1}, {1, 1, 1, 1}),
+     conv_param_t<>(1, 1024, 2048, {7, 7}, 1, {1, 1}, {1, 1}, {0, 0, 0, 0})}
+
+};
+
+/*
+ * Benchmark a number of Conv2d operators with representative input shapes.
+ * There are a number of parallel Conv2d nodes which are created, one
+ * per core. Each core handles one weight matrix. Then these are chained
+ * together in multiple layers. After each layer, output tensor is passed to the
+ * next layer.
+ */
+class Int8Conv2dParallelBench : public Benchmark {
+  /// Matrices.
+  std::vector<conv_param_t<2>> input_shapes_;
+  size_t numLayers_;
+  PlaceholderBindings bindings_;
+  std::unique_ptr<runtime::HostManager> hostManager_;
+  size_t asyncLaunchSize_;
+  size_t numCores_;
+  const char *backendStr_;
+  const char *devId_;
+
+public:
+  Int8Conv2dParallelBench(vector<conv_param_t<2>> &input_shapes_,
+                          size_t numLayers_, size_t asyncLaunchSize_,
+                          size_t numCores_, const char *backendStr_,
+                          const char *devId_)
+      : input_shapes_(input_shapes_), numLayers_(numLayers_),
+        asyncLaunchSize_(asyncLaunchSize_), numCores_(numCores_),
+        backendStr_(backendStr_), devId_(devId_) {}
+
+  void setup() override {
+
+    // Setup host manager
+    std::vector<std::unique_ptr<runtime::DeviceConfig>> configs;
+    auto config = glow::make_unique<runtime::DeviceConfig>(backendStr_);
+    if (devId_ != nullptr) {
+      config->parameters["DeviceID"] = devId_;
+    }
+    configs.push_back(std::move(config));
+    hostManager_ = glow::make_unique<runtime::HostManager>(std::move(configs));
+
+    dim_t N, IC, IH, IW, OC, OH, OW;
+    if (input_shapes_.size() == 1) {
+      N = input_shapes_[0].MB;
+      IC = input_shapes_[0].IC;
+      IH = input_shapes_[0].IN_DIM[0];
+      IW = input_shapes_[0].IN_DIM[1];
+      OC = input_shapes_[0].OC;
+      OH = input_shapes_[0].OUT_DIM[0];
+      OW = input_shapes_[0].OUT_DIM[1];
+    } else {
+      N = input_shapes_[0].MB;
+      IC = input_shapes_[0].IC;
+      IH = input_shapes_[0].IN_DIM[0];
+      IW = input_shapes_[0].IN_DIM[1];
+      OC = input_shapes_[input_shapes_.size() - 1].OC;
+      OH = input_shapes_[input_shapes_.size() - 1].OUT_DIM[0];
+      OW = input_shapes_[input_shapes_.size() - 1].OUT_DIM[1];
+    }
+    std::unique_ptr<Module> mod(new Module);
+    auto fn = mod->createFunction("singleNode");
+
+    std::vector<Node *> cur(numCores_);
+    std::vector<Placeholder *> filters(numCores_ * input_shapes_.size());
+    std::vector<Placeholder *> bias(numCores_ * input_shapes_.size());
+    std::vector<Node *> conv(numCores_ * input_shapes_.size());
+    std::vector<Placeholder *> input(numCores_);
+    std::vector<Placeholder *> output(numCores_);
+
+    for (size_t core = 0; core < numCores_; core++) {
+      input[core] =
+          mod->createPlaceholder(ElemKind::Int8QTy, {N, IH, IW, IC}, 1.0, 0,
+                                 "input_" + std::to_string(core), false);
+      output[core] =
+          mod->createPlaceholder(ElemKind::Int8QTy, {N, OH, OW, OC}, 1.0, 0,
+                                 "output_" + std::to_string(core), false);
+      cur[core] = input[core];
+    }
+
+    for (size_t layer = 0; layer < numLayers_; layer++) {
+      for (size_t core = 0; core < numCores_; core++) {
+        size_t conv_ops = 0;
+        for (auto conv_param : input_shapes_) {
+          filters[core * input_shapes_.size() + conv_ops] =
+              mod->createPlaceholder(
+                  ElemKind::Int8QTy,
+                  {(unsigned long)(conv_param.OC),
+                   (unsigned long)(conv_param.K[0]),
+                   (unsigned long)(conv_param.K[1]),
+                   (unsigned long)(conv_param.IC / conv_param.G)},
+                  1.0, 0,
+                  "filters_" + std::to_string(core) + "_" +
+                      std::to_string(conv_ops),
+                  false);
+          bias[core * input_shapes_.size() + conv_ops] = mod->createPlaceholder(
+              ElemKind::Int32QTy, {(unsigned long)(conv_param.OC)}, 1.0, 0,
+              "bias_" + std::to_string(core) + "_" + std::to_string(conv_ops),
+              false);
+          bindings_.allocate(filters[core * input_shapes_.size() + conv_ops])
+              ->getHandle<int8_t>()
+              .clear(0);
+          bindings_.allocate(bias[core * input_shapes_.size() + conv_ops])
+              ->getHandle<int32_t>()
+              .clear(0);
+          auto outTy = mod->uniqueType(ElemKind::Int8QTy,
+                                       {(unsigned long)(conv_param.MB),
+                                        (unsigned long)(conv_param.OUT_DIM[0]),
+                                        (unsigned long)(conv_param.OUT_DIM[1]),
+                                        (unsigned long)(conv_param.OC)},
+                                       1.0, 0);
+          conv[core * input_shapes_.size() + conv_ops] = fn->createConv(
+              "conv" + std::to_string(core) + "_" + std::to_string(layer) +
+                  "_" + std::to_string(conv_ops),
+              cur[core], filters[core * input_shapes_.size() + conv_ops],
+              bias[core * input_shapes_.size() + conv_ops], outTy,
+              {(unsigned int)(conv_param.K[0]),
+               (unsigned int)(conv_param.K[1])},
+              {(unsigned int)(conv_param.stride[0]),
+               (unsigned int)(conv_param.stride[1])},
+              {(unsigned int)(conv_param.pad[0]),
+               (unsigned int)(conv_param.pad[1]),
+               (unsigned int)(conv_param.pad[2]),
+               (unsigned int)(conv_param.pad[3]),
+               (unsigned int)(conv_param.pad[4])},
+              (unsigned int)(conv_param.G),
+              (unsigned int)(conv_param.dilation[0]));
+
+          cur[core] = conv[core * input_shapes_.size() + conv_ops];
+          conv_ops += 1;
+        }
+      }
+    }
+    for (size_t core = 0; core < numCores_; core++) {
+      fn->createSave("save" + std::to_string(core),
+                     cur[(core + 1) * input_shapes_.size() - 1], output[core]);
+    }
+
+    for (size_t core = 0; core < numCores_; core++) {
+      ::glow::convertPlaceholdersToConstants(fn, bindings_,
+                                             {
+                                                 input[core],
+                                                 output[core],
+                                             });
+    }
+
+    CompilationContext ctx;
+    EXIT_ON_ERR(hostManager_->addNetwork(std::move(mod), ctx));
+  }
+
+  void run() override {
+    std::vector<std::promise<void>> promises(asyncLaunchSize_);
+    std::vector<std::future<void>> futures;
+    for (auto &runPromise : promises) {
+      std::unique_ptr<ExecutionContext> contextPtr(new ExecutionContext);
+      futures.push_back(runPromise.get_future());
+      hostManager_->runNetwork(
+          "singleNode", std::move(contextPtr),
+          [&runPromise](runtime::RunIdentifierTy, Error err,
+                        std::unique_ptr<ExecutionContext> /* contextPtr */) {
+            EXIT_ON_ERR(std::move(err));
+            runPromise.set_value();
+          });
+    }
+    for (auto &fut : futures) {
+      fut.wait();
+    }
+  }
+
+  void teardown() override {}
+};
+
+int main(int argc, char *argv[]) {
+  size_t numLayers = atoi(argv[1]);
+  size_t reps = atoi(argv[2]);
+  size_t asyncLaunches = atoi(argv[3]);
+  size_t numCores = atoi(argv[4]);
+  const char *backendStr = argv[5];
+  char *dev_id = nullptr;
+
+  printf("Int8Conv2dParallel Microbenchmark\n");
+  printf(
+      "Usage: Int8Conv2dParallelBench numLayers(Int) "
+      "numReps(Int) "
+      "numAsyncLaunches(Int) numCores(Int) backendStr(String) dev_id(Int)\n");
+  assert(argc == 6 || argc == 7);
+  if (argc > 6) {
+    dev_id = argv[6];
+    printf("Setting backend device: \"%s\"\n", dev_id);
+  }
+  printf("Start Int8Conv2dParallelBench\n");
+  size_t shape_idx = 0;
+  size_t total_input_shapes = shapes_2d.size();
+  for (auto shapes : shapes_2d) {
+    double gflops = 0;
+    string shape_info = "";
+    for (auto shape : shapes) {
+      gflops += 2.0 * shape.G * (shape.IC / shape.G) * shape.IN_DIM[0] *
+                shape.IN_DIM[1] * (shape.OC / shape.G) * shape.OUT_DIM[0] *
+                shape.OUT_DIM[1];
+      if (shape_info != "") {
+        shape_info += ";";
+      }
+      shape_info += shape.toString();
+    }
+    gflops *= numLayers * numCores / 1e9;
+    printf("\n=====Input shape %zu/%zu: %s\n", shape_idx, total_input_shapes,
+           shape_info.c_str());
+    Int8Conv2dParallelBench b(shapes, numLayers, asyncLaunches, numCores,
+                              backendStr, dev_id);
+    auto times = bench(&b, reps);
+    for (auto t : times) {
+      printf("BenchResult,Conv2dParallelBench,SW,%4zu,%4zu,%4zu,%4zu,"
+             "%s,%"
+             "2.6lf,%5.2lf\n",
+             numLayers, reps, asyncLaunches, numCores, backendStr,
+             t / asyncLaunches, gflops * asyncLaunches / t);
+    }
+    double min = *(std::min_element(times.begin(), times.end()));
+    size_t midElt = times.size() / 2;
+    std::nth_element(times.begin(), times.begin() + midElt, times.end());
+    double median = times[midElt];
+    double median_runtime = median / ((double)asyncLaunches);
+    double min_runtime = min / ((double)asyncLaunches);
+    printf("BenchSummary,Conv2dParallelBench,SW,%4zu,%4zu,%4zu,%4zu,%s,%"
+           "2.6lf,%2.6lf,%5.2lf,%5.2lf\n",
+           numLayers, reps, asyncLaunches, numCores, backendStr, median_runtime,
+           min_runtime, gflops / median_runtime, gflops / min_runtime);
+    shape_idx++;
+  }
+}
