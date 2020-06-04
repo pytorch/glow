f50172606798ba63055ddae3b4c7313aaaf35fcb
Jack Montgomery <jackmontgomery@fb.com>
2020-06-03 21:22:21 -0700

    Don't log partiion error in ONNXModelLoader::ONNXModelLoader
    Reviewed By: jfix71
    
    Differential Revision: D21877443
    
    fbshipit-source-id: e5ee7d38134005a6ac964315f90fbbcb49ad5919

--- lib/Importer/ONNXModelLoader.cpp
+++ lib/Importer/ONNXModelLoader.cpp
@@ -4604,7 +4604,7 @@ ONNXModelLoader::ONNXModelLoader(
 
     auto numPartitionsOrErr = getIntMetadataProp(modelDef, "numPartitions");
     if (!numPartitionsOrErr) {
-      ERR_TO_VOID(numPartitionsOrErr.takeError());
+      ERR_TO_VOID(numPartitionsOrErr.takeError(), /*log*/ false);
       G_ = mod_.createFunction(funName);
     } else {
       RETURN_ERR_IF_NOT(PPC, "No PrePartitionConfig to load partitions into");
