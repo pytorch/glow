8699dba3892e2b069411b34a0a301de9c1ed885d
Yinghai Lu <yinghai@fb.com>
2020-06-24 00:10:01 -0700

    Added NNPI device checker and fatal the program when card hang is detected (#4632)
    Summary:
    Pull Request resolved: https://github.com/pytorch/glow/pull/4632
    
    Preliminary impl. Might be too radical. And need tuning of params.
    
    Together with `--glow_nnpi_timeout_ms=5000`, it should be able to resolve
    - sporadic card hang
    - sporadic unresponsed inference
    
    Reviewed By: benjibc
    
    Differential Revision: D22183576
    
    fbshipit-source-id: 05d8837007cfc7cd2291ef091cf33c5fe9f38913

--- lib/Onnxifi/Flags.cpp
+++ lib/Onnxifi/Flags.cpp
@@ -72,6 +72,8 @@ extern unsigned GlowHabanaMemory;
 #ifdef GLOW_WITH_NNPI
 extern unsigned GlowNNPIMemory;
 extern unsigned GlowNNPITimeout;
+extern unsigned GlowNNPIDeviceCheckPeriodSec;
+extern bool GlowNNPIDeviceCheck;
 #endif
 extern bool GlowEnableDRT;
 extern bool GlowEnableP2P;
@@ -433,10 +435,27 @@ DEFINE_validator(glow_nnpi_memory, [](const char *flagname, int32_t value) {
 DEFINE_int32(glow_nnpi_timeout_ms, 0,
              "Timeout threshold for inferecnce in milliseconds. Default 0 "
              "means infinity");
-DEFINE_validator(glow_nnpi_timeout_ms, [](const char *flagname, int32_t value) {
-  glow::runtime::GlowNNPITimeout = value * 1000;
-  return true;
-});
+DEFINE_validator(glow_nnpi_timeout_ms,
+                 [](const char * /*unused*/, int32_t value) {
+                   glow::runtime::GlowNNPITimeout = value * 1000;
+                   return true;
+                 });
+
+DEFINE_bool(glow_nnpi_device_check, false,
+            "Whether to check NNPI device health or not");
+DEFINE_validator(glow_nnpi_device_check,
+                 [](const char * /*unused*/, bool value) {
+                   glow::runtime::GlowNNPIDeviceCheck = value;
+                   return true;
+                 });
+
+DEFINE_int32(glow_nnpi_device_check_period_s, 30,
+             "Period for NNPI device health check in seconds. Default to 32s.");
+DEFINE_validator(glow_nnpi_device_check_period_s,
+                 [](const char *flagname, int32_t value) {
+                   glow::runtime::GlowNNPIDeviceCheckPeriodSec = value;
+                   return true;
+                 });
 #endif
 
 DEFINE_bool(glow_log_partition, true, "Enable logging partition info");
