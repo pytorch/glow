d05b615292a7886b2e7b0396924959c5707f3aa0
Jack Montgomery <jackmontgomery@fb.com>
2020-07-23 09:16:58 -0700

    Fix flatten lowering (#4730)
    Summary:
    Pull Request resolved: https://github.com/pytorch/glow/pull/4730
    
    Fix how aten::flatten is lowered to Glow
    
    Previously aten::flatten lowering was misusing the startDim and endDim inputs by ignoring endDim and using startDim as the end dimension parameter to Glow's `flattenCdr` so the end result is the wrong shape for most cases. Instead we can just compute the correct final shape and use that to reshape the inputs directly during lowering.
    
    Reviewed By: yinghai
    
    Differential Revision: D22688305
    
    fbshipit-source-id: 21feb55b7b94944281d424428b2e38c52082d11e

--- torch_glow/tests/nodes/flatten_test.py
+++ torch_glow/tests/nodes/flatten_test.py
@@ -5,13 +5,63 @@ from tests.utils import jitVsGlow
 import unittest
 
 
-class TestFaltten(unittest.TestCase):
+class TestFlatten(unittest.TestCase):
     def test_flatten_basic(self):
         """Test of the PyTorch flatten Node on Glow."""
 
-        def flatten_basic(a):
-            return torch.flatten(a, 1)
+        def test_f(a):
+            return torch.flatten(a)
 
-        x = torch.randn(2, 2, 2)
+        x = torch.randn(2, 3, 2, 5)
 
-        jitVsGlow(flatten_basic, x, expected_fused_ops={"aten::flatten"})
+        jitVsGlow(test_f, x, expected_fused_ops={"aten::flatten"})
+
+    def test_flatten_start_at_0(self):
+        """Test of the PyTorch flatten Node on Glow."""
+
+        def test_f(a):
+            return torch.flatten(a, start_dim=0, end_dim=2)
+
+        x = torch.randn(2, 3, 2, 5)
+
+        jitVsGlow(test_f, x, expected_fused_ops={"aten::flatten"})
+
+    def test_flatten_start_in_middle(self):
+        """Test of the PyTorch flatten Node on Glow."""
+
+        def test_f(a):
+            return torch.flatten(a, start_dim=1, end_dim=2)
+
+        x = torch.randn(2, 3, 2, 5)
+
+        jitVsGlow(test_f, x, expected_fused_ops={"aten::flatten"})
+
+    def test_flatten_negative_end_dim(self):
+        """Test of the PyTorch flatten Node on Glow."""
+
+        def test_f(a):
+            return torch.flatten(a, start_dim=0, end_dim=-2)
+
+        x = torch.randn(2, 3, 2, 5)
+
+        jitVsGlow(test_f, x, expected_fused_ops={"aten::flatten"})
+
+    def test_flatten_same_dim(self):
+        """Test of the PyTorch flatten Node on Glow."""
+
+        def test_f(a):
+            return torch.flatten(a, start_dim=2, end_dim=2)
+
+        x = torch.randn(2, 3, 2, 5)
+
+        jitVsGlow(test_f, x, expected_fused_ops={"aten::flatten"})
+
+    def test_flatten_negative_start_dim(self):
+        """Test of the PyTorch flatten Node on Glow."""
+
+        def test_f(a):
+            return torch.flatten(a, start_dim=-3, end_dim=-1)
+
+        x = torch.randn(2, 3, 2, 5)
+
+        jitVsGlow(test_f, x, expected_fused_ops={"aten::flatten"})
