8caddd97d3e9111b0bff2b6064445163bb51d6a8
Jordan Fix <jfix@fb.com>
2020-08-06 11:17:53 -0700

    Fixes for AOT for Glow Loaders
    Differential Revision:
    D22762122
    ***
    sort Glow dot files
    
    fbshipit-source-id: 8a9713050d8538fe5f2c7718ff5358da04c8fec1

--- lib/Onnxifi/InlineOnnxifi.cpp
+++ lib/Onnxifi/InlineOnnxifi.cpp
@@ -54,8 +54,8 @@ InlineGraph::initGraph(const void *onnxModel, size_t onnxModelSize,
   std::unique_ptr<ONNXIFIModelLoader> loader;
   auto loaderOrErr = ONNXIFIModelLoader::parse(
       onnxModel, onnxModelSize, weightCount, weightDescriptors, mod, "function",
-      /* PPC */ nullptr, true /*loadInputsAsPlaceholdersForOnnx*/,
-      backendPtr_->getUseOnnx());
+      /* PPC */ nullptr, /* BSNI */ nullptr,
+      true /*loadInputsAsPlaceholdersForOnnx*/, backendPtr_->getUseOnnx());
   if (loaderOrErr) {
     loader = std::move(*loaderOrErr);
   } else {
