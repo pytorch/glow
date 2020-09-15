3857a52113d86b5e2f26eaf8b84ff55efa3da676
Satish Nadathur <nrsatish@fb.com>
2020-09-14 19:03:38 -0700

    Add benchmark for AvgPool2d (#4885)
    Summary:
    Pull Request resolved: https://github.com/pytorch/glow/pull/4885
    
    Add a microbenchmark for AvgPool2d
    
    Reviewed By: jackm321
    
    Differential Revision: D23657528
    
    fbshipit-source-id: 2416e45d9164bc056e2e721adb1d7197f73a923e

--- tests/benchmark/ConvUtils.h
+++ tests/benchmark/ConvUtils.h
@@ -137,3 +137,88 @@ template <int SPATIAL_DIM = 2> struct conv_param_t {
     return out;
   }
 };
+
+/**
+ * @brief A struct to conveniently store all convolution parameters.
+ */
+template <int SPATIAL_DIM = 2> struct avg_pool_param_t {
+  int MB;                              ///< Mini Batch size
+  int IC;                              ///< Number of Input Channels
+  std::array<int, SPATIAL_DIM> IN_DIM; ///< Input Image Dimension
+  std::array<int, SPATIAL_DIM> K;      ///< Kernel dimensions
+  std::array<int, SPATIAL_DIM> stride; //< Strides
+  std::array<int, SPATIAL_DIM * 2>
+      pad; //< Padding (first SPATIAL_DIM is for prev/top/left padding, second
+           // SPATIAL_DIM is for next/bottom/right padding)
+
+  // The following are derived parameters
+  int OC;                               ///< Number of Ouptut Channels
+  std::array<int, SPATIAL_DIM> OUT_DIM; //< Output Image Dimension
+  std::array<int, SPATIAL_DIM> IN_DIMP; //< Input Image Dimension Padded
+
+  /**
+   * @brief Constructor for initializing the average pool parameters.
+   */
+  avg_pool_param_t(int mb, int ic, std::array<int, SPATIAL_DIM> in_dim,
+                   std::array<int, SPATIAL_DIM> k,
+                   std::array<int, SPATIAL_DIM> strd,
+                   std::array<int, SPATIAL_DIM * 2> pd)
+      : MB(mb), IC(ic), IN_DIM(in_dim), K(k), stride(strd), pad(pd) {
+    for (int d = 0; d < SPATIAL_DIM; ++d) {
+      OC = IC;
+      IN_DIMP[d] = IN_DIM[d] + pad[d] + pad[SPATIAL_DIM + d];
+      OUT_DIM[d] = (IN_DIMP[d] - (K[d] - 1) - 1) / stride[d] + 1;
+    }
+  }
+
+  /**
+   * @brief Helper function to get convolution parameters as string.
+   */
+  std::string toString() const {
+    std::string dim_string[3] = {"T", "H", "W"};
+
+    std::string out = "";
+    out += "MB:" + std::to_string(MB) + ", ";
+    out += "IC:" + std::to_string(IC) + ", ";
+    out += "OC:" + std::to_string(OC) + ", ";
+    if (SPATIAL_DIM <= 3) {
+      for (int d = 0; d < SPATIAL_DIM; ++d) {
+        out += "I" + dim_string[3 - SPATIAL_DIM + d] + ":" +
+               std::to_string(IN_DIM[d]) + ", ";
+      }
+    } else {
+      for (int d = 0; d < SPATIAL_DIM; ++d) {
+        out += "I" + std::to_string(d) + ":" + std::to_string(IN_DIM[d]) + ", ";
+      }
+    }
+    if (SPATIAL_DIM <= 3) {
+      for (int d = 0; d < SPATIAL_DIM; ++d) {
+        out += "K" + dim_string[3 - SPATIAL_DIM + d] + ":" +
+               std::to_string(K[d]) + ", ";
+      }
+      for (int d = 0; d < SPATIAL_DIM; ++d) {
+        out += "stride_" + dim_string[3 - SPATIAL_DIM + d] + ":" +
+               std::to_string(stride[d]) + ", ";
+      }
+      for (int d = 0; d < SPATIAL_DIM * 2; ++d) {
+        out += "pad_" + dim_string[3 - SPATIAL_DIM + (d % SPATIAL_DIM)] + ":" +
+               std::to_string(pad[d]) + ", ";
+      }
+    } else {
+      for (int d = 0; d < SPATIAL_DIM; ++d) {
+        out += "K" + std::to_string(d) + ":" + std::to_string(K[d]) + ", ";
+      }
+      for (int d = 0; d < SPATIAL_DIM; ++d) {
+        out += "stride_" + std::to_string(d) + ":" + std::to_string(stride[d]) +
+               ", ";
+      }
+      for (int d = 0; d < SPATIAL_DIM; ++d) {
+        out += "pad_" + std::to_string(d) + ":" + std::to_string(pad[d]);
+        if (d < SPATIAL_DIM * 2 - 1) {
+          out += ", ";
+        }
+      }
+    }
+    return out;
+  }
+};
