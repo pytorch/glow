d18fb25c20dc67f3cdd197db2437c1b10f590973
Roman Levenstein <rlevenstein@fb.com>
2020-09-18 14:40:29 -0700

    Move CommandLine.h for LLVMIRCodeGen into the include directory

--- lib/LLVMIRCodeGen/LLVMIRGen.cpp
+++ lib/LLVMIRCodeGen/LLVMIRGen.cpp
@@ -17,8 +17,8 @@
 #include "glow/LLVMIRCodeGen/LLVMIRGen.h"
 #include "glow/Base/DimType.h"
 
-#include "CommandLine.h"
 #include "glow/LLVMIRCodeGen/AllocationsInfo.h"
+#include "glow/LLVMIRCodeGen/CommandLine.h"
 #include "glow/LLVMIRCodeGen/LLVMBackend.h"
 
 #include "glow/Graph/Graph.h"
