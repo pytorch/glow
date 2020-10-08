d3b9eb22f1c5ec1e98e672e64ac792b6c690d8a6
Iancu Ciprian Mindru <mciprian13@yahoo.com>
2020-10-08 18:27:49 +0300

    Small fix.

--- lib/Optimizer/Lower/Lower.cpp
+++ lib/Optimizer/Lower/Lower.cpp
@@ -1044,11 +1044,13 @@ static void lowerGroupConvolutionNode(Function *F, CompilationContext &cctx,
         {(groupId + 1) * outCperG, kdim.height, kdim.width, inCperG});
     auto *bias_slice = F->createSlice(BNG.getName(), bias, {groupId * outCperG},
                                       {(groupId + 1) * outCperG});
-    convs.push_back(F->createConv(BNG.getName(), in_slice, filter_slice,
-                                  bias_slice, outTy, kernels, strides, pads, 1,
-                                  BNG.getDilation()));
-    convs.back().setFusedActivation(BNG.getFusedActivation());
-    convs.back().setFusedActivationArgs(BNG.getFusedActivationArgs());
+
+    auto *conv = F->createConv(BNG.getName(), in_slice, filter_slice,
+                               bias_slice, outTy, kernels, strides, pads, 1,
+                               BNG.getDilation());
+    conv->setFusedActivation(BNG.getFusedActivation());
+    conv->setFusedActivationArgs(BNG.getFusedActivationArgs());
+    convs.push_back(conv);
   }
   auto *result = F->createConcat(BNG.getName(), convs, 3);
   replaceAllUsesOfWith(cctx.loweredInfoMap, BNG.getResult(), result);
