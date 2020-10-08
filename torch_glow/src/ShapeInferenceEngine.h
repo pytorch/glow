d4d99e9f5abe80e1fa22b6ff148d443efcff9bb0
Yiwen Chen <yiwen@fb.com>
2020-10-08 13:39:22 -0700

    Add support for aten::stack
    Summary:
    1) Add support for aten::stack
    2) Update the prim::listcontruct to handle tensor input, which is used to contruct tensor list as input for aten::stack in models
    
    Differential Revision: D24187017
    
    fbshipit-source-id: a0de3a8763dda1a7fe435bb07392bdd3b01a50ed

--- torch_glow/src/ShapeInferenceEngine.h
+++ torch_glow/src/ShapeInferenceEngine.h
@@ -131,7 +131,8 @@ private:
   static Expected<TensorShape> fusedConcat(const MetaStack &variableMetas,
                                            int64_t dim);
   // Shape inference for prim::ListConstruct
-  static Expected<TensorShape> listConstruct(const MetaStack &variableMetas);
+  static Expected<TensorListShape>
+  listConstruct(const MetaStack &variableMetas);
   // Shape inference for aten::permute
   static Expected<TensorShape> permute(const MetaStack &variableMetas);
   // Shape inference for aten::reshape
@@ -152,6 +153,8 @@ private:
   static Expected<TensorShape> embeddingBag(const MetaStack &variableMetas);
   // Shape inference for aten::chuck
   static Expected<TensorListShape> chunk(const MetaStack &variableMetas);
+  // Shape inference for aten::stack
+  static Expected<TensorShape> stack(const MetaStack &variableMetas);
 };
 
 } // namespace glow
