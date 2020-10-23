cd829908c4fb07fca90623ab6361b48b846ee51b
Andrew Millspaugh <spaugh@fb.com>
2020-10-23 09:34:13 -0700

    Fix utility function call
    Summary: Merge with master on landing resulted in another test showing up and not having the correct utility function call. Tests passed here and on GitHub, but failed after landing. This diff remedies that.
    
    Differential Revision: D24506131
    
    fbshipit-source-id: 54d6f8cab49fdc4e8702854ae426be163d37e4f7

--- torch_glow/tests/nodes/zero_test.py
+++ torch_glow/tests/nodes/zero_test.py
@@ -3,17 +3,18 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import unittest
 
 import torch
-from tests.utils import jitVsGlow
+from tests import utils
 
 
 class TestZero(unittest.TestCase):
     def test_zero_basic(self):
         """Basic test of the PyTorch zero Node on Glow."""
 
-        def test_f(a):
-            b = torch.zeros(a.size(), dtype=torch.float)
-            return a + b
+        class TestModule(torch.nn.Module):
+            def forward(self, a):
+                b = torch.zeros(a.size(), dtype=torch.float)
+                return a + b
 
         x = torch.randn(2, 3, 4)
 
-        jitVsGlow(test_f, x, expected_fused_ops={"aten::zeros"})
+        utils.compare_tracing_methods(TestModule(), x, fusible_ops={"aten::zeros"})
