ed35747c62e5c89a1e5cef5473dbde0e04dba3cc
Oleg Khabinov <khabinov@fb.com>
2020-12-22 11:30:14 -0800

    Remove duplicate checks for Mul operator (#5197)
    Summary:
    Pull Request resolved: https://github.com/pytorch/glow/pull/5197
    
    Currently "Mul" is checked twice.
    
    Reviewed By: yinghai
    
    Differential Revision: D25676554
    
    fbshipit-source-id: ac22fe86508f59a7c952c050b268067b7f13272b

--- lib/Exporter/ONNXModelWriter.cpp
+++ lib/Exporter/ONNXModelWriter.cpp
@@ -2587,8 +2587,8 @@ bool ONNXModelWriter::hasMultidirectionalBroadcast(
         (typeName == "Div") || (typeName == "Equal") ||
         (typeName == "Greater") || (typeName == "Less") ||
         (typeName == "Max") || (typeName == "Mean") || (typeName == "Min") ||
-        (typeName == "Mul") || (typeName == "Or") || (typeName == "Pow") ||
-        (typeName == "Sum") || (typeName == "Xor")) {
+        (typeName == "Or") || (typeName == "Pow") || (typeName == "Sum") ||
+        (typeName == "Xor")) {
       return true;
     }
   }
