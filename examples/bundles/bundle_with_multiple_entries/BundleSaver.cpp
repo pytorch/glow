33000908f505905fe5508ce12aa218fcacc09394
Iancu Ciprian Mindru <mciprian13@yahoo.com>
2021-02-16 17:27:51 -0800

    Improve memory usage for activations (#5146)
    Summary:
    **Summary**
    Improve memory allocator by adding an `allocateAll` function which has visibility to all the allocations at once and hence can do a better allocation (with higher efficiency) for activations. In principle this is done by allocating the larger buffers first and thus avoiding early fragmentation (BestFit).
    
    **Test Plan**
    Added unit tests in MemoryAllocator containing:
    - feature testing
    - negative testing
    - activation allocation for 79 models (the unit tests were automatically generated using a Matlab script using the activation pattern extracted from the IR dumped by Glow)
    
    Pull Request resolved: https://github.com/pytorch/glow/pull/5146
    
    Reviewed By: jfix71
    
    Differential Revision: D25562925
    
    Pulled By: opti-mix
    
    fbshipit-source-id: 731484ba6c945f72dade9379a122098104cb66e2

--- examples/bundles/bundle_with_multiple_entries/BundleSaver.cpp
+++ examples/bundles/bundle_with_multiple_entries/BundleSaver.cpp
@@ -33,32 +33,46 @@ llvm::cl::opt<std::string>
 int main(int argc, char **argv) {
   llvm::cl::ParseCommandLineOptions(argc, argv, "Bundle Saver\n");
 
-  // Create a bundle with multiple entry points and save it.
+  // Create a bundle with multiple entry points and save it. We use multiple
+  // MatMul layers to force usage of activations memory pool for each function.
   Module M;
-  constexpr dim_t tensorSize = 64;
+  constexpr dim_t tensorSize = 16;
 
-  Constant *addConst =
-      M.createConstant(ElemKind::FloatTy, {tensorSize}, "const");
-  addConst->getHandle().clear(10.0f);
-  // Create a simple graph.
+  // Common constants.
+  Constant *matMulConst1 = M.createConstant(
+      ElemKind::FloatTy, {tensorSize, tensorSize}, "matmulconst1");
+  matMulConst1->getHandle().clear(0.0625f * 1.0f);
+
+  Constant *matMulConst2 = M.createConstant(
+      ElemKind::FloatTy, {tensorSize, tensorSize}, "matmulconst2");
+  matMulConst2->getHandle().clear(0.0625f * 2.0f);
+
+  Constant *matMulConst3 = M.createConstant(
+      ElemKind::FloatTy, {tensorSize, tensorSize}, "matmulconst3");
+  matMulConst3->getHandle().clear(0.0625f * 3.0f);
+
+  // Create a simple graph with 2 MatMul layers.
   Function *F1 = M.createFunction("F1");
   auto *inputPH1 =
       M.createPlaceholder(ElemKind::FloatTy, {tensorSize}, "input1", false);
-  auto *addConst1 = M.createConstant(ElemKind::FloatTy, {tensorSize}, "const1");
-  addConst1->getHandle().clear(20.0f);
-  Node *addNode11 = F1->createAdd("add", inputPH1, addConst);
-  Node *addNode12 = F1->createAdd("add", addNode11, addConst1);
-  auto *save1 = F1->createSave("output1", addNode12);
+  auto *reshapeNode11 = F1->createReshape("reshape", inputPH1, {1, tensorSize});
+  auto *matMulNode11 = F1->createMatMul("matmul", reshapeNode11, matMulConst1);
+  auto *matMulNode12 = F1->createMatMul("matmul", matMulNode11, matMulConst2);
+  auto *reshapeNode12 =
+      F1->createReshape("reshape", matMulNode12, {tensorSize});
+  auto *save1 = F1->createSave("output1", reshapeNode12);
 
-  // Create a simple graph.
+  // Create a simple graph with 3 MatMul layers.
   Function *F2 = M.createFunction("F2");
   auto *inputPH2 =
       M.createPlaceholder(ElemKind::FloatTy, {tensorSize}, "input2", false);
-  auto *addConst2 = M.createConstant(ElemKind::FloatTy, {tensorSize}, "const2");
-  addConst2->getHandle().clear(30.0f);
-  Node *addNode21 = F2->createAdd("add", inputPH2, addConst);
-  Node *addNode22 = F2->createSub("add", addNode21, addConst2);
-  auto *save2 = F2->createSave("output2", addNode22);
+  auto *reshapeNode21 = F2->createReshape("reshape", inputPH2, {1, tensorSize});
+  auto *matMulNode21 = F2->createMatMul("matmul", reshapeNode21, matMulConst1);
+  auto *matMulNode22 = F2->createMatMul("matmul", matMulNode21, matMulConst2);
+  auto *matMulNode23 = F2->createMatMul("matmul", matMulNode22, matMulConst3);
+  auto *reshapeNode22 =
+      F2->createReshape("reshape", matMulNode23, {tensorSize});
+  auto *save2 = F2->createSave("output2", reshapeNode22);
 
   // Save a bundle with multiple functions.
   llvm::StringRef outputDir = emitBundle;
@@ -79,7 +93,7 @@ int main(int argc, char **argv) {
   ctx1.getPlaceholderBindings()->get(inputPH1)->getHandle().clear(1.0);
   EXIT_ON_ERR(EXIT_ON_ERR(backend->compile(F1))->execute(&ctx1));
   Tensor expected1(ElemKind::FloatTy, {tensorSize});
-  expected1.getHandle().clear(31.0f);
+  expected1.getHandle().clear(2.0f);
   DCHECK(ctx1.getPlaceholderBindings()
              ->get(save1->getPlaceholder())
              ->isEqual(expected1))
@@ -91,7 +105,7 @@ int main(int argc, char **argv) {
   ctx2.getPlaceholderBindings()->get(inputPH2)->getHandle().clear(1.0);
   EXIT_ON_ERR(EXIT_ON_ERR(backend->compile(F2))->execute(&ctx2));
   Tensor expected2(ElemKind::FloatTy, {tensorSize});
-  expected2.getHandle().clear(-19.0f);
+  expected2.getHandle().clear(6.0f);
   DCHECK(ctx2.getPlaceholderBindings()
              ->get(save2->getPlaceholder())
              ->isEqual(expected2))
