d22dbd62cac425bf4788e1a3c2f5d11654704683
Michael Anderson <anderso2@fb.com>
2021-05-06 13:33:36 -0700

    Adding kernel generator for CompareInt32 (#5636)
    Summary: Pull Request resolved: https://github.com/pytorch/glow/pull/5636
    
    Reviewed By: khabinov, mikekgfb
    
    Differential Revision: D28233771
    
    fbshipit-source-id: 513b6d911acfc7c281568a3782c11ef6d2c5bf3d

--- lib/Backends/NNPI/CustomKernels/DSPInjectors/DSPInjectors.h
+++ lib/Backends/NNPI/CustomKernels/DSPInjectors/DSPInjectors.h
@@ -18,6 +18,21 @@ struct CustomCmpNEQNodeDSPKernelInjector : public CustomKernelInjector {
   bool tryInject(Function *F, Node *node) const override;
 };
 
+/// Injector for custom CmpEQ node.
+struct CustomCmpEQNodeDSPKernelInjector : public CustomKernelInjector {
+  bool tryInject(Function *F, Node *node) const override;
+};
+
+/// Injector for custom CmpLT node.
+struct CustomCmpLTNodeDSPKernelInjector : public CustomKernelInjector {
+  bool tryInject(Function *F, Node *node) const override;
+};
+
+/// Injector for custom CmpLTE node.
+struct CustomCmpLTENodeDSPKernelInjector : public CustomKernelInjector {
+  bool tryInject(Function *F, Node *node) const override;
+};
+
 /// \returns the list of all CustomKernelInjectors for DSP nodes.
 std::vector<std::unique_ptr<CustomKernelInjector>> buildDSPInjectors();
 
