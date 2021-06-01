90eb9152ed3d4d4df147112d0befbcce1ea31096
Roman Levenstein <rlevenstein@fb.com>
2021-06-01 10:11:44 -0700

    Allow for initialization of the TargetOptions in a backend-specific way (#5691)
    Summary:
    Pull Request resolved: https://github.com/pytorch/glow/pull/5691
    
    This way backends have more control over options being used during object code emission.
    
    Reviewed By: shajrawi
    
    Differential Revision: D28799688
    
    fbshipit-source-id: f8371a8ceadb264fd379a3c63f16243bc515502a

--- include/glow/LLVMIRCodeGen/LLVMIRGen.h
+++ include/glow/LLVMIRCodeGen/LLVMIRGen.h
@@ -348,6 +348,11 @@ public:
   /// Init the TargetMachine using settings provided by \p llvmBackend.
   virtual void initTargetMachine(const LLVMBackendOptions &opts);
 
+  /// Init the TargetOptions \p targetOpts in a backend-specific way. Use \p
+  /// backendOpts as input if necessary.
+  virtual void initTargetOptions(llvm::TargetOptions &targetOpts,
+                                 const LLVMBackendOptions &backendOpts);
+
   /// Emit LLVM-IR for the instruction \p I, using the builder \p builder.
   /// Derived classes may want to override this function to implement a
   /// backend-specific LLVM IR generation logic for some intructions.
