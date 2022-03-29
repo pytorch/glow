0c7fc9a22f592279bd7337ff6430276b4608f5d3
EpureRares <rares.epure@nxp.com>
2022-03-29 10:29:39 +0300

    dummy commit

--- lib/LLVMIRCodeGen/LLVMBackend.cpp
+++ lib/LLVMIRCodeGen/LLVMBackend.cpp
@@ -76,8 +76,6 @@ bool LLVMBackend::isOpSupported(const NodeInfo &NI) const {
   case Kinded::Kind::BatchedReduceAddNodeKind:
   case Kinded::Kind::MatMulNodeKind:
   case Kinded::Kind::HardSwishNodeKind:
-    return NI.allInputsAndOutputsHaveSameElemKind({ElemKind::FloatTy});
-
   case Kinded::Kind::AvgPoolNodeKind:
     return NI.allInputsAndOutputsHaveSameElemKind(
         {ElemKind::FloatTy, ElemKind::Int8QTy});
