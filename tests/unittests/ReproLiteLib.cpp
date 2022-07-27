4783b5972b3bfefa18bef6b3e679cf1aed05a010
Jay Banerjee <jaybane@fb.com>
2022-07-27 13:26:01 -0700

    Introduce ReproLite to work with AFG artifacts.
    Summary: ReproFX can not effectively use artifacts generated by AFG. It is more practical to introduce a new target. In time, ReproFX will be retired once AGM is no longer used.
    
    Reviewed By: dborkovic
    
    Differential Revision: D38104200
    
    fbshipit-source-id: 32e02792e3c27106a995cb7c3992bc4dbdc46769

---
+++ tests/unittests/ReproLiteLib.cpp
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) Glow Contributors. See CONTRIBUTORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "glow/glow/tests/unittests/ReproLiteLib.h"
+#include "glow/glow/lib/Backends/FBA/FBABackend.h"
+#include <filesystem>
+#include <folly/dynamic.h>
+#include <folly/json.h>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <torch/script.h>
+#include <torch/torch.h>
+#include <vector>
+
+llvm::cl::OptionCategory reproTestCat("Repro Category");
+llvm::cl::opt<std::string>
+    reproPathOpt("repro_path", llvm::cl::desc("Path to Repro Base Directory."),
+                 llvm::cl::Required, llvm::cl::cat(reproTestCat));
+
+// Parses JSON input, weights, and user inputs
+
+static const std::string &checkPath(const std::string &path,
+                                    bool missingPathFatal = false) {
+  if (std::filesystem::exists(path)) {
+    return path;
+  }
+  if (!missingPathFatal) {
+    LOG(INFO) << "Didn't find " << path;
+    static const std::string empty = "";
+    return empty;
+  } else {
+    LOG(FATAL) << "Didn't find " << path;
+  }
+}
+
+void ReproLiteLib::loadFromAFG() {
+  std::ifstream jsonFile(jsonPathOpt_.c_str());
+  std::stringstream buffer;
+  buffer << jsonFile.rdbuf();
+  serializedGraphJson_ = buffer.str();
+  std::ifstream fin(weightsPathOpt_, std::ios::binary);
+  std::vector<char> bytes((std::istreambuf_iterator<char>(fin)),
+                          (std::istreambuf_iterator<char>()));
+  auto res = torch::pickle_load(bytes);
+  fin.close();
+  for (auto &item : res.toGenericDict()) {
+    at::IValue key = item.key();
+    at::IValue val = item.value();
+    strweights_[key.toString()->string()] = (void *)&(val.toTensor());
+  }
+}
+
+void ReproLiteLib::parseCommandLine(int argc, char **argv) {
+  llvm::cl::ParseCommandLineOptions(argc, argv);
+
+  jsonPathOpt_ = checkPath(reproPathOpt + "/graph_ir.json");
+  std::cout << "repro_path flag set to '" << reproPathOpt;
+  weightsPathOpt_ = checkPath(reproPathOpt + "/weights.pt", true);
+}
+
+void ReproLiteLib::run() {
+  // Create FXGlowCompileSpec.
+  loadFromAFG();
+  config_["lower_to_llvm"] = "true";
+  auto backend = glow::FBABackend();
+  auto compiledOrErr = backend.compileFXToCompiledResults(
+      folly::parseJson(serializedGraphJson_), strweights_, config_);
+}
---
+++ tests/unittests/ReproLiteLib.h
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) Glow Contributors. See CONTRIBUTORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <llvm/ADT/StringMap.h>
+#include <map>
+#include <string>
+#include <vector>
+
+#ifndef GLOW_TESTS_REPROFXLIB_H
+#define GLOW_TESTS_REPROFXLIB_H
+
+class ReproLiteLib {
+public:
+  std::string serializedGraphJson_;
+  std::string jsonPathOpt_;
+  std::string weightsPathOpt_;
+  std::map<std::string, std::string> config_;
+  llvm::StringMap<const void *> strweights_;
+
+  void loadFromAFG();
+  void parseCommandLine(int argc, char **argv);
+  void run();
+};
+
+#endif // GLOW_TESTS_REPROFXLIB_H
